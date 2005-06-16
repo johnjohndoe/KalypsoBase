e617d9445103f6fbb5c32241f7a4289332af38ba
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2005-06-16 09:31:35 +0000

    *** empty log message ***

---
+++ EclipseRCPContributions/src/org/bce/eclipse/ui/PartAdapter2.java
@@ -0,0 +1,40 @@
+package org.bce.eclipse.ui;
+
+import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IWorkbenchPartReference;
+
+/** Default implementation of {@link org.eclipse.ui.IPartListener2} which does nothing. */
+public class PartAdapter2 implements IPartListener2
+{
+  public void partActivated( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partBroughtToTop( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partClosed( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partDeactivated( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partOpened( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partHidden( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partVisible( IWorkbenchPartReference partRef )
+  {
+  }
+
+  public void partInputChanged( IWorkbenchPartReference partRef )
+  {
+  }
+}
