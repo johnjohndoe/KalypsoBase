9d6758b5989f6ebd6475d43a2902c0e23dd1c877
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2005-08-23 09:20:43 +0000

    - ein paar GUI Probleme repariert (zuviele offene Fenster beim Neustart) - Reparator eingebaut

---
+++ EclipsePlatformContributions/src/org/bce/eclipse/ui/FileResourceEditorMatchingStrategy.java
@@ -0,0 +1,25 @@
+package org.bce.eclipse.ui;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorMatchingStrategy;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+
+/**
+ * This matching strategies matches if both editor and the given input adapt to an equal
+ * {@link org.eclipse.core.resources.IFile}.
+ * 
+ * @author belger
+ */
+public class FileResourceEditorMatchingStrategy implements IEditorMatchingStrategy
+{
+  public boolean matches( final IEditorReference editorRef, final IEditorInput input )
+  {
+    final IEditorPart editor = editorRef.getEditor( false );
+    final Object editorFile = editor.getAdapter( IFile.class );
+    final Object inputFile = input.getAdapter( IFile.class );
+
+    return inputFile != null && inputFile.equals( editorFile );
+  }
+}
