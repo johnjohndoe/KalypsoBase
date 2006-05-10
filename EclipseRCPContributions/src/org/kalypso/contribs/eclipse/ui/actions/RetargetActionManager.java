932eb77f095d2d12f9f4b7068ff403608d554628
kimwerner <kimwerner@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2006-05-10 13:20:36 +0000

    *** empty log message ***

--- EclipseRCPContributions/src/org/kalypso/contribs/eclipse/ui/actions/RetargetActionManager.java
+++ EclipseRCPContributions/src/org/kalypso/contribs/eclipse/ui/actions/RetargetActionManager.java
@@ -1,14 +1,21 @@
 package org.kalypso.contribs.eclipse.ui.actions;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.jface.action.AbstractGroupMarker;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IContributionManager;
+import org.eclipse.jface.action.IMenuCreator;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
@@ -30,7 +37,7 @@ public class RetargetActionManager
   public static final String MENU_GROUP_RADIO = "radio";
 
   public static final String MENU_GROUP_CHECK = "check";
-  
+
   public static final String MENU_GROUP_MENU = "menu";
 
   public static final class RetargetInfo
@@ -43,18 +50,36 @@ public class RetargetActionManager
 
     private String m_menuPath = null;
 
+    private String m_menuGroup = null;
+
     /**
-     * @param id global action id
+     * @param id
+     *          global action id
      * @see org.eclipse.ui.actions.ActionFactory
      */
     public RetargetInfo( final String id, final String label, final int style )
     {
-      m_retargetAction = new RetargetAction( id, label, style );
+      this( new RetargetAction( id, label, style ) );
     }
-    
+
     public RetargetInfo( final RetargetAction retargetAction )
     {
       m_retargetAction = retargetAction;
+      if( (m_retargetAction.getStyle() == IAction.AS_DROP_DOWN_MENU) && (m_retargetAction.getMenuCreator() == null) )
+      {
+        m_retargetAction.setMenuCreator( new DropDownMenuCreator() );
+      }
+      
+    }
+
+    public String getMenuGroup( )
+    {
+      return m_menuGroup;
+    }
+
+    public void setMenuGroup( String menuGroup )
+    {
+      m_menuGroup = menuGroup;
     }
 
     public IEditorPartAction getActionHandler( )
@@ -103,9 +128,26 @@ public class RetargetActionManager
     {
       m_toolbarGroup = toolbarGroup;
     }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString( )
+    {
+      return getID() + " - " + getMenuPath();
+    }
   }
 
   private Map<String, RetargetInfo> m_retargetMap = new HashMap<String, RetargetInfo>();
+
+  private List<RetargetInfo> m_sortedRetargetinfo = new ArrayList<RetargetInfo>();
+
+  public RetargetActionManager( )
+  {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
   public RetargetInfo addRetargetInfo( final RetargetInfo info )
   {
@@ -155,32 +197,55 @@ public class RetargetActionManager
     toolBarManager.add( new Separator( MENU_GROUP_MENU ) );
     toolBarManager.add( new Separator( IWorkbenchActionConstants.MB_ADDITIONS ) );
 
-    for( final Map.Entry<String, RetargetInfo> entry : m_retargetMap.entrySet() )
+    for( final RetargetInfo info : m_sortedRetargetinfo )
     {
-      final RetargetInfo info = entry.getValue();
-      final String toolbarGroup = info.getToolbarGroup();
-      if( toolbarGroup != null )
-        toolBarManager.appendToGroup( toolbarGroup, info.getRetargetAction() );
+      final String toolBarGroup = info.getToolbarGroup();
+      final IContributionItem item = toolBarManager.find( toolBarGroup );
+      final RetargetAction retargetAction = info.getRetargetAction();
+      if( item instanceof Separator )
+        toolBarManager.appendToGroup( item.getId(), retargetAction );
+      else if( item instanceof ActionContributionItem )
+      {
+        final IAction action = ((ActionContributionItem) item).getAction();
+        final IMenuCreator mc = action.getMenuCreator();
+        if( mc instanceof DropDownMenuCreator )
+          ((DropDownMenuCreator) mc).addAction( info.getRetargetAction() );
+      }
     }
+  }
+
+  public void sortRetargetInfos( final Comparator<RetargetInfo> comparator )
+  {
+    if( !m_sortedRetargetinfo.containsAll( m_retargetMap.values() ) )
+    {
+      m_sortedRetargetinfo.clear();
+      m_sortedRetargetinfo.addAll( m_retargetMap.values() );
+    }
+    Collections.sort( m_sortedRetargetinfo, comparator );
   }
 
   public void contributeToMenu( final IMenuManager menuManager )
   {
-    for( final Map.Entry<String, RetargetInfo> entry : m_retargetMap.entrySet() )
+
+    for( RetargetInfo info : m_sortedRetargetinfo )
     {
-      final RetargetInfo info = entry.getValue();
-      final String menuPath = info.getMenuPath();
-      if( menuPath != null )
+      final String menuGroup = info.getMenuGroup();
+      if( menuGroup != null )
       {
-        final IContributionItem item = menuManager.findUsingPath( menuPath );
+        final IContributionItem item = menuManager.findUsingPath( menuGroup );
         final RetargetAction retargetAction = info.getRetargetAction();
+
         if( item instanceof IMenuManager )
+        {
           ((IMenuManager) item).add( retargetAction );
+        }
         else if( item instanceof AbstractGroupMarker )
         {
-          final AbstractGroupMarker gm = (AbstractGroupMarker) item;
-          final IContributionManager parent = gm.getParent();
-          parent.appendToGroup( gm.getGroupName(), retargetAction );
+          final AbstractGroupMarker gm = ((AbstractGroupMarker) item);
+          if( retargetAction.getStyle() == IAction.AS_DROP_DOWN_MENU )
+            gm.getParent().appendToGroup( gm.getGroupName(), new MenuManager( retargetAction.getText(), retargetAction.getId() ) );
+          else
+            gm.getParent().appendToGroup( gm.getGroupName(), retargetAction );
         }
       }
     }
@@ -221,12 +286,24 @@ public class RetargetActionManager
     manager.add( new Separator( MENU_GROUP_MENU ) );
     manager.add( new Separator( IWorkbenchActionConstants.MB_ADDITIONS ) );
 
-    for( final Map.Entry<String, RetargetInfo> entry : m_retargetMap.entrySet() )
+    for( RetargetInfo info : m_sortedRetargetinfo )
     {
-      final RetargetInfo info = entry.getValue();
+      final String toolbarGroup = (info.getToolbarGroup() == null) ? IWorkbenchActionConstants.MB_ADDITIONS : info.getToolbarGroup();
 
-      final String group = menuGroupForStyle( info.getRetargetAction().getStyle() );
-      manager.appendToGroup( group, info.getRetargetAction() );
+      final IContributionItem item = manager.findUsingPath( toolbarGroup );
+      final RetargetAction retargetAction = info.getRetargetAction();
+      if( item instanceof AbstractGroupMarker )
+      {
+        if( retargetAction.getStyle() == IAction.AS_DROP_DOWN_MENU )
+          manager.appendToGroup( toolbarGroup, new MenuManager( retargetAction.getText(), retargetAction.getId() ) );
+        else
+          manager.appendToGroup( toolbarGroup, retargetAction );
+      }
+      else if( item instanceof MenuManager )
+      {
+        ((MenuManager) item).add( retargetAction );
+      }
+
     }
   }
 
@@ -242,6 +319,7 @@ public class RetargetActionManager
 
       case IAction.AS_RADIO_BUTTON:
         return MENU_GROUP_RADIO;
+
       case IAction.AS_DROP_DOWN_MENU:
         return MENU_GROUP_MENU;
     }
