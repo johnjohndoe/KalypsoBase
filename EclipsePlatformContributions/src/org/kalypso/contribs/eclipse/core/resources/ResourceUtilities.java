bf0a3ab1899999d60614a9944a459fcd89fd967b
alibu <alibu@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2008-01-25 14:42:33 +0000

    added comment: if projects are imported and not copied into workspace, no files are found

--- EclipsePlatformContributions/src/org/kalypso/contribs/eclipse/core/resources/ResourceUtilities.java
+++ EclipsePlatformContributions/src/org/kalypso/contribs/eclipse/core/resources/ResourceUtilities.java
@@ -108,20 +108,24 @@ public class ResourceUtilities
 
   /**
    * Only works with relative paths. The location (absolute) of the workspace root is used as parent, the resources path
-   * (relative) is used as child.
+   * (relative) is used as child. Warning: No files are found if the project root of the project in which the resource
+   * should exist lies outside the workspace root (i.e. if a project is imported without copying it into the workspace).
    * 
    * @return A Java-File representing the resource.
    */
   public static File makeFileFromPath( final IPath resource )
   {
+
     final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
     final IPath rootLocation = root.getLocation();
-    //note: this works for linked files
+    // note: this works for linked files
     IFile[] files = root.findFilesForLocation( rootLocation.append( resource ) );
     if( files.length == 1 )
       return files[0].getLocation().toFile();
-    else
+    else if( files.length > 1 )
       throw new IllegalStateException( "More than one matching resource found for path " + resource + "!" );
+    else
+      throw new IllegalStateException( "No matching resource found for path " + resource + "!" );
   }
 
   public static IProject findProjectFromURL( final URL baseURL )
