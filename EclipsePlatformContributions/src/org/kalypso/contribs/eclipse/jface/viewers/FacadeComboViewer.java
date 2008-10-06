d3a231030e20aec2a2c08b02b3ff569c65b915c1
holger.albert <holger.albert@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2008-10-06 11:38:40 +0000

    before merge (head)

---
+++ EclipsePlatformContributions/src/org/kalypso/contribs/eclipse/jface/viewers/FacadeComboViewer.java
@@ -0,0 +1,187 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.contribs.eclipse.jface.viewers;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * Easy to use ComboViewer class<br>
+ * <br>
+ * ComboViewer has always Input data, an starting selection and selection change event<br>
+ * This class helps handling those aspects (example):<br>
+ * <br>
+ * final INPUT[] wqInput = new INPUT[] { INPUT.eOne, INPUT.eTwo };<br>
+ * final FacadeComboViewer fcv = new FacadeComboViewer( new FCVArrayDelegate( wqInput ) );<br>
+ * fcv.draw( composite, new GridData(), SWT.BORDER | SWT.READ_ONLY | SWT.SINGLE );<br>
+ * fcv.addSelectionChangedListener( new Runnable());<br>
+ * <br>
+ * IFacadeComboViewerDelegate handles the processing of your input data and default selection of your combobox
+ * 
+ * @author Dirk Kuch
+ */
+public class FacadeComboViewer
+{
+  private final List<Runnable> m_listener = new LinkedList<Runnable>();
+
+  protected ISelection m_selection = new StructuredSelection();
+
+  protected ComboViewer m_comboViewer;
+
+  private Object[] m_inputData;
+
+  protected final IFCVDelegate m_delegate;
+
+  private final boolean m_enableSingleElementSelection;
+
+  public FacadeComboViewer( final IFCVDelegate delegate )
+  {
+    this( delegate, false );
+  }
+
+  public FacadeComboViewer( final IFCVDelegate delegate, final boolean enableSingleElementSelection )
+  {
+    m_delegate = delegate;
+    m_enableSingleElementSelection = enableSingleElementSelection;
+    initSelection();
+  }
+
+  private void initSelection( )
+  {
+    m_inputData = m_delegate.getInputData();
+
+    if( m_enableSingleElementSelection )
+      if( m_inputData.length == 1 )
+      {
+        final StructuredSelection selection = new StructuredSelection( m_inputData[0] );
+        m_selection = selection;
+      }
+
+    if( m_inputData.length > 0 )
+      m_selection = m_delegate.getDefaultKey();
+  }
+
+  public void addSelectionChangedListener( final Runnable selectionChangedListener )
+  {
+    m_listener.add( selectionChangedListener );
+  }
+
+  public void draw( final Composite parent, final GridData layout, final int style )
+  {
+    m_comboViewer = new ComboViewer( parent, style );
+    m_comboViewer.getCombo().setLayoutData( layout );
+    m_comboViewer.setContentProvider( new ArrayContentProvider() );
+    m_comboViewer.setLabelProvider( new LabelProvider()
+    {
+      /**
+       * @see org.eclipse.jface.viewers.LabelProvider#getText(java.lang.Object)
+       */
+      @Override
+      public String getText( final Object element )
+      {
+        final String label = m_delegate.getValue( element );
+        if( label != null )
+          return label;
+
+        return super.getText( element );
+      }
+    } );
+
+    m_comboViewer.setInput( m_inputData );
+
+    if( m_enableSingleElementSelection )
+      if( m_inputData.length == 1 )
+      {
+        final StructuredSelection selection = new StructuredSelection( m_inputData[0] );
+        m_comboViewer.setSelection( selection );
+        m_selection = selection;
+        m_comboViewer.getCombo().setEnabled( false );
+      }
+
+    if( m_inputData.length == 0 )
+      m_comboViewer.getCombo().setEnabled( false );
+    else
+    {
+      m_comboViewer.setSelection( m_delegate.getDefaultKey() );
+      m_selection = m_delegate.getDefaultKey();
+    }
+
+    m_comboViewer.addSelectionChangedListener( new ISelectionChangedListener()
+    {
+      public void selectionChanged( final SelectionChangedEvent event )
+      {
+        m_selection = m_comboViewer.getSelection();
+        processListener();
+      }
+    } );
+  }
+
+  public ISelection getSelection( )
+  {
+    return m_selection;
+  }
+
+  protected void processListener( )
+  {
+    for( final Runnable listener : m_listener )
+      listener.run();
+  }
+
+  public void setEnabled( final boolean b )
+  {
+    m_comboViewer.getCombo().setEnabled( b );
+  }
+
+  public void setSelection( final ISelection selection )
+  {
+    m_comboViewer.setSelection( selection );
+  }
+}
---
+++ EclipsePlatformContributions/src/org/kalypso/contribs/eclipse/jface/wizard/FileChooserGroup.java
@@ -0,0 +1,328 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ *
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ *
+ *  and
+ *
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Contact:
+ *
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.contribs.eclipse.jface.wizard;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.kalypso.contribs.eclipse.EclipsePlatformContributionsPlugin;
+import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
+import org.kalypso.contribs.eclipse.jface.wizard.FileChooserGroup.FileChooserDelegate.FILE_CHOOSER_GROUP_TYPE;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * A group to choose a file from the file system.
+ * 
+ * @author Gernot Belger
+ */
+public class FileChooserGroup
+{
+  static public class FileChooserDelegate
+  {
+    private final FILE_CHOOSER_GROUP_TYPE m_type;
+
+    public enum FILE_CHOOSER_GROUP_TYPE
+    {
+      eOpen,
+      eSave
+    }
+
+    public FileChooserDelegate( final FILE_CHOOSER_GROUP_TYPE type )
+    {
+      m_type = type;
+    }
+
+    public int getTextBoxStyle( )
+    {
+      switch( m_type )
+      {
+        case eOpen:
+          return SWT.BORDER;
+
+        case eSave:
+          return SWT.BORDER | SWT.READ_ONLY;
+
+        default:
+          throw (new NotImplementedException());
+      }
+    }
+
+    public String getButtonText( )
+    {
+      switch( m_type )
+      {
+        case eOpen:
+          return "Datei ausw‰hlen";
+
+        case eSave:
+          return "Datei speichern unter";
+
+        default:
+          throw (new NotImplementedException());
+      }
+    }
+
+    public int getFileDialogType( )
+    {
+      switch( m_type )
+      {
+        case eOpen:
+          return SWT.OPEN;
+
+        case eSave:
+          return SWT.SAVE;
+
+        default:
+          throw (new NotImplementedException());
+      }
+    }
+
+    /**
+     * overwrite
+     */
+    public String[] getFilterExtensions( )
+    {
+      return new String[] {};
+    }
+
+    /**
+     * overwrite
+     */
+    public String[] getFilterNames( )
+    {
+      return new String[] {};
+    }
+
+    public String updateFileName( final FileDialog dialog, final String newFilename )
+    {
+      switch( m_type )
+      {
+        case eOpen:
+          return newFilename;
+
+        case eSave:
+          final String[] extensions = dialog.getFilterExtensions();
+          return newFilename + "." + extensions[0];
+
+        default:
+          throw (new NotImplementedException());
+      }
+    }
+  }
+
+  private static final String SETTINGS_FILENAME = "fileChooserGroup.filename";
+
+  private File m_file;
+
+  private IDialogSettings m_settings;
+
+  private Text m_text;
+
+  private final Set<FileChangedListener> m_listeners = new HashSet<FileChangedListener>();
+
+  private final FileChooserDelegate m_delegate;
+
+  public interface FileChangedListener
+  {
+    public void fileChanged( final File file );
+  }
+
+  public FileChooserGroup( )
+  {
+    this( new FileChooserDelegate( FILE_CHOOSER_GROUP_TYPE.eOpen ) );
+  }
+
+  public FileChooserGroup( final FileChooserDelegate delegate )
+  {
+    m_delegate = delegate;
+  }
+
+  /**
+   * Sets the dialog settings used to remeber the last entered filename.
+   * 
+   * @param settings
+   *            If <code>null</code>, the filename will not be stored.
+   */
+  public void setDialogSettings( final IDialogSettings settings )
+  {
+    m_settings = settings;
+
+    if( (m_settings != null) && (m_text != null) && !m_text.isDisposed() )
+    {
+      final String filename = m_settings.get( FileChooserGroup.SETTINGS_FILENAME );
+      if( filename != null )
+      {
+        final Text text = m_text;
+        text.getDisplay().asyncExec( new Runnable()
+        {
+          public void run( )
+          {
+            text.setText( filename );
+          }
+        } );
+      }
+    }
+  }
+
+  public Group createControl( final Composite parent, final int style )
+  {
+    final Group group = new Group( parent, style );
+
+    final GridLayout gridLayout = new GridLayout();
+    gridLayout.numColumns = 3;
+
+    group.setLayout( gridLayout );
+
+    final Label label = new Label( group, SWT.NONE );
+    label.setText( "&Datei" );
+
+    final Text text = new Text( group, m_delegate.getTextBoxStyle() );
+    m_text = text;
+    text.setLayoutData( new GridData( SWT.FILL, SWT.CENTER, true, false ) );
+    m_text.addModifyListener( new ModifyListener()
+    {
+      public void modifyText( final ModifyEvent e )
+      {
+        textModified( text.getText() );
+      }
+    } );
+
+    final Button button = new Button( group, SWT.NONE );
+    button.setText( "..." );
+    button.setToolTipText( m_delegate.getButtonText() );
+    button.addSelectionListener( new SelectionAdapter()
+    {
+      @Override
+      public void widgetSelected( final SelectionEvent e )
+      {
+        buttonPressed();
+      }
+    } );
+
+    /* Restore settings */
+    if( m_settings != null )
+    {
+      final String filename = m_settings.get( FileChooserGroup.SETTINGS_FILENAME );
+      if( filename != null )
+        m_text.setText( filename );
+    }
+
+    return group;
+  }
+
+  protected void textModified( final String text )
+  {
+    setFile( new File( text ) );
+  }
+
+  protected void buttonPressed( )
+  {
+    final String currentFilename = m_text.getText();
+
+    final FileDialog dialog = new FileDialog( m_text.getShell(), m_delegate.getFileDialogType() );
+    dialog.setFileName( currentFilename );
+    dialog.setText( m_delegate.getButtonText() );
+
+    dialog.setFilterExtensions( m_delegate.getFilterExtensions() );
+    dialog.setFilterNames( m_delegate.getFilterNames() );
+
+    final String newFilename = dialog.open();
+    if( newFilename == null )
+      return;
+
+    m_text.setText( m_delegate.updateFileName( dialog, newFilename ) );
+  }
+
+  private void setFile( final File file )
+  {
+    if( m_file == file )
+      return;
+
+    m_file = file;
+
+    if( m_settings != null )
+      m_settings.put( FileChooserGroup.SETTINGS_FILENAME, file.getAbsolutePath() );
+
+    fireFileChanged( m_file );
+  }
+
+  private void fireFileChanged( final File file )
+  {
+    for( final FileChangedListener l : m_listeners )
+      try
+      {
+        l.fileChanged( file );
+      }
+      catch( final Throwable e )
+      {
+        EclipsePlatformContributionsPlugin.getDefault().getLog().log( StatusUtilities.statusFromThrowable( e ) );
+      }
+  }
+
+  /**
+   * Has no effekt if the same listener has already been added.
+   */
+  public void addFileChangedListener( final FileChangedListener l )
+  {
+    m_listeners.add( l );
+  }
+
+  public void removeFileChangedListener( final FileChangedListener l )
+  {
+    m_listeners.remove( l );
+  }
+}
