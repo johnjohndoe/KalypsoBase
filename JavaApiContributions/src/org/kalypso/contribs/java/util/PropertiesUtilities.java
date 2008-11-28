65ec47e62d400b50cfae8732be32066af9ec94d8
gernot.belger <gernot.belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2008-11-28 15:28:23 +0000

    RESOLVED - bug 1147: [i18n] Internationalize the workflow.xml https://dev.bjoernsen.de/cgi-bin/bugzilla/show_bug.cgi?id=1147
    Access to workflow-binding stuff hidden behind interfaces; access to translatable string intercepted by wrapper classes implementing these interfaces.

--- JavaApiContributions/src/org/kalypso/contribs/java/util/PropertiesUtilities.java
+++ JavaApiContributions/src/org/kalypso/contribs/java/util/PropertiesUtilities.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.contribs.java.util;
 
@@ -49,6 +49,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
+
+import org.kalypso.contribs.java.io.FileUtilities;
 
 /**
  * @author doemming
@@ -83,6 +85,9 @@ public class PropertiesUtilities
     return collector;
   }
 
+  /**
+   * Tires to load a (international) properties file from a given location.
+   */
   public static void loadI18nProperties( final Properties properties, final URL baseUrl, final String path )
   {
     final String[] suffixes = getLocalSuffixes();
@@ -91,6 +96,52 @@ public class PropertiesUtilities
       try
       {
         final URL url = new URL( baseUrl, path + suffix );
+        load( url, properties );
+        // On first success: stop loading
+        return;
+      }
+      catch( final FileNotFoundException e )
+      {
+        // ignore file not found exceptions, as we have several tries
+      }
+      catch( final IOException e )
+      {
+        e.printStackTrace();
+      }
+      finally
+      {
+      }
+    }
+
+    // If we reach this line, nothing was found...
+    // TODO: produces too much output, make a tracing option
+// final IStatus status = new Status( IStatus.ERROR,
+// JavaApiContributionsPlugin.getDefault().getBundle().getSymbolicName(), "Message file not found: " + path +
+// suffixes[0] );
+// JavaApiContributionsPlugin.getDefault().getLog().log( status );
+  }
+
+  /**
+   * Tries to load a (international) properties file for a given existing 'file'.<br>
+   * The existing file is given by its url location. The property file is searches as follows: the file extension is
+   * replaces by the current language pattern plus '.properties'.<br>
+   * Example: <code>http://somehist/workflow.xml</code> is replaced by
+   * <code>http://somehost/workflow_de_DE.properties</code>.<br>
+   * According to java conventions, different language patterns are tried before giving up: 'de_DE', 'de', 'DE', ''.<br>
+   * Attention: as the file extension is determined by {@link URL#toExternalForm()}, url with query parts are not
+   * supported.
+   */
+  public static void loadI18nProperties( final Properties properties, final URL baseUrl )
+  {
+    final String baseUrlAsString = baseUrl.toExternalForm();
+    final String baseUrlWithoutExtension = FileUtilities.nameWithoutExtension( baseUrlAsString );
+
+    final String[] suffixes = getLocalSuffixes();
+    for( final String suffix : suffixes )
+    {
+      try
+      {
+        final URL url = new URL( baseUrlWithoutExtension + suffix );
         load( url, properties );
         // On first success: stop loading
         return;
@@ -155,7 +206,7 @@ public class PropertiesUtilities
 
   /**
    * Loads the contents from the given {@link URL} into a {@link Properties} object.
-   * 
+   *
    * @see Properties#load(InputStream)
    */
   public static void load( final URL location, final Properties properties ) throws IOException
@@ -183,4 +234,38 @@ public class PropertiesUtilities
     }
   }
 
+  /**
+   * Returns an translated string for a given key, similar to i18n of the plugin.xml file.<br>
+   * The string is translated according to the given properties, if it start with '%'. If not, the original value is
+   * returned.<br>
+   * If the given string starts with '%', but the properties do not contain a corresponding entry, the original string
+   * is returned.
+   *
+   * @param translatableString
+   *          If <code>null</code>, <code>null</code> is returned.
+   * @param i10nProperties
+   *          If <code>null</code>, we always return <code>translatableString</code>.
+   * @return If <code>translatableString</code> starts with '%',
+   *         <code>i10nproperties.get( translatableString.substring(1) )</code> is returned. Else return
+   *         <code>translatableString</code>.
+   */
+  public static String getI18NString( final String translatableString, final Properties i10nProperties )
+  {
+    if( translatableString == null )
+      return null;
+
+    if( translatableString.isEmpty() )
+      return translatableString;
+
+    if( i10nProperties == null )
+      return translatableString;
+
+    if( translatableString.charAt( 0 ) == '%' )
+    {
+      final String key = translatableString.substring( 1 );
+      return i10nProperties.getProperty( key, translatableString );
+    }
+
+    return translatableString;
+  }
 }
