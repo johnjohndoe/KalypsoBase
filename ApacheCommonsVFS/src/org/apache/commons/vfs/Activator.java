c491b5aa8f939f13649840bf8f86b397ef4f3419
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-02-23 13:16:51 +0000

    Put Kalypso-Settings into plugin, formatted and put header into existing files. PLEASE DO THIS YOURSELF NEXT TIME!

--- ApacheCommonsVFS/src/org/apache/commons/vfs/Activator.java
+++ ApacheCommonsVFS/src/org/apache/commons/vfs/Activator.java
@@ -1,10 +1,44 @@
+/*----------------    FILE HEADER Kimport java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.vfs.impl.DefaultFileSystemManager;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+rmany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
 package org.apache.commons.vfs;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.vfs.impl.DefaultFileSystemManager;
-import org.apache.commons.vfs.provider.FileProvider;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
@@ -12,44 +46,43 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-public class Activator extends Plugin {
-	private static final String EXTENSION_POINT_ID = "org.apache.commons.vfs.provider";
-	private static HashMap<String, IConfigurationElement> THE_PROVIDER_LOCATIONS;
+public class Activator extends Plugin
+{
+  private static final String EXTENSION_POINT_ID = "org.apache.commons.vfs.provider";
 
-	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		readExtensions();
-		final DefaultFileSystemManager fsManager = (DefaultFileSystemManager) VFS
-				.getManager();
-		for (final Map.Entry<String, IConfigurationElement> entry : THE_PROVIDER_LOCATIONS
-				.entrySet()) {
-			final IConfigurationElement element = (IConfigurationElement) entry
-					.getValue();
+  private static HashMap<String, IConfigurationElement> THE_PROVIDER_LOCATIONS;
 
-			final String scheme = element.getAttribute("scheme");
-			final VFSProviderExtension provider = (VFSProviderExtension) element
-					.createExecutableExtension("class");
-			fsManager.addProvider(scheme, provider.getProvider());
-			provider.init(fsManager);
-		}
-	}
+  @Override
+  public void start( final BundleContext context ) throws Exception
+  {
+    super.start( context );
+    readExtensions();
+    final DefaultFileSystemManager fsManager = (DefaultFileSystemManager) VFS.getManager();
+    for( final Map.Entry<String, IConfigurationElement> entry : THE_PROVIDER_LOCATIONS.entrySet() )
+    {
+      final IConfigurationElement element = entry.getValue();
 
-	private static void readExtensions() {
-		final IExtensionRegistry registry = Platform.getExtensionRegistry();
+      final String scheme = element.getAttribute( "scheme" );
+      final VFSProviderExtension provider = (VFSProviderExtension) element.createExecutableExtension( "class" );
+      fsManager.addProvider( scheme, provider.getProvider() );
+      provider.init( fsManager );
+    }
+  }
 
-		if (THE_PROVIDER_LOCATIONS == null) {
-			final IExtensionPoint extensionPoint = registry
-					.getExtensionPoint(EXTENSION_POINT_ID);
-			final IConfigurationElement[] configurationElements = extensionPoint
-					.getConfigurationElements();
-			THE_PROVIDER_LOCATIONS = new HashMap<String, IConfigurationElement>(
-					configurationElements.length);
-			for (int i = 0; i < configurationElements.length; i++) {
-				final IConfigurationElement element = configurationElements[i];
-				final String bundleName = element.getContributor().getName();
-				THE_PROVIDER_LOCATIONS.put(bundleName, element);
-			}
-		}
-	}
+  private static void readExtensions( )
+  {
+    final IExtensionRegistry registry = Platform.getExtensionRegistry();
+
+    if( THE_PROVIDER_LOCATIONS == null )
+    {
+      final IExtensionPoint extensionPoint = registry.getExtensionPoint( EXTENSION_POINT_ID );
+      final IConfigurationElement[] configurationElements = extensionPoint.getConfigurationElements();
+      THE_PROVIDER_LOCATIONS = new HashMap<String, IConfigurationElement>( configurationElements.length );
+      for( final IConfigurationElement element : configurationElements )
+      {
+        final String bundleName = element.getContributor().getName();
+        THE_PROVIDER_LOCATIONS.put( bundleName, element );
+      }
+    }
+  }
 }
