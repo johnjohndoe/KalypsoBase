fe7da11242dc1ffb2fd897ebaba6d0abe90e8225
nicoschrage <nicoschrage@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-03-10 16:30:48 +0000

    Tagging Milestone 1 release made on March, 10th 2009

---
+++ KalypsoSimulationUI/src/org/kalypso/simulation/ui/navigator/ModellContentProvider.java
@@ -0,0 +1,329 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.simulation.ui.navigator;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.IMemento;
+import org.eclipse.ui.navigator.ICommonContentExtensionSite;
+import org.eclipse.ui.navigator.ICommonContentProvider;
+import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
+import org.kalypso.contribs.eclipse.jface.viewers.ViewerUtilities;
+import org.kalypso.core.KalypsoCorePlugin;
+import org.kalypso.core.util.pool.IPoolListener;
+import org.kalypso.core.util.pool.IPoolableObjectType;
+import org.kalypso.core.util.pool.PoolableObjectType;
+import org.kalypso.core.util.pool.ResourcePool;
+import org.kalypso.ui.KalypsoGisPlugin;
+import org.kalypso.ui.editor.gmleditor.ui.FeatureAssociationTypeElement;
+import org.kalypso.ui.editor.gmleditor.ui.GMLContentProvider;
+import org.kalypsodeegree.model.feature.Feature;
+import org.kalypsodeegree.model.feature.GMLWorkspace;
+
+/**
+ * @author Gernot Belger
+ */
+public class ModellContentProvider implements ICommonContentProvider, IPoolListener
+{
+  private final static class KeyWrapper
+  {
+    private final IPoolableObjectType m_key;
+
+    private final GMLWorkspace m_workspace;
+
+    private final IStatus m_status;
+
+    public KeyWrapper( final IPoolableObjectType key, final GMLWorkspace workspace, final IStatus status )
+    {
+      m_key = key;
+      m_workspace = workspace;
+      m_status = status;
+    }
+
+    public IPoolableObjectType getKey( )
+    {
+      return m_key;
+    }
+
+    public GMLWorkspace getWorkspace( )
+    {
+      return m_workspace;
+    }
+
+    public IStatus getStatus( )
+    {
+      return m_status;
+    }
+  }
+
+  private final Map<IFile, KeyWrapper> m_workspaces = new HashMap<IFile, KeyWrapper>();
+
+  private final Map<GMLWorkspace, GMLContentProvider> m_contentProviderMap = new HashMap<GMLWorkspace, GMLContentProvider>();
+
+  private Viewer m_viewer;
+
+  /**
+   * @see org.eclipse.ui.navigator.ICommonContentProvider#init(org.eclipse.ui.navigator.ICommonContentExtensionSite)
+   */
+  public void init( final ICommonContentExtensionSite aConfig )
+  {
+    System.out.println( "Initialising ModellContentProvider" );
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+   */
+  public Object[] getChildren( final Object parentElement )
+  {
+    if( parentElement instanceof IProject )
+    {
+      final KeyWrapper wrapperForProject = wrapperForProject( (IProject) parentElement );
+      if( wrapperForProject == null )
+        return new Object[0];
+      else if( wrapperForProject.getWorkspace() == null )
+        return new Object[] { wrapperForProject.getStatus() };
+    }
+
+    final GMLWorkspace workspace = getWorkspace( parentElement );
+    if( workspace == null )
+      return new Object[] {};
+
+    final GMLContentProvider cp = getContentProviderForWorkspace( workspace );
+    if( cp == null )
+      return new Object[] {};
+
+    if( parentElement instanceof IProject )
+      return cp.getElements( null );
+    else
+      return cp.getChildren( parentElement );
+
+  }
+
+  private GMLContentProvider getContentProviderForWorkspace( final GMLWorkspace workspace )
+  {
+    if( m_contentProviderMap.containsKey( workspace ) )
+      return m_contentProviderMap.get( workspace );
+
+    final GMLContentProvider cp = new GMLContentProvider( true );
+    cp.inputChanged( null, null, workspace );
+
+    m_contentProviderMap.put( workspace, cp );
+
+    return cp;
+  }
+
+  private GMLWorkspace getWorkspace( final Object parentElement )
+  {
+    if( parentElement instanceof IProject )
+    {
+      final KeyWrapper wrapper = wrapperForProject( (IProject) parentElement );
+      return wrapper.getWorkspace();
+    }
+    else if( parentElement instanceof Feature )
+    {
+      final Feature f = (Feature) parentElement;
+      return f.getWorkspace();
+    }
+    else if( parentElement instanceof FeatureAssociationTypeElement )
+    {
+      final FeatureAssociationTypeElement fate = (FeatureAssociationTypeElement) parentElement;
+      return fate.getParentFeature().getWorkspace();
+    }
+
+    return null;
+  }
+
+  private KeyWrapper wrapperForProject( final IProject project )
+  {
+    try
+    {
+      final IFile file = project.getFile( "modell.gml" );
+      final KeyWrapper wrapper = getWorkspaceForFile( file );
+      return wrapper;
+    }
+    catch( final Exception e )
+    {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  private KeyWrapper getWorkspaceForFile( final IFile file ) throws MalformedURLException
+  {
+    if( m_workspaces.containsKey( file ) )
+    {
+      final KeyWrapper keyWrapper = m_workspaces.get( file );
+      return keyWrapper;
+    }
+
+    // Workspace was not loaded yet, so start to load it via the pool
+
+    final URL gmlUrl = ResourceUtilities.createURL( file );
+
+    final ResourcePool pool = KalypsoCorePlugin.getDefault().getPool();
+    final PoolableObjectType key = new PoolableObjectType( "gml", gmlUrl.toString(), gmlUrl );
+    pool.addPoolListener( this, key );
+    final KeyWrapper keyWrapper = new KeyWrapper( key, null, new Status( IStatus.INFO, KalypsoGisPlugin.getId(), "wird geladen..." ) );
+    m_workspaces.put( file, keyWrapper );
+    return keyWrapper;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+   */
+  public Object getParent( final Object element )
+  {
+    return null;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+   */
+  public boolean hasChildren( final Object parentElement )
+  {
+    if( parentElement instanceof IProject )
+    {
+      final IProject project = (IProject) parentElement;
+      final IFile file = project.getFile( "modell.gml" );
+      return file.exists();
+    }
+    else if( parentElement instanceof Feature )
+    {
+      return getChildren( parentElement ).length > 0;
+    }
+    else if( parentElement instanceof FeatureAssociationTypeElement )
+    {
+      return getChildren( parentElement ).length > 0;
+    }
+
+    return false;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+   */
+  public Object[] getElements( final Object inputElement )
+  {
+    return new Object[] { "Hallo Gernot" };
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+   */
+  public void dispose( )
+  {
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object,
+   *      java.lang.Object)
+   */
+  public void inputChanged( final Viewer viewer, final Object oldInput, final Object newInput )
+  {
+    m_viewer = viewer;
+  }
+
+  /**
+   * @see org.eclipse.ui.navigator.IMementoAware#restoreState(org.eclipse.ui.IMemento)
+   */
+  public void restoreState( final IMemento aMemento )
+  {
+  }
+
+  /**
+   * @see org.eclipse.ui.navigator.IMementoAware#saveState(org.eclipse.ui.IMemento)
+   */
+  public void saveState( final IMemento aMemento )
+  {
+  }
+
+  /**
+   * @see org.kalypso.util.pool.IPoolListener#dirtyChanged(org.kalypso.util.pool.IPoolableObjectType, boolean)
+   */
+  public void dirtyChanged( final IPoolableObjectType key, final boolean isDirty )
+  {
+  }
+
+  /**
+   * @see org.kalypso.util.pool.IPoolListener#isDisposed()
+   */
+  public boolean isDisposed( )
+  {
+    return false;
+  }
+
+  /**
+   * @see org.kalypso.util.pool.IPoolListener#objectInvalid(org.kalypso.util.pool.IPoolableObjectType, java.lang.Object)
+   */
+  public void objectInvalid( IPoolableObjectType key, Object oldValue )
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+  /**
+   * @see org.kalypso.util.pool.IPoolListener#objectLoaded(org.kalypso.util.pool.IPoolableObjectType, java.lang.Object,
+   *      org.eclipse.core.runtime.IStatus)
+   */
+  public void objectLoaded( final IPoolableObjectType key, final Object newValue, final IStatus status )
+  {
+    for( final Map.Entry<IFile, KeyWrapper> entry : m_workspaces.entrySet() )
+    {
+      final IFile file = entry.getKey();
+      final KeyWrapper wrapper = entry.getValue();
+      if( wrapper.getKey().equals( key ) )
+      {
+        m_workspaces.put( file, new KeyWrapper( key, (GMLWorkspace) newValue, status ) );
+        ViewerUtilities.refresh( m_viewer, true );
+        return;
+      }
+    }
+  }
+
+}
---
+++ KalypsoSimulationUI/src/org/kalypso/simulation/ui/wizards/exporter/ExporterHelper.java
@@ -0,0 +1,233 @@
+/*--------------- Kalypso-Header --------------------------------------------------------------------
+
+ This file is part of kalypso.
+ Copyright (C) 2004, 2005 by:
+
+ Technical University Hamburg-Harburg (TUHH)
+ Institute of River and Coastal Engineering
+ Denickestr. 22
+ 21073 Hamburg, Germany
+ http://www.tuhh.de/wb
+
+ and
+
+ Bjoernsen Consulting Engineers (BCE)
+ Maria Trost 3
+ 56070 Koblenz, Germany
+ http://www.bjoernsen.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ Contact:
+
+ E-Mail:
+ belger@bjoernsen.de
+ schlienger@bjoernsen.de
+ v.doemming@tuhh.de
+
+ ---------------------------------------------------------------------------------------------------*/
+package org.kalypso.simulation.ui.wizards.exporter;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Properties;
+import java.util.Map.Entry;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.kalypso.commons.arguments.Arguments;
+import org.kalypso.contribs.java.net.UrlResolverSingleton;
+import org.kalypso.gmlschema.property.IPropertyType;
+import org.kalypso.simulation.ui.KalypsoSimulationUIPlugin;
+import org.kalypso.zml.obslink.TimeseriesLinkType;
+import org.kalypsodeegree.model.feature.Feature;
+
+/**
+ * Helps with the handling of exporter related operations.
+ * 
+ * @author belger
+ * @author schlienger
+ */
+public final class ExporterHelper
+{
+  public static final String MSG_TOKEN_NOT_FOUND = "Token not found";
+
+  private ExporterHelper( )
+  {
+    // not intended to be instanciated
+  }
+
+  /**
+   * Creates a replace-tokens properties list. For each token, it fetches the value of its associated feature-property.
+   * 
+   * @param feature
+   *          feature from which to get properties' values
+   * @param tokens
+   *          list of name-property pair
+   */
+  public static Properties createReplaceTokens( final Feature feature, final Arguments tokens )
+  {
+    final Properties replacetokens = new Properties();
+    for( final Entry<String, Object> entry : tokens.entrySet() )
+    {
+      final String tokenname = entry.getKey();
+      final String featureProperty = (String) entry.getValue();
+
+      final IPropertyType pt = feature.getFeatureType().getProperty( featureProperty );
+      final Object property = pt == null ? null : feature.getProperty( pt );
+      String replace = null;
+      if( property instanceof TimeseriesLinkType )
+        replace = ((TimeseriesLinkType) property).getHref();
+      else if( property != null )
+        replace = property.toString();
+      else if( property == null )
+        replace = "!" + MSG_TOKEN_NOT_FOUND + ": " + tokenname + "!";
+
+      if( replace != null )
+        replacetokens.setProperty( tokenname, replace );
+    }
+
+    return replacetokens;
+  }
+
+  /**
+   * @return list of argument-values whose associated name begins with the given prefix
+   */
+  public static String[] getArgumentValues( final String prefix, final Arguments args )
+  {
+    final List<String> values = new ArrayList<String>();
+
+    for( final Entry<String, Object> entry : args.entrySet() )
+    {
+      final String argName = entry.getKey();
+
+      if( argName.startsWith( prefix ) )
+      {
+        final String argValue = (String) entry.getValue();
+        values.add( argValue );
+      }
+    }
+
+    return values.toArray( new String[values.size()] );
+  }
+
+  /**
+   * Creates an {@link UrlArgument}for each argument which begins with 'argumentPrefix'. Each such Argument should at
+   * least have the following sub-arguments:
+   * <ul>
+   * <li>'label'
+   * <li>'href'
+   * </ul>
+   * <p>
+   * Other sub-arguments are simply passed to the newly created UrlArgument so that they can later be retrieved using
+   * UrlArgument.getProperty(...).
+   * 
+   * @return list of Url items found in the arguments that we got from the supplier (ex: ExportResultsWizardPage)
+   */
+  public static UrlArgument[] createUrlItems( final String argumentPrefix, final Arguments arguments, final URL context ) throws CoreException
+  {
+    final Collection<UrlArgument> items = new ArrayList<UrlArgument>();
+    final Collection<IStatus> stati = new ArrayList<IStatus>();
+
+    for( final Entry<String, Object> entry : arguments.entrySet() )
+    {
+      final String key = entry.getKey();
+      if( key.startsWith( argumentPrefix ) )
+      {
+        final Arguments args = (Arguments) entry.getValue();
+        final String label = args.getProperty( "label", "<unbekannt>" );
+        final String strFile = args.getProperty( "href" );
+        if( strFile == null )
+          stati.add( new Status( IStatus.WARNING, KalypsoSimulationUIPlugin.getID(), 0, "Keine Datei-Angabe für: " + key, null ) );
+
+        try
+        {
+          items.add( new UrlArgument( label, UrlResolverSingleton.resolveUrl( context, strFile ), args ) );
+        }
+        catch( final MalformedURLException e )
+        {
+          stati.add( new Status( IStatus.WARNING, KalypsoSimulationUIPlugin.getID(), 0, "Ungültiger Pfad: " + strFile, e ) );
+        }
+      }
+    }
+
+    if( stati.size() > 0 )
+      throw new CoreException( new MultiStatus( KalypsoSimulationUIPlugin.getID(), 0, stati.toArray( new IStatus[stati.size()] ), "Siehe Details", null ) );
+
+    return items.toArray( new UrlArgument[items.size()] );
+  }
+
+  /**
+   * Denotes the items which are displayed in the template selection wizard page
+   * 
+   * @author schlienger
+   */
+  public static class UrlArgument
+  {
+    private String m_label;
+
+    private final URL m_templateUrl;
+
+    private final Arguments m_args;
+
+    public UrlArgument( final String label, final URL templateUrl, final Arguments args )
+    {
+      m_label = label;
+      m_templateUrl = templateUrl;
+      m_args = args;
+    }
+
+    public String getLabel( )
+    {
+      return m_label;
+    }
+
+    public URL getUrl( )
+    {
+      return m_templateUrl;
+    }
+
+    public String getProperty( final String key )
+    {
+      return m_args.getProperty( key );
+    }
+
+    public String getProperty( final String key, final String defaultValue )
+    {
+      return m_args.getProperty( key, defaultValue );
+    }
+
+    @Override
+    public String toString( )
+    {
+      return m_label;
+    }
+
+    public Arguments getArguments( )
+    {
+      return m_args;
+    }
+
+    public void setLabel( final String name )
+    {
+      m_label = name;
+    }
+  }
+}
