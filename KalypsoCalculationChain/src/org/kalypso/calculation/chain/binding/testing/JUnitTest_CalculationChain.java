3bb965a58da0bffb5009be6fb1f2e15a45de687b
adejan <adejan@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-03-27 13:34:30 +0000

    Splitting calculation chain and connectors, dependency problem fixed

--- KalypsoCalculationChain/src/org/kalypso/calculation/chain/binding/testing/JUnitTest_CalculationChain.java
+++ KalypsoCalculationChain/src/org/kalypso/calculation/chain/binding/testing/JUnitTest_CalculationChain.java
@@ -15,8 +15,6 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.kalypso.calculation.chain.CalculationChainMemberJobSpecification;
 import org.kalypso.calculation.chain.CalculationChainRunnable;
-import org.kalypso.calculation.chain.IKalypsoModelConnectorType.MODELSPEC_CONNECTOR_NA_WSPM;
-import org.kalypso.calculation.chain.IKalypsoModelConnectorType.MODEL_CONNECTOR_TYPEID;
 import org.kalypso.contribs.eclipse.jface.operation.RunnableContextHelper;
 
 public class JUnitTest_CalculationChain extends TestCase
@@ -38,13 +36,13 @@ public class JUnitTest_CalculationChain extends TestCase
      * NA - WSPM connector; defines custom model data specification
      */
     final IProject calcCaseWSPM = ResourcesPlugin.getWorkspace().getRoot().getProject( "02-Kollau-1D-PlanerClient" );
-    final CalculationChainMemberJobSpecification jobSpecification_NA_WSPM = new CalculationChainMemberJobSpecification( MODEL_CONNECTOR_TYPEID.CONNECTOR_NA_WSPM.getValue(), calcCaseWSPM );
-    jobSpecification_NA_WSPM.addInput( MODELSPEC_CONNECTOR_NA_WSPM.NA_Model.name(), "platform:/resource//01-Kollau-NA-PlanerClient/modell.gml", false );
-    jobSpecification_NA_WSPM.addInput( MODELSPEC_CONNECTOR_NA_WSPM.NA_StatisticalReport.name(), "platform:/resource//01-Kollau-NA-PlanerClient/Rechenvarianten/kz-2002_10_26/Ergebnisse/Aktuell/Reports/nodesMax.zml", false );
-    jobSpecification_NA_WSPM.addInput( MODELSPEC_CONNECTOR_NA_WSPM.NA_RiverCode.name(), "" );
-    jobSpecification_NA_WSPM.addInput( MODELSPEC_CONNECTOR_NA_WSPM.WSPM_RunoffEventID.name(), "RunOffEvent1234888262265697" );
-    jobSpecification_NA_WSPM.addInput( MODELSPEC_CONNECTOR_NA_WSPM.WSPM_Model.name(), "modell.gml", true );
-    jobSpecification_NA_WSPM.addOutput( MODELSPEC_CONNECTOR_NA_WSPM.WSPM_Model.name(), "modell.gml", true );
+    final CalculationChainMemberJobSpecification jobSpecification_NA_WSPM = new CalculationChainMemberJobSpecification( "KalypsoModelConnector_NA_WSPM", calcCaseWSPM );
+    jobSpecification_NA_WSPM.addInput( "NA_Model", "platform:/resource//01-Kollau-NA-PlanerClient/modell.gml", false );
+    jobSpecification_NA_WSPM.addInput( "NA_StatisticalReport", "platform:/resource//01-Kollau-NA-PlanerClient/Rechenvarianten/kz-2002_10_26/Ergebnisse/Aktuell/Reports/nodesMax.zml", false );
+    jobSpecification_NA_WSPM.addInput( "NA_RiverCode", "" );
+    jobSpecification_NA_WSPM.addInput( "WSPM_RunoffEventID", "RunOffEvent1234888262265697" );
+    jobSpecification_NA_WSPM.addInput( "WSPM_Model", "modell.gml", true );
+    jobSpecification_NA_WSPM.addOutput( "WSPM_Model", "modell.gml", true );
     jobSpecificationList.add( jobSpecification_NA_WSPM );
 
     /*
@@ -61,7 +59,7 @@ public class JUnitTest_CalculationChain extends TestCase
      * WSPM - FM connector; defines custom model data specification
      */
     final IContainer calcCaseFM = ResourcesPlugin.getWorkspace().getRoot().getProject( "FM_FloodDemo" ).getFolder( "Basis" );
-    final CalculationChainMemberJobSpecification jobSpecification_WSPM_FM = new CalculationChainMemberJobSpecification( MODEL_CONNECTOR_TYPEID.CONNECTOR_WSPM_FLOOD.getValue(), calcCaseFM );
+    final CalculationChainMemberJobSpecification jobSpecification_WSPM_FM = new CalculationChainMemberJobSpecification( "KalypsoModelConnector_WSPM_FM", calcCaseFM );
     jobSpecification_WSPM_FM.addInput( "WSPM_TinFile", "platform:/resource//02-Kollau-1D-PlanerClient/Ergebnisse/kollau-ist-HQ_5_neu/_aktuell/Daten/WspTin.gml", false );
     jobSpecification_WSPM_FM.addInput( "WSPM_TinReference", "platform:/resource//02-Kollau-1D-PlanerClient/Ergebnisse/kollau-ist-HQ_5_neu/_aktuell/Daten/WspTin.gml" );
     jobSpecification_WSPM_FM.addInput( "FM_Model", "models/flood.gml", true );
@@ -79,7 +77,7 @@ public class JUnitTest_CalculationChain extends TestCase
      * FM - RM connector; defines custom model data specification
      */
     final IContainer calcCaseRM = ResourcesPlugin.getWorkspace().getRoot().getProject( "RM_C01" ).getFolder( "Basis" );
-    final CalculationChainMemberJobSpecification jobSpecification_FM_RM = new CalculationChainMemberJobSpecification( MODEL_CONNECTOR_TYPEID.CONNECTOR_FLOOD_RISK.getValue(), calcCaseRM );
+    final CalculationChainMemberJobSpecification jobSpecification_FM_RM = new CalculationChainMemberJobSpecification( "KalypsoModelConnector_FM_RM", calcCaseRM );
     jobSpecification_FM_RM.addInput( "FM_Model", "platform:/resource//FM_FloodDemo/Basis/models/flood.gml", false );
     jobSpecification_FM_RM.addInput( "FM_EventsFolder", "platform:/resource//FM_FloodDemo/Basis/events", false );
     jobSpecification_FM_RM.addInput( "RM_Model", "models/RasterDataModel.gml", true );
