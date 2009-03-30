d2cf8da2336592d4a473e78fde39269916c5a7e4
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-03-30 07:50:41 +0000

    Small WSPM fix

--- EclipseRCPContributions/src/org/kalypso/contribs/eclipse/ui/partlistener/AdapterPartListener.java
+++ EclipseRCPContributions/src/org/kalypso/contribs/eclipse/ui/partlistener/AdapterPartListener.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.contribs.eclipse.ui.partlistener;
 
@@ -63,7 +63,7 @@ import org.eclipse.ui.IWorkbenchPartReference;
  * What to do when the curent part we are adapting to is closed.
  * </p>
  * </p>
- * 
+ *
  * @author Gernot Belger
  */
 public class AdapterPartListener<C> implements IPartListener2
@@ -147,6 +147,7 @@ public class AdapterPartListener<C> implements IPartListener2
    */
   public void partOpened( final IWorkbenchPartReference partRef )
   {
+    adaptPartReference( partRef );
   }
 
   /**
@@ -172,7 +173,7 @@ public class AdapterPartListener<C> implements IPartListener2
 
   /**
    * Tries to find the adapter for the given part. If one is found, return true.
-   * 
+   *
    * @return If an adapter for this partRef is found return true, false otherwise.
    */
   public boolean adaptPartReference( final IWorkbenchPartReference partRef )
