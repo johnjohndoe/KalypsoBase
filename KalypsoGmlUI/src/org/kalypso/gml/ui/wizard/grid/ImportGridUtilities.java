221453814895ffbb74c95eba8a1ab48bfbc07a53
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-04-16 09:42:25 +0000

    Fixed: created relativ pathes when importing grids into flood (and others). We now try more aggressively to make the path relative.

--- KalypsoGmlUI/src/org/kalypso/gml/ui/wizard/grid/ImportGridUtilities.java
+++ KalypsoGmlUI/src/org/kalypso/gml/ui/wizard/grid/ImportGridUtilities.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- * 
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- * 
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.gml.ui.wizard.grid;
 
@@ -50,7 +50,9 @@ import java.util.List;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -63,6 +65,7 @@ import org.eclipse.ui.dialogs.IOverwriteQuery;
 import org.eclipse.ui.wizards.datatransfer.FileSystemStructureProvider;
 import org.eclipse.ui.wizards.datatransfer.ImportOperation;
 import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
+import org.kalypso.contribs.eclipse.core.runtime.PathUtils;
 import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
 import org.kalypso.contribs.eclipse.ui.progress.ProgressUtilities;
 import org.kalypso.contribs.java.lang.NumberUtils;
@@ -239,12 +242,19 @@ public class ImportGridUtilities
     final IFile contextFile = ResourceUtilities.findFileFromURL( context );
     if( contextFile != null )
     {
-      // Most common case, grid file in same folder as gml file
-      if( contextFile.getParent().equals( gridFile.getParent() ) )
-        return gridFile.getName();
+      final IPath relativePath = PathUtils.makeRelativ( contextFile.getFullPath().removeLastSegments( 1 ), gridFile.getFullPath() );
+      if( relativePath != null )
+        return relativePath.toPortableString();
     }
 
-    // TODO: make file path relative to gml path
+    final IFolder contextFolder = ResourceUtilities.findFolderFromURL( context );
+    if( contextFolder != null )
+    {
+      final IPath relativePath = PathUtils.makeRelativ( contextFolder.getFullPath(), gridFile.getFullPath() );
+      if( relativePath != null )
+        return relativePath.toPortableString();
+    }
+
     final URL outputGridUrl = ResourceUtilities.createURL( gridFile );
     return outputGridUrl.toExternalForm();
   }
@@ -287,7 +297,7 @@ public class ImportGridUtilities
 
   /**
    * TODO: this method should not be necessary: the meta-reader should return the domain and nothing else.
-   * 
+   *
    * @see org.kalypso.grid.IGridMetaReader#getDomain(java.lang.String)
    */
   public static RectifiedGridDomain getDomain( final IGridMetaReader reader, final String crs ) throws Exception
