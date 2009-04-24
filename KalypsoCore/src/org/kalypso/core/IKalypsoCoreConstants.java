2254fc82728ab69a8bcb24cfa395c2bea44a492e
holgeralbert <holgeralbert@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-04-24 15:54:29 +0000

    - removed resource lock marker constant and extension point from kalypso core

--- KalypsoCore/src/org/kalypso/core/IKalypsoCoreConstants.java
+++ KalypsoCore/src/org/kalypso/core/IKalypsoCoreConstants.java
@@ -46,9 +46,10 @@ package org.kalypso.core;
 public interface IKalypsoCoreConstants
 {
   public final static String PLUGIN_ID = "org.kalypso.core"; //$NON-NLS-1$
-  
-    /** System property dfrom which the locations of the kalypso-client.ini is read. If this property is set, it overwrites (and sets) the user preferences. */
+
+  /**
+   * System property from which the locations of the kalypso-client.ini is read. If this property is set, it overwrites
+   * (and sets) the user preferences.
+   */
   public static final String CONFIG_PROPERTY_CLIENT_INI_LOCATION = "kalypso.client-ini-locations";
-    
-  public static final String RESOURCE_LOCK_MARKER_TYPE = IKalypsoCoreConstants.PLUGIN_ID + ".resourcePoolLockMarker"; //$NON-NLS-1$
 }
