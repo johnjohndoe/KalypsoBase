b932326560aaf696d980b9b3994d2c29de7af50b
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-05-01 18:06:13 +0000

    Huge(!) performance improvement for feature-themes that get filtered via sld using <Literal>

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/filterencoding/Literal.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/filterencoding/Literal.java
@@ -15,16 +15,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * history:
- * 
+ *
  * Files in this package are originally taken from deegree and modified here
  * to fit in kalypso. As goals of kalypso differ from that one in deegree
- * interface-compatibility to deegree is wanted but not retained always. 
- * 
- * If you intend to use this software in other ways than in kalypso 
+ * interface-compatibility to deegree is wanted but not retained always.
+ *
+ * If you intend to use this software in other ways than in kalypso
  * (e.g. OGC-web services), you should consider the latest version of deegree,
  * see http://www.deegree.org .
  *
- * all modifications are licensed as deegree, 
+ * all modifications are licensed as deegree,
  * original copyright:
  *
  * Copyright (C) 2001 by:
@@ -43,29 +43,30 @@ import org.w3c.dom.Element;
 
 /**
  * Encapsulates the information of a <Literal>element as defined in the FeatureId DTD.
- * 
+ *
  * @author Markus Schneider
  * @version 07.08.2002
  */
 public class Literal extends Expression_Impl
 {
-
   /** The literal's value. */
-  String m_value;
+  private String m_value;
+
+  private Double m_number;
 
   /** Constructs a new Literal. */
   public Literal( final String value )
   {
     m_id = ExpressionDefines.LITERAL;
-    m_value = value;
+    setValue( value );
   }
 
   /**
-   * Given a DOM-fragment, a corresponding Expression-object is built. This method recursively calls other buildFromDOM () -
-   * methods to validate the structure of the DOM-fragment.
-   * 
+   * Given a DOM-fragment, a corresponding Expression-object is built. This method recursively calls other buildFromDOM
+   * () - methods to validate the structure of the DOM-fragment.
+   *
    * @throws FilterConstructionException
-   *             if the structure of the DOM-fragment is invalid
+   *           if the structure of the DOM-fragment is invalid
    */
   public static Expression buildFromDOM( final Element element ) throws FilterConstructionException
   {
@@ -91,7 +92,15 @@ public class Literal extends Expression_Impl
    */
   public void setValue( final String value )
   {
-    this.m_value = value;
+    m_value = value;
+    try
+    {
+      m_number = new Double( m_value );
+    }
+    catch( final NumberFormatException e )
+    {
+      m_number = null;
+    }
   }
 
   /** Produces an indented XML representation of this object. */
@@ -104,24 +113,19 @@ public class Literal extends Expression_Impl
   }
 
   /**
-   * Returns the <tt>Literal</tt>'s value (to be used in the evaluation of a complexer <tt>Expression</tt>). If
-   * the value appears to be numerical, a <tt>Double</tt> is returned, else a <tt>String</tt>. TODO: Improve
-   * datatype handling.
-   * 
+   * Returns the <tt>Literal</tt>'s value (to be used in the evaluation of a complexer <tt>Expression</tt>). If the
+   * value appears to be numerical, a <tt>Double</tt> is returned, else a <tt>String</tt>. TODO: Improve datatype
+   * handling.
+   *
    * @param feature
-   *            that determines the values of <tt>PropertyNames</tt> in the expression (no use here)
+   *          that determines the values of <tt>PropertyNames</tt> in the expression (no use here)
    * @return the resulting value
    */
   public Object evaluate( final Feature feature )
   {
-    try
-    {
-      return new Double( m_value );
-    }
-    catch( final NumberFormatException e )
-    {
-      //  
-    }
+    if( m_number != null )
+      return m_number;
+
     return m_value;
   }
 }
