3f1426463dc9a5066e0970e802841be555cf7039
nicoschrage <nicoschrage@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-08-17 08:25:04 +0000

    Tagging for Milestone Release of Kalypso version 2.2.0

---
+++ KalypsoGmlProcesses/src/org/kalypso/gml/processes/raster2vector/ringtree/RingTree.java
@@ -0,0 +1,107 @@
+package org.kalypso.gml.processes.raster2vector.ringtree;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.vividsolutions.jts.geom.Coordinate;
+
+/**
+ * @author belger
+ */
+public class RingTree
+{
+  public final RingTreeElement root = new RingTreeElement( null, null, Double.NaN, null, "root" );
+
+  public Object walk( final RingTreeWalker rtw )
+  {
+    // für jedes child was machen
+    walkInternal( rtw, root );
+
+    return rtw.getResult();
+  }
+
+  private void walkInternal( final RingTreeWalker rtw, final RingTreeElement element )
+  {
+    if( element != root )
+      rtw.operate( element );
+
+    for( final RingTreeElement child : element.children() )
+      walkInternal( rtw, child );
+  }
+
+  public void insertElement( final RingTreeElement rte )
+  {
+    // rekursiv durch den Baum suchen und das Element an der richtigen Stelle einbauen
+    insertInternal( root, rte );
+  }
+
+  private void insertInternal( final RingTreeElement target, final RingTreeElement element )
+  {
+    // für jedes Kind checken, ob element drin liegt, falls ja dort einfügen
+    final Coordinate c = element.innerCrd;
+
+    if( c == null )
+    {
+      final double area = element.polygon.getArea();
+      if( area == 0 )
+      {
+        System.out.println( "Fläche ist 0.0" );
+      }
+      return;
+    }
+
+    for( final RingTreeElement child : target.children() )
+    {
+      if( child.pir.isInside( c ) && child.contains( element ) )
+      {
+        if( element.contains( child ) )
+        {
+          // child and element have the same geometry
+
+          final double elementValue = element.lineValue;
+          final double childValue = child.lineValue;
+          // Keep the one with the bigger value: this makes sense, as segments are generated
+          // for all borders with values smaller then the actual cell value for edges with Double.NaN
+          if( elementValue > childValue )
+          {
+            // keep element: get children from child and replace child with element
+            for( final RingTreeElement childChild : child.children() )
+              element.addChild( childChild );
+
+            target.addChild( element );
+            target.removeChild( child );
+          }
+          else
+          {
+            // keep child: nothing to do, just ignore element
+          }
+        }
+        else
+        {
+          insertInternal( child, element );
+        }
+
+        return;
+      }
+    }
+
+    // wenns nirgends reinpasst schaun, welche children in das element gehören
+    final Collection<RingTreeElement> removeElements = new ArrayList<RingTreeElement>();
+    for( final RingTreeElement child : target.children() )
+    {
+      if( element.pir.isInside( child.innerCrd ) && element.contains( child ) )
+      {
+        removeElements.add( child );
+        element.addChild( child );
+      }
+    }
+
+    // und diese elemente löschen
+    for( final RingTreeElement removeChild : removeElements )
+      target.removeChild( removeChild );
+
+    // und das element selbst hinzufügen
+    target.addChild( element );
+  }
+
+}
