52a8f412e632381ad93982e879fc0fe710cf07d9
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2009-08-20 11:24:40 +0000

    KalypsoRRM bugfix: After changing relation (EditRelationWidget), the map was not always properly updated.

--- KalypsoUI/src/org/kalypso/ui/editor/gmleditor/util/command/AddRelationCommand.java
+++ KalypsoUI/src/org/kalypso/ui/editor/gmleditor/util/command/AddRelationCommand.java
@@ -1,13 +1,3 @@
-package org.kalypso.ui.editor.gmleditor.util.command;
-
-import org.kalypso.commons.command.ICommand;
-import org.kalypso.gmlschema.property.relation.IRelationType;
-import org.kalypso.ogc.gml.command.FeatureChange;
-import org.kalypso.ogc.gml.command.FeatureChangeModellEvent;
-import org.kalypsodeegree.model.feature.Feature;
-import org.kalypsodeegree.model.feature.GMLWorkspace;
-import org.kalypsodeegree.model.feature.event.FeatureStructureChangeModellEvent;
-
 /*----------------    FILE HEADER KALYPSO ------------------------------------------
  *
  *  This file is part of kalypso.
@@ -48,6 +38,16 @@ import org.kalypsodeegree.model.feature.event.FeatureStructureChangeModellEvent;
  *  v.doemming@tuhh.de
  *
  *  ---------------------------------------------------------------------------*/
+package org.kalypso.ui.editor.gmleditor.util.command;
+
+import org.kalypso.commons.command.ICommand;
+import org.kalypso.gmlschema.property.relation.IRelationType;
+import org.kalypso.ogc.gml.command.FeatureChange;
+import org.kalypso.ogc.gml.command.FeatureChangeModellEvent;
+import org.kalypsodeegree.model.feature.Feature;
+import org.kalypsodeegree.model.feature.GMLWorkspace;
+import org.kalypsodeegree.model.feature.event.FeatureStructureChangeModellEvent;
+
 
 public class AddRelationCommand implements ICommand
 {
