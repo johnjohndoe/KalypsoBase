fd9c1f460bcc1808e13e7857a4103421147e7a0c
dirk_kuch <dirk_kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-02-02 14:45:45 +0000

    branch of kalypso base for the hvz prognose project

---
+++ JavaApiContributions/src/org/kalypso/contribs/java/swing/table/ExcelClipboardAdapter.java
@@ -0,0 +1,211 @@
+/*--------------- Kalypso-Header --------------------------------------------------------------------
+
+ This file is part of kalypso.
+ Copyright (C) 2004, 2005 by:
+
+ Technical University Hamburg-Harburg (TUHH)
+ Institute of River and coastal engineering
+ Denickestr. 22
+ 21073 Hamburg, Germany
+ http://www.tuhh.de/wb
+
+ and
+ 
+ Bjoernsen Consulting Engineers (BCE)
+ Maria Trost 3
+ 56070 Koblenz, Germany
+ http://www.bjoernsen.de
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ Contact:
+
+ E-Mail:
+ belger@bjoernsen.de
+ schlienger@bjoernsen.de
+ v.doemming@tuhh.de
+ 
+ ---------------------------------------------------------------------------------------------------*/
+package org.kalypso.contribs.java.swing.table;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.text.NumberFormat;
+import java.util.StringTokenizer;
+
+import javax.swing.AbstractAction;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.KeyStroke;
+
+/**
+ * ExcelAdapter enables Copy-Paste Clipboard functionality on JTables. The clipboard data format used by the adapter is
+ * compatible with the clipboard format used by Excel. This provides for clipboard interoperability between enabled
+ * JTables and Excel.
+ * <p>
+ * This code is adapted from the Online Article at {see http://www.javaworld.com/javaworld/javatips/jw-javatip77.html}
+ */
+public final class ExcelClipboardAdapter
+{
+  public final static String CMD_COPY = "COPY";
+
+  public final static String CMD_PASTE = "PASTE";
+
+  protected final JTable m_table;
+
+  protected final NumberFormat m_nf;
+
+  private final KeyStroke m_ksCopy;
+
+  private final KeyStroke m_ksPaste;
+
+  private final CopyAction m_copyAction;
+
+  private final PasteAction m_pasteAction;
+
+  /**
+   * The Excel Adapter is constructed with a JTable on which it enables Copy-Paste and acts as a Clipboard listener.
+   */
+  public ExcelClipboardAdapter( final JTable table, final NumberFormat nf )
+  {
+    m_table = table;
+    m_nf = nf;
+
+    m_ksCopy = KeyStroke.getKeyStroke( KeyEvent.VK_C, ActionEvent.CTRL_MASK, false );
+
+    m_ksPaste = KeyStroke.getKeyStroke( KeyEvent.VK_V, ActionEvent.CTRL_MASK, false );
+
+    m_table.getInputMap().put( m_ksCopy, CMD_COPY );
+    m_copyAction = new CopyAction();
+    m_table.getActionMap().put( CMD_COPY, m_copyAction );
+    m_table.getInputMap().put( m_ksPaste, CMD_PASTE );
+    m_pasteAction = new PasteAction();
+    m_table.getActionMap().put( CMD_PASTE, m_pasteAction );
+  }
+
+  public void dispose()
+  {
+    m_table.getInputMap().remove( m_ksCopy );
+    m_table.getActionMap().remove( CMD_COPY );
+    m_table.getInputMap().remove( m_ksPaste );
+    m_table.getActionMap().remove( CMD_PASTE );
+  }
+
+  public CopyAction getCopyAction()
+  {
+    return m_copyAction;
+  }
+
+  public PasteAction getPasteAction()
+  {
+    return m_pasteAction;
+  }
+
+  private class CopyAction extends AbstractAction
+  {
+    public CopyAction()
+    {
+      super( "Kopieren" );
+    }
+
+    public void actionPerformed( final ActionEvent e )
+    {
+      // Check to ensure we have selected only a contiguous block of cells
+      final int numcols = m_table.getSelectedColumnCount();
+      final int numrows = m_table.getSelectedRowCount();
+      final int[] rowsselected = m_table.getSelectedRows();
+      final int[] colsselected = m_table.getSelectedColumns();
+      if( !( ( numrows - 1 == rowsselected[rowsselected.length - 1] - rowsselected[0] && numrows == rowsselected.length ) && ( numcols - 1 == colsselected[colsselected.length - 1]
+          - colsselected[0] && numcols == colsselected.length ) ) )
+      {
+        JOptionPane.showMessageDialog( null, "Ungültige Copy Selektion", "Ungültige Copy Selektion",
+            JOptionPane.ERROR_MESSAGE );
+        return;
+      }
+
+      final StringBuffer sbf = new StringBuffer();
+      for( int i = 0; i < numrows; i++ )
+      {
+        for( int j = 0; j < numcols; j++ )
+        {
+          final Object value = m_table.getValueAt( rowsselected[i], colsselected[j] );
+          sbf.append( m_nf.format( value ) );
+
+          if( j < numcols - 1 )
+            sbf.append( "\t" );
+        }
+
+        sbf.append( "\n" );
+      }
+
+      final StringSelection stsel = new StringSelection( sbf.toString() );
+      Toolkit.getDefaultToolkit().getSystemClipboard().setContents( stsel, stsel );
+    }
+  }
+
+  private class PasteAction extends AbstractAction
+  {
+    public PasteAction()
+    {
+      super( "Einfügen" );
+    }
+
+    public void actionPerformed( final ActionEvent e )
+    {
+      final int startRow = ( m_table.getSelectedRows() )[0];
+      final int startCol = ( m_table.getSelectedColumns() )[0];
+      try
+      {
+        final String trstring = (String)( Toolkit.getDefaultToolkit().getSystemClipboard().getContents( this )
+            .getTransferData( DataFlavor.stringFlavor ) );
+
+        final StringTokenizer st1 = new StringTokenizer( trstring, "\n" );
+
+        for( int i = 0; st1.hasMoreTokens(); i++ )
+        {
+          final String rowstring = st1.nextToken();
+
+          final StringTokenizer st2 = new StringTokenizer( rowstring, "\t" );
+          for( int j = 0; st2.hasMoreTokens(); j++ )
+          {
+            final String nextToken = st2.nextToken();
+
+            final int rowIndex = startRow + i;
+            if( rowIndex < m_table.getRowCount() && startCol + j < m_table.getColumnCount() )
+            {
+              final int columnIndex = startCol + j;
+              if( m_table.isCellEditable( rowIndex, columnIndex ) )
+              {
+                final Object value = m_nf.parseObject( nextToken );
+                m_table.setValueAt( value, rowIndex, columnIndex );
+              }
+            }
+          }
+        }
+      }
+      catch( final Exception ex )
+      {
+        ex.printStackTrace();
+      }
+      finally
+      {
+        m_table.repaint();
+      }
+    }
+  }
+}
---
+++ JavaApiContributions/src/org/kalypso/contribs/javax/xml/namespace/QNameUtilities.java
@@ -0,0 +1,181 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.contribs.javax.xml.namespace;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.namespace.QName;
+
+/**
+ * @author Gernot Belger
+ */
+public class QNameUtilities
+{
+  static private HashMap<String, Long> m_mLocalIDs = new HashMap<String, Long>();
+  static private long  m_iNextFullID = 0;
+  
+  static HashMap<String, Long> m_mFullIDs = new HashMap<String, Long>();
+  static private long m_iNextLocalID = 0;
+  
+  
+  private QNameUtilities( )
+  {
+    // never instantiate
+  }
+
+  public synchronized static long getFullID( final QName name )
+  {
+    if (name == null)
+      return 0;
+    
+    final String l_sName = name.toString();
+    if (m_mFullIDs.containsKey( l_sName ) ) {
+      return m_mFullIDs.get( l_sName );
+    }
+    m_iNextFullID++;
+    final long id = m_iNextFullID;
+    m_mFullIDs.put( l_sName, id );
+    return id;
+  }
+
+  public static long getLocalID( final QName name )
+  {
+    if (name == null)
+      return 0;
+
+    final String l_sName = name.getLocalPart().toString();
+    if (m_mLocalIDs.containsKey( l_sName ) ) {
+      return m_mLocalIDs.get( l_sName );
+    }
+    m_iNextLocalID++;
+    final long id = m_iNextLocalID;
+    m_mLocalIDs.put( l_sName, id );
+    return id;
+  }
+
+  
+  public static boolean equals( final QName qname, final String namespace, final String localPart )
+  {
+    return qname.equals( new QName( namespace, localPart ) );
+  }
+
+  /**
+   * Tests if two qnames are equal in respect to their local part.
+   */
+  public static boolean equalsLocal( final QName name1, final QName name2 )
+  {
+    final String local1 = name1 == null ? null : name1.getLocalPart();
+    final String local2 = name2 == null ? null : name2.getLocalPart();
+    if( local1 == null )
+      return local2 == null;
+
+    return local1.equals( local2 );
+  }
+
+  /**
+   * syntax of fragmentedFullQName :
+   * 
+   * <pre>
+   *         &lt;namespace&gt;#&lt;localpart&gt;
+   * </pre>
+   * 
+   * example: fragmentedFullQName = www.w3c.org#index.html<br/>
+   * If no '#' is given, a qname with only a localPart is created.
+   * 
+   * @return qname from fragmentedFullQName
+   */
+  public static QName createQName( final String fragmentedFullQName )
+  {
+    final String[] parts = fragmentedFullQName.split( "#" );
+    if( parts.length == 2 )
+      return new QName( parts[0], parts[1] );
+
+    return QName.valueOf( fragmentedFullQName );
+  }
+
+  public static QName[] createQNames( final String fragmentedFullQNameList, final String separator )
+  {
+    final ArrayList<QName> allQNames = new ArrayList<QName>();
+    final String[] qNameStrings = fragmentedFullQNameList.split( separator );
+    for( final String s : qNameStrings )
+    {
+      allQNames.add( createQName( s ) );
+    }
+    return allQNames.toArray( new QName[allQNames.size()] );
+  }
+
+  /**
+   * Parses a {@link QName} from a string in xml-syntax using a prefix resolver. <br/> Syntax of the qname:
+   * 
+   * <pre>
+   *    prefix:localPart
+   * </pre>
+   * 
+   * <br/> For backwards compability, also the {@link QName#toString()} form (i.e. {namespace}localPart) form is
+   * recognized.<br/> If either the prefix is empty or the namespaceContext is null, returns a qname with empty local
+   * part (due to XML-Spec).
+   */
+  public static QName createQName( final String condition, final NamespaceContext namespaceContext )
+  {
+    // REMARK: First, check if it is formatted like {namespaceUri}localPart (which is not regular, but still recognized
+    // for backwards compability
+    if( condition.contains( "{" ) )
+      return QName.valueOf( condition );
+
+    final String[] split = condition.split( ":" );
+    if( split.length == 0 || split.length > 2 )
+      return null;
+
+    if( split.length == 1 )
+      return new QName( condition );
+
+    final String prefix = split[0];
+    final String localPart = split[1];
+
+    if( prefix == null || prefix.length() == 0 || namespaceContext == null )
+      return new QName( localPart );
+
+    final String namespaceUri = namespaceContext.getNamespaceURI( prefix );
+    return new QName( namespaceUri, localPart );
+  }
+}
