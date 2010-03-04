f2ee608fa36716e08e82b0229fb349b1cfed2700
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-03-04 07:33:56 +0000

    Branch für KalypsoSachsen - Mulde-Release im November 2009

---
+++ KalypsoGMLSchema/src/org/kalypso/gmlschema/builder/RelationType2ComplexTypeBuilder.java
@@ -0,0 +1,77 @@
+/*
+ * --------------- Kalypso-Header --------------------------------------------------------------------
+ * 
+ * This file is part of kalypso. Copyright (C) 2004, 2005 by:
+ * 
+ * Technical University Hamburg-Harburg (TUHH) Institute of River and coastal engineering Denickestr. 22 21073 Hamburg,
+ * Germany http://www.tuhh.de/wb
+ * 
+ * and
+ * 
+ * Bjoernsen Consulting Engineers (BCE) Maria Trost 3 56070 Koblenz, Germany http://www.bjoernsen.de
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact:
+ * 
+ * E-Mail: belger@bjoernsen.de schlienger@bjoernsen.de v.doemming@tuhh.de
+ * 
+ * ---------------------------------------------------------------------------------------------------
+ */
+package org.kalypso.gmlschema.builder;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.ComplexType;
+import org.apache.xmlbeans.impl.xb.xsdschema.Element;
+import org.kalypso.gmlschema.GMLSchema;
+import org.kalypso.gmlschema.GMLSchemaException;
+import org.kalypso.gmlschema.GMLSchemaUtilities;
+import org.kalypso.gmlschema.property.relation.IRelationType;
+import org.kalypso.gmlschema.xml.ComplexTypeReference;
+import org.kalypso.gmlschema.xml.QualifiedElement;
+
+/**
+ * another builder
+ * 
+ * @author doemming
+ */
+public class RelationType2ComplexTypeBuilder implements IBuilder
+{
+  /**
+   * @see org.kalypso.gmlschema.builder.IBuilder#build(org.kalypso.gmlschema.GMLSchema, java.lang.Object)
+   */
+  public Object[] build( final GMLSchema gmlSchema, final Object relationTypeObject ) throws GMLSchemaException
+  {
+    final QualifiedElement qe = (QualifiedElement) relationTypeObject;
+    final Element element = qe.getElement();
+    final ComplexTypeReference complexTypeReference = GMLSchemaUtilities.getComplexTypeReferenceFor( gmlSchema, element );
+    final ComplexType complexType = complexTypeReference.getComplexType();
+    gmlSchema.register( qe, complexType );
+    return new Object[] { complexType };
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.builder.IBuilder#isBuilderFor(org.kalypso.gmlschema.GMLSchema, java.lang.Object,
+   *      java.lang.String)
+   */
+  public boolean isBuilderFor( final GMLSchema gmlSchema, final Object object, final String namedPass )
+  {
+    return object instanceof IRelationType;
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.builder.IBuilder#replaces(org.kalypso.gmlschema.builder.IBuilder)
+   */
+  public boolean replaces( final IBuilder other )
+  {
+    return false;
+  }
+}
