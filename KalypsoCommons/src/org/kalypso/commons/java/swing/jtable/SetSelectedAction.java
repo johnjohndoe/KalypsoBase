65d982a57841d3b024bf72ec3e7ff8455ede9b21
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-03-04 07:22:25 +0000

    Branch für KalypsoSachsen - Mulde-Release im November 2009

---
+++ KalypsoCommons/src/org/kalypso/commons/java/swing/jtable/SetSelectedAction.java
@@ -0,0 +1,33 @@
+package org.kalypso.commons.java.swing.jtable;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JTable;
+
+import org.kalypso.commons.i18n.Messages;
+
+/**
+ * SetAllAction
+ *
+ * @author schlienger
+ */
+public class SetSelectedAction extends AbstractObservationTableAction
+{
+  public SetSelectedAction( final JTable table )
+  {
+    super( table, Messages.getString("org.kalypso.commons.java.swing.jtable.SetSelectedAction.0"), Messages.getString("org.kalypso.commons.java.swing.jtable.SetSelectedAction.1") ); //$NON-NLS-1$ //$NON-NLS-2$
+  }
+
+  @Override
+  public void internalActionPerformed( final ActionEvent e )
+  {
+    final JTable table = getTable();
+    final int col = table.getSelectedColumn();
+    final int row = table.getSelectedRow();
+    final int[] rows = table.getSelectedRows();
+    final Object value = table.getValueAt( row, col );
+
+    for( final int row2 : rows )
+      table.setValueAt( value, row2, col );
+  }
+}
