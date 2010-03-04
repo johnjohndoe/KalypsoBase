d42ea9ea7bc94f6a60043a4d681d601850251139
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-03-04 07:28:43 +0000

    Branch für KalypsoSachsen - Mulde-Release im November 2009

---
+++ OdysseusChartFramework/src/de/openali/odysseus/chart/framework/model/data/IDataOperator.java
@@ -0,0 +1,23 @@
+package de.openali.odysseus.chart.framework.model.data;
+
+import java.text.Format;
+import java.util.Comparator;
+
+public interface IDataOperator<T_logical> extends IStringDataConverter<T_logical>
+{
+
+  public Number logicalToNumeric( T_logical logVal );
+
+  public T_logical numericToLogical( Number numVal );
+
+  // public IDataRange<T_logical> getContainingInterval( T_logical logVal, Number numIntervalWidth, T_logical
+  // logFixedPoint );
+
+  public Comparator<T_logical> getComparator( );
+
+  /**
+   * returns a format to format data values as strings; the format shall try to adapt itself to the given range
+   */
+  public Format getFormat( IDataRange<Number> range );
+
+}
