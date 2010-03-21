ccd60ae760e9b5592454636aa3e225d08b1d9244
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-03-21 16:10:47 +0000

    Fixed: Picture theme throws exception in constructor if image cannot be found. Setting status instead...

--- KalypsoCore/src/org/kalypso/ogc/gml/KalypsoPictureThemeWorldFile.java
+++ KalypsoCore/src/org/kalypso/ogc/gml/KalypsoPictureThemeWorldFile.java
@@ -46,11 +46,11 @@ import javax.media.jai.JAI;
 import javax.media.jai.RenderedOp;
 import javax.media.jai.TiledImage;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.kalypso.commons.i18n.I10nString;
 import org.kalypso.contribs.eclipse.core.runtime.StatusUtilities;
 import org.kalypso.contribs.java.net.UrlResolverSingleton;
+import org.kalypso.core.KalypsoCorePlugin;
 import org.kalypso.core.i18n.Messages;
 import org.kalypso.grid.GridFileVerifier;
 import org.kalypso.grid.IGridMetaReader;
@@ -68,7 +68,7 @@ import org.kalypsodeegree_impl.model.geometry.GeometryFactory;
  */
 public class KalypsoPictureThemeWorldFile extends KalypsoPictureTheme
 {
-  public KalypsoPictureThemeWorldFile( final I10nString layerName, final StyledLayerType layerType, final URL context, final IMapModell modell, final String system ) throws CoreException
+  public KalypsoPictureThemeWorldFile( final I10nString layerName, final StyledLayerType layerType, final URL context, final IMapModell modell, final String system ) 
   {
     super( layerName, layerType, context, modell );
 
@@ -76,43 +76,52 @@ public class KalypsoPictureThemeWorldFile extends KalypsoPictureTheme
     if( href == null || !href.contains( "." ) ) //$NON-NLS-1$
       throw new IllegalStateException();
 
+    // FIXME: the second argument is never read...!
     final String[] arrFileName = href.split( "#" ); //$NON-NLS-1$
     if( arrFileName.length != 2 )
       throw new IllegalStateException();
 
+    RenderedOp image = null; 
     try
     {
-      final URL imageUrl = UrlResolverSingleton.resolveUrl( context, arrFileName[0] );
+      final String relativeURL = arrFileName[0];
+      final String srsName = arrFileName[1];
+      
+      // UGLY HACK: replace backslashes with slashes. The add-picture-theme action seems to put backslashes (on windows)
+      // in the relative URLs (which is even wrong in windows). Should be fixed there, but is fixed also here to support older projects.
+      final String relativeURLchecked = relativeURL.replaceAll( "\\\\", "/" );
+      
+      final URL imageUrl = UrlResolverSingleton.resolveUrl( context, relativeURLchecked );
 
       if( GridFileVerifier.verify( imageUrl ) )
       {
-        final IGridMetaReader reader = GridFileVerifier.getRasterMetaReader( imageUrl, system );
+        final IGridMetaReader reader = GridFileVerifier.getRasterMetaReader( imageUrl, null );
 
-        final RenderedOp image = JAI.create( "url", imageUrl ); //$NON-NLS-1$
+        image = JAI.create( "url", imageUrl ); //$NON-NLS-1$
         setImage( new TiledImage( image, true ) );
 
-        // TODO: the image keeps does not release the stream onto the tiff
-        // maybe we must call image.dispose in order to do this?
-        // can we do that here??
         final int height = getImage().getHeight();
         final int width = getImage().getWidth();
-
-        final GM_Point origin = GeometryFactory.createGM_Point( new Double( reader.getOriginCornerX() ), new Double( reader.getOriginCornerY() ), system );
+        final GM_Point origin = GeometryFactory.createGM_Point( reader.getOriginCornerX(), reader.getOriginCornerY(), srsName );
 
         final OffsetVector offsetX = new OffsetVector( new Double( reader.getVectorXx() ), new Double( reader.getVectorXy() ) );
         final OffsetVector offsetY = new OffsetVector( new Double( reader.getVectorYx() ), new Double( reader.getVectorYy() ) );
         final GridRange gridRange = new GridRange_Impl( new double[] { 0, 0 }, new double[] { width, height } );
 
         setRectifiedGridDomain( new RectifiedGridDomain( origin, offsetX, offsetY, gridRange ) );
-
-        image.dispose();
       }
     }
     catch( final Exception e )
     {
-      e.printStackTrace();
       final IStatus status = StatusUtilities.createStatus( IStatus.ERROR, Messages.getString("org.kalypso.ogc.gml.KalypsoPictureThemeWorldFile.0"), e ); //$NON-NLS-1$
-      throw new CoreException( status );
+      KalypsoCorePlugin.getDefault().getLog().log( status );
+      setStatus( status );
+      // We cannot throw exceptions here, it will break the whole map. This holds for all themes...
+    }
+    finally
+    {
+      if( image != null )
+        image.dispose();
     }
   }
 }
