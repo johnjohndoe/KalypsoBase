d559ced67e2712b178c76852983ad3b02ef5db5d
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-04-29 17:08:55 +0000

    Tag for 2.2 stable release.

---
+++ OdysseusServiceODS/src/de/openali/odysseus/service/ods/environment/IODSEnvironment.java
@@ -0,0 +1,35 @@
+package de.openali.odysseus.service.ods.environment;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.Status;
+
+import de.openali.odysseus.service.ows.extension.IOWSOperation;
+
+public interface IODSEnvironment
+{
+
+	public ODSConfigurationLoader getConfigLoader();
+
+	public Status getStatus();
+
+	public Map<String, List<IODSChart>> getScenes();
+
+	public String getDefaultSceneId();
+
+	public IOWSOperation[] getOperations();
+
+	public String getServiceUrl();
+
+	public File getTmpDir();
+
+	public File getConfigDir();
+
+	boolean validateChartId(String sceneId, String chartId);
+
+	boolean validateSceneId(String sceneId);
+
+	boolean validateLayerId(String sceneId, String chartId, String layerId);
+}
