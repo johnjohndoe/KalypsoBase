6ceff321e6a626257b464978e54844f00b9fd3fa
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-05-05 12:05:35 +0000

    some cleanups

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/io/sax/parser/GMLPropertyChoiceContentHandler.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/io/sax/parser/GMLPropertyChoiceContentHandler.java
@@ -48,7 +48,6 @@ import javax.xml.namespace.QName;
 import org.kalypso.gmlschema.KalypsoGMLSchemaPlugin;
 import org.kalypso.gmlschema.property.IPropertyMarshallingTypeHandler;
 import org.kalypso.gmlschema.types.IValueHandler;
-import org.kalypsodeegree_impl.tools.GMLConstants;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
@@ -56,83 +55,82 @@ import org.xml.sax.SAXParseException;
 import org.xml.sax.XMLReader;
 
 /**
- * When a feature can be specified using more than one property, this content handler can be set as delegate, and
- * the properties that can specify the feature must be registered. 
- * This content handler then delegates the parsing to a child content handler that
- * parses an specific sub-element.   
+ * When a feature can be specified using more than one property, this content handler can be set as delegate, and the
+ * properties that can specify the feature must be registered. This content handler then delegates the parsing to a
+ * child content handler that parses an specific sub-element.
  * 
  * @author Felipe Maximino
  */
 public class GMLPropertyChoiceContentHandler extends DelegatingContentHandler
 {
-  private Map<QName, ContentHandler> m_registeredProperties;
-  
-  private String m_defaultSrs;
-  
-  public GMLPropertyChoiceContentHandler( ContentHandler parentContentHandler, final XMLReader xmlReader, final String defaultSrs )
+  private final Map<QName, ContentHandler> m_registeredProperties;
+
+  private final String m_defaultSrs;
+
+  public GMLPropertyChoiceContentHandler( final ContentHandler parentContentHandler, final XMLReader xmlReader, final String defaultSrs )
   {
     super( xmlReader, parentContentHandler );
-    
+
     m_registeredProperties = new HashMap<QName, ContentHandler>();
     m_defaultSrs = defaultSrs;
   }
-  
+
   @Override
   public void endElement( final String uri, final String localName, final String name ) throws SAXException
-  { 
-    GMLElementContentHandler parentContentHandler = (GMLElementContentHandler) getParentContentHandler();
-    
+  {
+    final GMLElementContentHandler parentContentHandler = (GMLElementContentHandler) getParentContentHandler();
+
     if( parentContentHandler.getLocalName().equals( localName ) )
     {
       endDelegation();
-      parentContentHandler.endElement( uri, localName, name );      
+      parentContentHandler.endElement( uri, localName, name );
     }
   }
 
   @Override
   public void startElement( final String uri, final String localName, final String name, final Attributes atts ) throws SAXException
-  { 
-    ContentHandler delegate = findDelegate( new QName( uri, localName ) );
+  {
+    final ContentHandler delegate = findDelegate( new QName( uri, localName ) );
 
     if( delegate != null )
     {
       delegate( delegate );
-      delegate.startElement( uri, localName, name, atts );      
-    }    
+      delegate.startElement( uri, localName, name, atts );
+    }
     else
     {
       throw new SAXParseException( String.format( "Unexpected start element: %s - %s -  %s", uri, localName, name ), getLocator() );
     }
   }
-  
+
   private ContentHandler findDelegate( final QName qName )
   {
     return m_registeredProperties.get( qName );
   }
-  
+
   private void registerProperty( final QName qName, final ContentHandler contentHandler )
   {
     m_registeredProperties.put( qName, contentHandler );
   }
-  
+
   /**
    * Loads the properties that can specify the geometry given as parameter
    */
-  public void loadPropertiesFor( QName geometry ) throws SAXParseException
+  public void loadPropertiesFor( final QName geometry ) throws SAXParseException
   {
     try
     {
-      IPropertyMarshallingTypeHandler[] allowedProps = KalypsoGMLSchemaPlugin.getDefault().getAllowedProperties( geometry );
-      
-      for( IPropertyMarshallingTypeHandler handler : allowedProps )
-      { 
+      final IPropertyMarshallingTypeHandler[] allowedProps = KalypsoGMLSchemaPlugin.getDefault().getAllowedProperties( geometry );
+
+      for( final IPropertyMarshallingTypeHandler handler : allowedProps )
+      {
         registerProperty( handler.getTypeName(), handler.createContentHandler( m_xmlReader, this, (IValueHandler) m_parentContentHandler, m_defaultSrs ) );
       }
-    } 
-    catch ( Exception e )
+    }
+    catch( final Exception e )
     {
       e.printStackTrace();
-      throw new SAXParseException( "Could not get properties for " + geometry.toString() +  ": " + e.getMessage(), m_locator );
+      throw new SAXParseException( "Could not get properties for " + geometry.toString() + ": " + e.getMessage(), m_locator );
     }
   }
 }
