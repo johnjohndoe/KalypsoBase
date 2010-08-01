86d796f09996b6718be945b9d62c8207f7f31d6b
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-08-01 16:05:27 +0000

    Helper classes for sax parsing.

--- JavaApiContributions/src/org/kalypso/contribs/org/xml/sax/DelegateContentHandler.java
+++ JavaApiContributions/src/org/kalypso/contribs/org/xml/sax/DelegateContentHandler.java
@@ -40,19 +40,21 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.contribs.org.xml.sax;
 
+import org.eclipse.core.runtime.Assert;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
 
 /**
- * A {@link ContentHandler} implementation witch delegates all calls to a delegate.
+ * A content handler implementation, that delegates all call to another {@link ContentHandler}.<br/>
+ * The delegate must not be <code>null</code>, once this content handler is being used.
  * 
  * @author Gernot Belger
  */
 public class DelegateContentHandler implements ContentHandler
 {
-  private ContentHandler m_delegate;
+  private ContentHandler m_delegate = null;
 
   private Locator m_locator;
 
@@ -63,15 +65,7 @@ public class DelegateContentHandler implements ContentHandler
 
   public DelegateContentHandler( final ContentHandler delegate )
   {
-    setDelegate( delegate );
-  }
-
-  protected void setDelegate( final ContentHandler delegate )
-  {
     m_delegate = delegate;
-
-    if( m_delegate != null )
-      m_delegate.setDocumentLocator( m_locator );
   }
 
   public ContentHandler getDelegate( )
@@ -79,74 +73,12 @@ public class DelegateContentHandler implements ContentHandler
     return m_delegate;
   }
 
-  protected boolean isDelegating( )
+  public void setDelegate( final ContentHandler delegate )
   {
-    return m_delegate != null;
-  }
+    m_delegate = delegate;
 
-  public Locator getLocator( )
-  {
-    return m_locator;
-  }
-
-  /**
-   * @see org.xml.sax.ContentHandler#characters(char[], int, int)
-   */
-  @Override
-  public void characters( final char[] ch, final int start, final int length ) throws SAXException
-  {
-    if( m_delegate != null )
-      m_delegate.characters( ch, start, length );
-  }
-
-  /**
-   * @see org.xml.sax.ContentHandler#endDocument()
-   */
-  @Override
-  public void endDocument( ) throws SAXException
-  {
-    if( m_delegate != null )
-      m_delegate.endDocument();
-  }
-
-  /**
-   * @see org.xml.sax.ContentHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
-   */
-  @Override
-  public void endElement( final String uri, final String localName, final String name ) throws SAXException
-  {
-    if( m_delegate != null )
-      m_delegate.endElement( uri, localName, name );
-  }
-
-  /**
-   * @see org.xml.sax.ContentHandler#endPrefixMapping(java.lang.String)
-   */
-  @Override
-  public void endPrefixMapping( final String prefix ) throws SAXException
-  {
-    if( m_delegate != null )
-      m_delegate.endPrefixMapping( prefix );
-  }
-
-  /**
-   * @see org.xml.sax.ContentHandler#ignorableWhitespace(char[], int, int)
-   */
-  @Override
-  public void ignorableWhitespace( final char[] ch, final int start, final int length ) throws SAXException
-  {
-    if( m_delegate != null )
-      m_delegate.ignorableWhitespace( ch, start, length );
-  }
-
-  /**
-   * @see org.xml.sax.ContentHandler#processingInstruction(java.lang.String, java.lang.String)
-   */
-  @Override
-  public void processingInstruction( final String target, final String data ) throws SAXException
-  {
-    if( m_delegate != null )
-      m_delegate.processingInstruction( target, data );
+    if( m_locator != null )
+      m_delegate.setDocumentLocator( m_locator );
   }
 
   /**
@@ -155,21 +87,15 @@ public class DelegateContentHandler implements ContentHandler
   @Override
   public void setDocumentLocator( final Locator locator )
   {
-    /* We remember the document locator in order to set it to every new delegate. */
     m_locator = locator;
 
     if( m_delegate != null )
       m_delegate.setDocumentLocator( locator );
   }
 
-  /**
-   * @see org.xml.sax.ContentHandler#skippedEntity(java.lang.String)
-   */
-  @Override
-  public void skippedEntity( final String name ) throws SAXException
+  public Locator getDocumentLocator( )
   {
-    if( m_delegate != null )
-      m_delegate.skippedEntity( name );
+    return m_locator;
   }
 
   /**
@@ -178,19 +104,20 @@ public class DelegateContentHandler implements ContentHandler
   @Override
   public void startDocument( ) throws SAXException
   {
-    if( m_delegate != null )
-      m_delegate.startDocument();
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.startDocument();
   }
 
   /**
-   * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String,
-   *      org.xml.sax.Attributes)
+   * @see org.xml.sax.ContentHandler#endDocument()
    */
   @Override
-  public void startElement( final String uri, final String localName, final String name, final Attributes atts ) throws SAXException
+  public void endDocument( ) throws SAXException
   {
-    if( m_delegate != null )
-      m_delegate.startElement( uri, localName, name, atts );
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.endDocument();
   }
 
   /**
@@ -199,8 +126,87 @@ public class DelegateContentHandler implements ContentHandler
   @Override
   public void startPrefixMapping( final String prefix, final String uri ) throws SAXException
   {
-    if( m_delegate != null )
-      m_delegate.startPrefixMapping( prefix, uri );
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.startPrefixMapping( prefix, uri );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#endPrefixMapping(java.lang.String)
+   */
+  @Override
+  public void endPrefixMapping( final String prefix ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.endPrefixMapping( prefix );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String,
+   *      org.xml.sax.Attributes)
+   */
+  @Override
+  public void startElement( final String uri, final String localName, final String qName, final Attributes atts ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.startElement( uri, localName, qName, atts );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+   */
+  @Override
+  public void endElement( final String uri, final String localName, final String qName ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.endElement( uri, localName, qName );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#characters(char[], int, int)
+   */
+  @Override
+  public void characters( final char[] ch, final int start, final int length ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.characters( ch, start, length );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#ignorableWhitespace(char[], int, int)
+   */
+  @Override
+  public void ignorableWhitespace( final char[] ch, final int start, final int length ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.ignorableWhitespace( ch, start, length );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#processingInstruction(java.lang.String, java.lang.String)
+   */
+  @Override
+  public void processingInstruction( final String target, final String data ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.processingInstruction( target, data );
+  }
+
+  /**
+   * @see org.xml.sax.ContentHandler#skippedEntity(java.lang.String)
+   */
+  @Override
+  public void skippedEntity( final String name ) throws SAXException
+  {
+    Assert.isNotNull( m_delegate );
+
+    m_delegate.skippedEntity( name );
   }
 
 }
