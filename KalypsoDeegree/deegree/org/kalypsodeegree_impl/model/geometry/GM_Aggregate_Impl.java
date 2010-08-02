d3086794ed3db2de265b0f5a937027fe1519cc9f
porien <porien@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-08-02 12:46:15 +0000

    Hydrotop Creation operation - work in progress

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/geometry/GM_Aggregate_Impl.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/geometry/GM_Aggregate_Impl.java
@@ -169,7 +169,7 @@ abstract class GM_Aggregate_Impl extends GM_Object_Impl implements GM_Aggregate,
   @Override
   public void setObjectAt( final GM_Object gmo, final int index ) throws GM_Exception
   {
-    if( (index < 0) || (index > this.getSize() - 1) )
+    if( index < 0 )
     {
       throw new GM_Exception( "invalid index/position: " + index + " to set a geometry!" );
     }
@@ -179,7 +179,9 @@ abstract class GM_Aggregate_Impl extends GM_Object_Impl implements GM_Aggregate,
       throw new GM_Exception( "gmo == null. it isn't possible to set a value" + " that equals null!" );
     }
 
-    m_aggregate.set( index, gmo );
+   ((ArrayList<GM_Object>) m_aggregate).ensureCapacity( index + 1 );
+
+   ((ArrayList<GM_Object>) m_aggregate).add( index, gmo );
 
     invalidate();
   }
