df44c158c2e7ebea916491f3a877b14e6238ed4c
holgeralbert <holgeralbert@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-08-06 16:00:26 +0000

    - prognose view now can contain heavy graphics

--- EclipseRCPContributions/src/org/kalypso/contribs/eclipse/swt/graphics/FontUtilities.java
+++ EclipseRCPContributions/src/org/kalypso/contribs/eclipse/swt/graphics/FontUtilities.java
@@ -40,8 +40,10 @@
  ---------------------------------------------------------------------------------------------------*/
 package org.kalypso.contribs.eclipse.swt.graphics;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.graphics.Font;
@@ -57,24 +59,67 @@ public class FontUtilities
 {
   private final Collection<Font> m_disposeFonts = new LinkedList<Font>();
 
-  public void dispose()
+  public void dispose( )
   {
     for( final Font font : m_disposeFonts )
       font.dispose();
   }
 
-  public Font createChangedFontData( final FontData[] fontData, final int heightOffset, final int styleOffset,
-      final Device device )
+  public Font createChangedFontData( final FontData[] fontData, final int heightOffset, final int styleOffset, final Device device )
   {
-    for( final FontData element : fontData )
-    {
-      element.setHeight( element.getHeight() + heightOffset );
-      element.setStyle( element.getStyle() | styleOffset );
-    }
-
-    final Font font = new Font( device, fontData );
+    Font font = changeHeightAndStyle( device, fontData, heightOffset, styleOffset );
     m_disposeFonts.add( font );
-
     return font;
+  }
+
+  /**
+   * This function copies the font and sets a new size and adds the style constants.
+   * 
+   * @param device
+   * @param fontData
+   *          The original font data.
+   * @param heightOffset
+   * @param style
+   *          The style constants of the original font and these will be set to the new font.
+   * @return The new font.
+   */
+  public static Font changeHeightAndStyle( Device device, FontData[] fontData, int heightOffset, int style )
+  {
+    /* The new font data. */
+    List<FontData> newFontData = new ArrayList<FontData>();
+
+    /* Create the new font data. */
+    for( FontData data : fontData )
+      newFontData.add( new FontData( data.getName(), data.getHeight() + heightOffset, data.getStyle() | style ) );
+
+    /* Create and return the new font. */
+    return new Font( device, newFontData.toArray( new FontData[] {} ) );
+  }
+
+  /**
+   * This function copies the font and sets a new size and adds the style constants.
+   * 
+   * @param device
+   *          The device.
+   * @param font
+   *          The original font.
+   * @param height
+   *          The height of the new font.
+   * @param style
+   *          The style constants of the original font and these will be set to the new font.
+   * @return The new font.
+   */
+  public static Font changeHeightAndStyle( Device device, Font font, int height, int style )
+  {
+    /* The new font data. */
+    List<FontData> fontData = new ArrayList<FontData>();
+
+    /* Create the new font data. */
+    FontData[] elements = font.getFontData();
+    for( FontData element : elements )
+      fontData.add( new FontData( element.getName(), height, element.getStyle() | style ) );
+
+    /* Create and return the new font. */
+    return new Font( device, fontData.toArray( new FontData[] {} ) );
   }
 }
