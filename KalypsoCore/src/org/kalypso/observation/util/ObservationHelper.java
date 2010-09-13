7a3fd6834c6a6d900e972b3a22ff232740f7aed5
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-09-13 17:52:33 +0000

    Tag for 2.3 RC1 Release

---
+++ KalypsoCore/src/org/kalypso/observation/util/ObservationHelper.java
@@ -0,0 +1,146 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ *
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ *
+ *  and
+ *
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Contact:
+ *
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.observation.util;
+
+import java.io.ByteArrayInputStream;
+import java.util.Calendar;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.kalypso.ogc.sensor.IAxis;
+import org.kalypso.ogc.sensor.IObservation;
+import org.kalypso.ogc.sensor.SensorException;
+import org.kalypso.ogc.sensor.impl.SimpleObservation;
+import org.kalypso.ogc.sensor.impl.SimpleTupleModel;
+import org.kalypso.ogc.sensor.metadata.MetadataList;
+import org.kalypso.ogc.sensor.request.ObservationRequest;
+import org.kalypso.ogc.sensor.zml.ZmlFactory;
+import org.xml.sax.InputSource;
+
+/**
+ * @author Dirk Kuch
+ */
+public final class ObservationHelper
+{
+  public static final int HISTORY = -40;
+
+  private ObservationHelper( )
+  {
+  }
+
+  public static byte[] flushMetaDataToByteArray( final IObservation observation ) throws SensorException
+  {
+    final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    try
+    {
+      /* serialize observation to xml (atm we need only the metadata) */
+      final IAxis[] axes = observation.getAxisList();
+      final String href = observation.getHref();
+      final String name = observation.getName();
+      final MetadataList metadataList = observation.getMetadataList();
+
+      final SimpleObservation simple = new SimpleObservation( href, name, metadataList, axes );
+      ZmlFactory.writeToStream( simple, outputStream, null );
+
+      return outputStream.toByteArray();
+
+    }
+    finally
+    {
+      IOUtils.closeQuietly( outputStream );
+    }
+  }
+
+  public static byte[] flushToByteArray( final IObservation observation ) throws SensorException
+  {
+    final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    try
+    {
+      ZmlFactory.writeToStream( observation, outputStream, null );
+
+      return outputStream.toByteArray();
+    }
+    finally
+    {
+      IOUtils.closeQuietly( outputStream );
+    }
+  }
+
+  public static ObservationRequest getDefaultRequest( )
+  {
+    final Calendar from = Calendar.getInstance();
+    final Calendar to = Calendar.getInstance();
+
+    from.add( Calendar.DAY_OF_MONTH, HISTORY );
+    to.add( Calendar.DAY_OF_MONTH, 1 );
+
+    return new ObservationRequest( from.getTime(), to.getTime() );
+  }
+
+  public static SimpleObservation getSimpleObservation( final SimpleTupleModel model, final IObservation baseObservation )
+  {
+    final String href = baseObservation.getHref();
+    final String name = baseObservation.getName();
+    final MetadataList metadataList = baseObservation.getMetadataList();
+
+    return new SimpleObservation( href, name, metadataList, model );
+  }
+
+  public static IObservation parseFromByteArray( final byte[] buffer ) throws SensorException
+  {
+    final ByteArrayInputStream stream = new ByteArrayInputStream( buffer );
+    try
+    {
+      final InputSource source = new InputSource( stream );
+      return ZmlFactory.parseXML( source, null );
+    }
+    finally
+    {
+      IOUtils.closeQuietly( stream );
+    }
+  }
+
+  public static IObservation clone( final IObservation observation ) throws SensorException
+  {
+    final byte[] byteArray = ObservationHelper.flushToByteArray( observation );
+    return ObservationHelper.parseFromByteArray( byteArray );
+  }
+}
---
+++ KalypsoCore/src/org/kalypso/ogc/sensor/adapter/NativeObservationGrapAdapter.java
@@ -0,0 +1,207 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.ogc.sensor.adapter;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.kalypso.core.i18n.Messages;
+import org.kalypso.ogc.sensor.IAxis;
+import org.kalypso.ogc.sensor.IObservation;
+import org.kalypso.ogc.sensor.ITupleModel;
+import org.kalypso.ogc.sensor.impl.DefaultAxis;
+import org.kalypso.ogc.sensor.impl.SimpleObservation;
+import org.kalypso.ogc.sensor.impl.SimpleTupleModel;
+import org.kalypso.ogc.sensor.metadata.ITimeseriesConstants;
+import org.kalypso.ogc.sensor.metadata.MetadataList;
+import org.kalypso.ogc.sensor.timeseries.TimeserieUtils;
+
+/**
+ * @author doemming
+ */
+public class NativeObservationGrapAdapter implements INativeObservationAdapter
+{
+  private final DateFormat m_grapDateFormat = new SimpleDateFormat( "dd MM yyyy HH mm ss" ); //$NON-NLS-1$
+
+  public static Pattern GRAP_PATTERN = Pattern.compile( "([0-9]{1,2}.+?[0-9]{1,2}.+?[0-9]{2,4}.+?[0-9]{1,2}.+?[0-9]{1,2}.[0-9 ]{1,2})(.*-?[0-9\\.]+)" ); //$NON-NLS-1$
+
+  private String m_title;
+
+  private String m_axisTypeValue;
+
+  private final int MAX_NO_OF_ERRORS = 30;
+
+  /**
+   * @see org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime.IConfigurationElement,
+   *      java.lang.String, java.lang.Object)
+   */
+  @Override
+  public void setInitializationData( final IConfigurationElement config, final String propertyName, final Object data )
+  {
+    m_title = config.getAttribute( "label" ); //$NON-NLS-1$
+    m_axisTypeValue = config.getAttribute( "axisType" ); //$NON-NLS-1$
+  }
+
+  @Override
+  public IObservation createObservationFromSource( final File source ) throws Exception
+  {
+    return createObservationFromSource( source, null, true );
+  }
+
+  @Override
+  public IObservation createObservationFromSource( final File source, TimeZone timeZone, final boolean continueWithErrors ) throws Exception
+  {
+    final MetadataList metaDataList = new MetadataList();
+
+    /* this is due to backwards compatibility */
+    if( timeZone == null )
+      timeZone = TimeZone.getTimeZone( "GMT+1" ); //$NON-NLS-1$
+
+    m_grapDateFormat.setTimeZone( timeZone );
+    // create axis
+    final IAxis[] axis = createAxis();
+    final ITupleModel tuppelModel = createTuppelModel( source, axis, continueWithErrors );
+    final SimpleObservation observation = new SimpleObservation( "href", "titel", metaDataList, tuppelModel ); //$NON-NLS-1$ //$NON-NLS-2$ 
+    return observation;
+  }
+
+  private ITupleModel createTuppelModel( final File source, final IAxis[] axis, final boolean continueWithErrors ) throws IOException
+  {
+
+    int numberOfErrors = 0;
+
+    final StringBuffer errorBuffer = new StringBuffer();
+    final FileReader fileReader = new FileReader( source );
+    final LineNumberReader reader = new LineNumberReader( fileReader );
+    final List<Date> dateCollector = new ArrayList<Date>();
+    final List<Double> valueCollector = new ArrayList<Double>();
+    String lineIn = null;
+    while( (lineIn = reader.readLine()) != null )
+    {
+      if( !continueWithErrors && (numberOfErrors > MAX_NO_OF_ERRORS) )
+        return null;
+      try
+      {
+        final Matcher matcher = GRAP_PATTERN.matcher( lineIn );
+        if( matcher.matches() )
+        {
+          final String dateString = matcher.group( 1 );
+          final Double value = new Double( matcher.group( 2 ) );
+
+          final String formatedDate = dateString.replaceAll( "[:\\.]", " " ); //$NON-NLS-1$ //$NON-NLS-2$
+          final Pattern datePattern = Pattern.compile( "([0-9 ]{2}) ([0-9 ]{2}) ([0-9]{4}) ([0-9 ]{2}) ([0-9 ]{2}) ([0-9 ]{2})" ); //$NON-NLS-1$
+          final Matcher dateMatcher = datePattern.matcher( formatedDate );
+          if( dateMatcher.matches() )
+          {
+            final StringBuffer buffer = new StringBuffer();
+            for( int i = 1; i <= dateMatcher.groupCount(); i++ )
+            {
+              if( i > 1 )
+                buffer.append( " " ); // separator //$NON-NLS-1$
+              buffer.append( dateMatcher.group( i ).replaceAll( " ", "0" ) ); // //$NON-NLS-1$ //$NON-NLS-2$
+              // correct
+              // empty
+              // fields
+            }
+            final String correctDate = buffer.toString();
+            final Date date = m_grapDateFormat.parse( correctDate );
+            dateCollector.add( date );
+            valueCollector.add( value );
+          }
+          else
+          {
+            errorBuffer.append( Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.14" ) + reader.getLineNumber() + Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.15" ) + lineIn + "\"\n" ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+            numberOfErrors++;
+          }
+        }
+        else
+        {
+          errorBuffer.append( Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.17" ) + reader.getLineNumber() + Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.18" ) + lineIn + "\"\n" ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+          numberOfErrors++;
+        }
+      }
+      catch( final Exception e )
+      {
+        errorBuffer.append( Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.20" ) + reader.getLineNumber() + Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.21" ) + e.getLocalizedMessage() + "\"\n" ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        numberOfErrors++;
+      }
+    }
+    final Object[][] tupelData = new Object[dateCollector.size()][2];
+    for( int i = 0; i < dateCollector.size(); i++ )
+    {
+      tupelData[i][0] = dateCollector.get( i );
+      tupelData[i][1] = valueCollector.get( i );
+    }
+    // TODO handle error
+    System.out.println( errorBuffer.toString() );
+    return new SimpleTupleModel( axis, tupelData );
+  }
+
+  @Override
+  public String toString( )
+  {
+    return m_title;
+  }
+
+  /**
+   * @see org.kalypso.ogc.sensor.adapter.INativeObservationAdapter#createAxis()
+   */
+  @Override
+  public IAxis[] createAxis( )
+  {
+    final IAxis dateAxis = new DefaultAxis( Messages.getString( "org.kalypso.ogc.sensor.adapter.NativeObservationGrapAdapter.23" ), ITimeseriesConstants.TYPE_DATE, "", Date.class, true ); //$NON-NLS-1$ //$NON-NLS-2$
+    TimeserieUtils.getUnit( m_axisTypeValue );
+    final IAxis valueAxis = new DefaultAxis( TimeserieUtils.getName( m_axisTypeValue ), m_axisTypeValue, TimeserieUtils.getUnit( m_axisTypeValue ), Double.class, false );
+    final IAxis[] axis = new IAxis[] { dateAxis, valueAxis };
+    return axis;
+  }
+}
