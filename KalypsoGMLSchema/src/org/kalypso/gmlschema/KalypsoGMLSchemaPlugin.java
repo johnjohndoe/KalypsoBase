12142b1f40986b9d5346558e78f9204b2129a6a3
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-09-13 17:53:29 +0000

    Tag for 2.3 RC1 Release

---
+++ KalypsoGMLSchema/src/org/kalypso/gmlschema/KalypsoGMLSchemaPlugin.java
@@ -0,0 +1,70 @@
+package org.kalypso.gmlschema;
+
+import javax.xml.namespace.QName;
+
+import org.eclipse.core.runtime.Plugin;
+import org.kalypso.contribs.java.JavaApiContributionsExtension;
+import org.kalypso.contribs.java.net.IUrlCatalog;
+import org.kalypso.gmlschema.property.IPropertyMarshallingTypeHandler;
+import org.osgi.framework.BundleContext;
+
+public class KalypsoGMLSchemaPlugin extends Plugin
+{
+  // The shared instance.
+  private static KalypsoGMLSchemaPlugin INSTANCE;
+
+  private GMLSchemaCatalog m_schemaCatalog = null;
+
+  public KalypsoGMLSchemaPlugin( )
+  {
+    super();
+
+    INSTANCE = this;
+  }
+
+  /**
+   * This method is called upon plug-in activation
+   */
+  @Override
+  public void start( final BundleContext context ) throws Exception
+  {
+    super.start( context );
+  }
+
+  /**
+   * This method is called when the plug-in is stopped
+   */
+  @Override
+  public void stop( final BundleContext context ) throws Exception
+  {
+    super.stop( context );
+
+    INSTANCE = null;
+    m_schemaCatalog = null;
+  }
+
+  public synchronized GMLSchemaCatalog getSchemaCatalog( )
+  {
+    if( m_schemaCatalog == null )
+    {
+      final IUrlCatalog theCatalog = JavaApiContributionsExtension.getAllRegisteredCatalogs();
+      m_schemaCatalog = new GMLSchemaCatalog( theCatalog );
+    }
+
+    return m_schemaCatalog;
+  }
+  
+  public synchronized IPropertyMarshallingTypeHandler[] getAllowedProperties( QName geometry ) throws Exception
+  {
+    return KalypsoGmlSchemaExtensions.getPropertiesHandlersForGeometry( geometry );
+  }  
+  
+  /**
+   * Returns the shared instance.
+   */
+  public static KalypsoGMLSchemaPlugin getDefault( )
+  {
+    return INSTANCE;
+  }
+
+}
---
+++ KalypsoGMLSchema/src/org/kalypso/gmlschema/property/relation/RelationContentTypeFromExtension.java
@@ -0,0 +1,125 @@
+/*
+ * --------------- Kalypso-Header --------------------------------------------------------------------
+ * 
+ * This file is part of kalypso. Copyright (C) 2004, 2005 by:
+ * 
+ * Technical University Hamburg-Harburg (TUHH) Institute of River and coastal engineering Denickestr. 22 21073 Hamburg,
+ * Germany http://www.tuhh.de/wb
+ * 
+ * and
+ * 
+ * Bjoernsen Consulting Engineers (BCE) Maria Trost 3 56070 Koblenz, Germany http://www.bjoernsen.de
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact:
+ * 
+ * E-Mail: belger@bjoernsen.de schlienger@bjoernsen.de v.doemming@tuhh.de
+ * 
+ * ---------------------------------------------------------------------------------------------------
+ */
+package org.kalypso.gmlschema.property.relation;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.impl.xb.xsdschema.AttributeGroupRef;
+import org.apache.xmlbeans.impl.xb.xsdschema.ComplexType;
+import org.apache.xmlbeans.impl.xb.xsdschema.ExtensionType;
+import org.kalypso.gmlschema.GMLSchema;
+import org.kalypso.gmlschema.GMLSchemaException;
+import org.kalypso.gmlschema.GMLSchemaUtilities;
+import org.kalypso.gmlschema.builder.IInitialize;
+import org.kalypso.gmlschema.xml.ComplexTypeReference;
+import org.kalypso.gmlschema.xml.ElementWithOccurs;
+
+/**
+ * representation of a feature content definition from xml schema that is defined by extension.
+ * 
+ * @author doemming
+ */
+public class RelationContentTypeFromExtension extends RelationContentType
+{
+  private final ExtensionType m_extension;
+
+  private final List<ElementWithOccurs> m_elements;
+
+  private AttributeGroupRef[] m_attributeGroupArray;
+
+  public RelationContentTypeFromExtension( final GMLSchema schema, final ComplexType complexType, final ExtensionType extension ) throws GMLSchemaException
+  {
+    super( schema, complexType );
+    m_extension = extension;
+    m_elements = GMLSchemaUtilities.collectElements( schema, extension, null, null );
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.feature.FeatureContentType#getSequence()
+   */
+  @Override
+  public List<ElementWithOccurs> getSequence( )
+  {
+    return m_elements;
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.basics.IInitialize#init(int)
+   */
+  @Override
+  public void init( final int initializeRun ) throws GMLSchemaException
+  {
+    switch( initializeRun )
+    {
+      case IInitialize.INITIALIZE_RUN_FIRST:
+
+        final QName base = m_extension.getBase();
+        final ComplexTypeReference reference = getGMLSchema().resolveComplexTypeReference( base );
+        final ComplexType complexType = reference.getComplexType();
+        m_attributeGroupArray = complexType.getAttributeGroupArray();
+        break;
+    }
+    super.init( initializeRun );
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.property.relation.RelationContentType#getAttributeGroups()
+   */
+  @Override
+  public AttributeGroupRef[] getAttributeGroups( )
+  {
+    return m_attributeGroupArray;
+  }
+
+  /**
+   * @see org.kalypso.gmlschema.property.relation.RelationContentType#collectReferences()
+   */
+  @Override
+  public String[] collectReferences( )
+  {
+    final XmlObject[] xmlObjects = getComplexType().selectPath( RelationContentType.DOCREF_XPATH );
+    final XmlObject[] xmlObjectsFromRestriction = m_extension.selectPath( RelationContentType.DOCREF_XPATH );
+
+    /* Derived refs overwrite ths from the extension. */
+    final Set<String> refs = new HashSet<String>( xmlObjects.length );
+    for( final XmlObject object : xmlObjectsFromRestriction )
+      refs.add( object.newCursor().getTextValue().trim() );
+    for( final XmlObject object : xmlObjects )
+      refs.add( object.newCursor().getTextValue().trim() );
+
+    return refs.toArray( new String[refs.size()] );
+  }
+
+}
