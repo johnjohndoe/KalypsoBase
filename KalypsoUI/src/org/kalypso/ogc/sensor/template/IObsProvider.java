e2e510ffa0a4ceb27f7569c0809cf5cbf6f0621d
dirk_kuch <dirk_kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-10-14 12:15:05 +0000

    NEW - # 646: [Master-Ticket] Refactoring of chart framework https://dev.bjoernsen.de/prognose/ticket/646

--- KalypsoUI/src/org/kalypso/ogc/sensor/template/IObsProvider.java
+++ KalypsoUI/src/org/kalypso/ogc/sensor/template/IObsProvider.java
@@ -8,16 +8,16 @@ import org.kalypso.ogc.sensor.request.IRequest;
  */
 public interface IObsProvider
 {
-  IObservation getObservation( );
-
-  IRequest getArguments( );
-
   void addListener( final IObsProviderListener listener );
-
-  void removeListener( final IObsProviderListener listener );
-
-  void dispose( );
 
   /** Clones this object, that is returns a provider of the same observation */
   IObsProvider copy( );
+
+  void dispose( );
+
+  IRequest getArguments( );
+
+  IObservation getObservation( );
+
+  void removeListener( final IObsProviderListener listener );
 }
