8c24d2ca2205ecd60409083cd1a4d02fbd88d89c
dirk_kuch <dirk_kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-11-22 14:50:02 +0000

    NEW - # 679: [Master-Ticket] refactoring zml table view https://dev.bjoernsen.de/prognose/ticket/679

--- KalypsoZmlUI/src/org/kalypso/zml/ui/KalypsoZmlUI.java
+++ KalypsoZmlUI/src/org/kalypso/zml/ui/KalypsoZmlUI.java
@@ -1,6 +1,15 @@
 package org.kalypso.zml.ui;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.kalypso.zml.ui.table.rules.IZmlTableRule;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -8,6 +17,9 @@ import org.osgi.framework.BundleContext;
  */
 public class KalypsoZmlUI extends AbstractUIPlugin
 {
+
+  private static List<IZmlTableRule> ZML_TABLE_RULES = null;
+
   // The plug-in ID
   public static final String PLUGIN_ID = "org.kalypso.zml.ui"; //$NON-NLS-1$
 
@@ -51,6 +63,42 @@ public class KalypsoZmlUI extends AbstractUIPlugin
   public static KalypsoZmlUI getDefault( )
   {
     return PLUGIN;
+  }
+
+  /**
+   * @return list of feature binding handlers, handling a special featureType qname
+   */
+  public synchronized IZmlTableRule[] getMetadataBoundaries( )
+  {
+    // fill binding map
+    if( ZML_TABLE_RULES == null )
+    {
+      ZML_TABLE_RULES = new ArrayList<IZmlTableRule>();
+
+      /* get extension points */
+      final IExtensionRegistry registry = Platform.getExtensionRegistry();
+      final IConfigurationElement[] elements = registry.getConfigurationElementsFor( IZmlTableRule.EXTENSION_POINT_ID );
+
+      for( final IConfigurationElement element : elements )
+      {
+        try
+        {
+          final String pluginid = element.getContributor().getName();
+          final Bundle bundle = Platform.getBundle( pluginid );
+          final Class< ? > featureClass = bundle.loadClass( element.getAttribute( "rule" ) ); //$NON-NLS-1$
+          final Constructor< ? > constructor = featureClass.getConstructor();
+
+          final IZmlTableRule instance = (IZmlTableRule) constructor.newInstance();
+          ZML_TABLE_RULES.add( instance );
+        }
+        catch( final Throwable e )
+        {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    return ZML_TABLE_RULES.toArray( new IZmlTableRule[] {} );
   }
 
 }
