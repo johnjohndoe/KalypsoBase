59ebb0fd5fbddfdb09216e55fe8b2931e3366629
dirk_kuch <dirk_kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-12-09 09:49:13 +0000

    new plugin kalypsobase/KalypsoZmlCore

---
+++ KalypsoCore/src/org/kalypso/ogc/sensor/provider/IObsProviderListener.java
@@ -0,0 +1,18 @@
+package org.kalypso.ogc.sensor.provider;
+
+/**
+ * @author belger
+ */
+public interface IObsProviderListener
+{
+  /**
+   * If called, the previous {@link org.kalypso.ogc.sensor.IObservation}is no more valid, get a new one by calling
+   * {@link IObsProvider#getObservation()}
+   */
+  public void observationReplaced();
+
+  /**
+   * The contents of the current available observation has changed.
+   */
+  public void observationChanged( Object source );
+}
