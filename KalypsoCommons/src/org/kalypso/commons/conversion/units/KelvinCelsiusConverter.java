2df5cc4b753502c2b4d720f04bcbd4291ebe9cd8
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2010-12-14 08:47:52 +0000

    Branch für release am 13.12.2010 in Sachsen

---
+++ KalypsoCommons/src/org/kalypso/commons/conversion/units/KelvinCelsiusConverter.java
@@ -0,0 +1,42 @@
+package org.kalypso.commons.conversion.units;
+
+/**
+ * KelvinCelsiusConverter
+ * 
+ * @author schlienger
+ */
+public class KelvinCelsiusConverter implements IValueConverter
+{
+  private static KelvinCelsiusConverter m_instance = null;
+
+  private KelvinCelsiusConverter()
+  {
+  // empty
+  }
+
+  public final static KelvinCelsiusConverter getInstance()
+  {
+    if( m_instance == null )
+      m_instance = new KelvinCelsiusConverter();
+
+    return m_instance;
+  }
+
+  /**
+   * @see org.kalypso.commons.conversion.units.IValueConverter#convert(double)
+   */
+  @Override
+  public double convert( final double value )
+  {
+    return value - 273.15;
+  }
+
+  /**
+   * @see org.kalypso.commons.conversion.units.IValueConverter#reverse(double)
+   */
+  @Override
+  public double reverse( final double value )
+  {
+    return value + 273.15;
+  }
+}
