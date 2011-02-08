2c7b17b8578073d5a1c2e586a432bd500ac289ef
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-02-08 17:02:43 +0000

    Fixed: sorting in feature tables behaviour was strange.

--- KalypsoUI/src/org/kalypso/ogc/gml/table/LayerTableSorter.java
+++ KalypsoUI/src/org/kalypso/ogc/gml/table/LayerTableSorter.java
@@ -10,7 +10,7 @@
  http://www.tuhh.de/wb
 
  and
- 
+
  Bjoernsen Consulting Engineers (BCE)
  Maria Trost 3
  56070 Koblenz, Germany
@@ -36,22 +36,29 @@
  belger@bjoernsen.de
  schlienger@bjoernsen.de
  v.doemming@tuhh.de
- 
+
  ---------------------------------------------------------------------------------------------------*/
 package org.kalypso.ogc.gml.table;
 
 import java.text.Collator;
 import java.util.Date;
+import java.util.List;
 
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerSorter;
+import org.kalypso.gmlschema.annotation.IAnnotation;
+import org.kalypso.gmlschema.property.IPropertyType;
+import org.kalypso.gmlschema.property.relation.IRelationType;
 import org.kalypsodeegree.model.feature.Feature;
+import org.kalypsodeegree_impl.model.feature.FeatureHelper;
 
 /**
  * @author belger
  */
 public class LayerTableSorter extends ViewerSorter
 {
+  // FIXME: we should instead use the index of the column and get the corresponding feature-modifier from the column;
+  // then delegate the comparison to the feature nmodifier or compare the labels of the column
   /** Die Features werden nach dieser Property sortiert */
   private String m_propertyName = null;
 
@@ -77,12 +84,12 @@ public class LayerTableSorter extends ViewerSorter
     return m_bInverse;
   }
 
-  public final void setInverse( boolean bInverse )
+  public final void setInverse( final boolean bInverse )
   {
     m_bInverse = bInverse;
   }
 
-  public final void setPropertyName( String propertyName )
+  public final void setPropertyName( final String propertyName )
   {
     m_propertyName = propertyName;
   }
@@ -99,48 +106,74 @@ public class LayerTableSorter extends ViewerSorter
 
     final Object o1;
     final Object o2;
-    if( kf1.getFeatureType().getProperty( propertyName ) == null )
+    final IPropertyType property = kf1.getFeatureType().getProperty( propertyName );
+    if( property == null )
     {
       o1 = kf1.getId();
       o2 = kf2.getId();
     }
+    else if( property instanceof IRelationType && !property.isList() )
+    {
+      o1 = FeatureHelper.resolveLink( kf1, property.getQName() );
+      o2 = FeatureHelper.resolveLink( kf2, property.getQName() );
+    }
     else
     {
-      o1 = kf1.getProperty( propertyName );
-      o2 = kf2.getProperty( propertyName );
+      o1 = kf1.getProperty( property );
+      o2 = kf2.getProperty( property );
     }
 
     final int sign = isInverse() ? -1 : 1;
+    final int basicCompare = doCompare( o1, o2 );
+    return basicCompare * sign;
+  }
+
+  private int doCompare( final Object o1, final Object o2 )
+  {
     if( o1 == o2 )
       return 0;
     if( o1 == null )
-      return sign;
+      return 1;
     if( o2 == null )
-      return -sign;
-    if( o1 instanceof String && o2 instanceof String )
-      return sign * ((String) o1).compareTo( (String) o2 );
-    else if( o1 instanceof Integer && o2 instanceof Integer )
-      return sign * ((Integer) o1).compareTo( (Integer) o2 );
-    else if( o1 instanceof Double && o2 instanceof Double )
-      return sign * ((Double) o1).compareTo( (Double) o2 );
-    else if( o1 instanceof Long && o2 instanceof Long )
-      return sign * ((Long) o1).compareTo( (Long) o2 );
-    else if( o1 instanceof Float && o1 instanceof Float )
-      return sign * ((Float) o1).compareTo( (Float) o2 );
+      return -1;
+
+    if( o1 instanceof Number && o2 instanceof Number )
+      return Double.compare( ((Number) o1).doubleValue(), ((Number) o2).doubleValue() );
     else if( o1 instanceof Date && o2 instanceof Date )
-      return sign * ((Date) o1).compareTo( (Date) o2 );
-    else if (o1 instanceof Boolean )
+      return ((Date) o1).compareTo( (Date) o2 );
+    else if( o1 instanceof Boolean && o2 instanceof Boolean )
     {
       final String s1 = String.valueOf( o1 );
       final String s2 = String.valueOf( o2 );
-      return sign * s1.compareTo( s2 );
-    }      
+      return s1.compareTo( s2 );
+    }
+    else if( o1 instanceof Feature && o2 instanceof Feature )
+    {
+      // FIXME: better than before, but still not OK: we should rather compare by the string that is really shown to the
+      // user instead of the feature's label (luckily often this is the same),
+      final Object l1 = FeatureHelper.getAnnotationValue( (Feature) o1, IAnnotation.ANNO_LABEL );
+      final Object l2 = FeatureHelper.getAnnotationValue( (Feature) o2, IAnnotation.ANNO_LABEL );
+      return doCompare( l1, l2 );
+    }
+    else if( o1 instanceof List && o2 instanceof List )
+    {
+      final List< ? > l1 = ((List< ? >) o1);
+      final List< ? > l2 = ((List< ? >) o2);
 
-    return 0;
+      final Object e1 = l1.isEmpty() ? null : l1.get( 0 );
+      final Object e2 = l2.isEmpty() ? null : l2.get( 0 );
+      return doCompare( e1, e2 );
+    }
+    else
+    {
+      final String s1 = o1.toString();
+      final String s2 = o2.toString();
+      return s1.compareToIgnoreCase( s2 );
+    }
   }
 
   @Override
-  public boolean isSorterProperty( Object element, String property )
+  public boolean isSorterProperty( final Object element, final String property )
   {
     return property.equals( m_propertyName );
   }
