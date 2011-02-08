d55998dab1571dcf3c04f8556896149f0ac79e44
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-02-08 21:14:33 +0000

    Changed behaviour of roughness edit panel. It is now possible to add/remove roughness types from the panel; the roughness layer is now always visible. Both roughness types (ks and kst) can be used at the same time in one profile.

--- KalypsoModelWspmCore/src/org/kalypso/model/wspm/core/profil/IProfilPointPropertyProvider.java
+++ KalypsoModelWspmCore/src/org/kalypso/model/wspm/core/profil/IProfilPointPropertyProvider.java
@@ -66,6 +66,7 @@ public interface IProfilPointPropertyProvider
    */
   String[] getPointProperties( );
 
+  // FIXME: does not belong in this interface
   IComponent getPointProperty( String propertyId );
 
   /**
