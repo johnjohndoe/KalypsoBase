6f4ec7f605eca85445295667121788e5b0a0cb5a
monika_t <monika_t@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-03-28 16:01:33 +0000

    hwv - Ticket 843: 2.4.1 Achsenbeschriftung - Dynamische Datumsachse

--- OdysseusChartExtensionBase/src/de/openali/odysseus/chart/ext/base/axisrenderer/provider/DateTimeAxisRendererProvider.java
+++ OdysseusChartExtensionBase/src/de/openali/odysseus/chart/ext/base/axisrenderer/provider/DateTimeAxisRendererProvider.java
@@ -30,17 +30,17 @@ public class DateTimeAxisRendererProvider extends AbstractGenericAxisRendererPro
       final long day = 24 * hour;
 
       if( dr < 3 * min )
-        return new DateTimeAxisField( DateTimeFieldType.millisOfSecond(), "dd.MM.YYYY\nHH:mm:ss:SSS", new int[] { 1, 10, 100, 500 }, new int[] {} );//$NON-NLS-1$
+        return new DateTimeAxisField( DateTimeFieldType.millisOfSecond(), "dd.MM\nHH:mm:ss:SSS", new int[] { 1, 10, 100, 500 }, new int[] {} );//$NON-NLS-1$
       else if( dr < 2 * hour )
-        return new DateTimeAxisField( DateTimeFieldType.minuteOfHour(), "dd.MM.YY\nHH:mm", new int[] { 1, 5, 10, 15, 20, 30 }, new int[] { 5, 15, 30 } );//$NON-NLS-1$
+        return new DateTimeAxisField( DateTimeFieldType.minuteOfHour(), "dd.MM\nHH:mm", new int[] { 1, 5, 10, 15, 20, 30 }, new int[] { 5, 15, 30 } );//$NON-NLS-1$
       else if( dr < 7 * hour )
         return new DateTimeAxisField( DateTimeFieldType.minuteOfHour(), "dd.MM\nHH:mm", new int[] { 1, 5, 10, 15, 20, 30 }, new int[] { 15, 30, 45 } );//$NON-NLS-1$
       else if( dr < 3 * day )
         return new DateTimeAxisField( DateTimeFieldType.minuteOfDay(), "dd.MM\nHH:mm", new int[] { 1, 15, 30 }, new int[] {} );//$NON-NLS-1$
-      else if( dr < 7 * day )
-        return new DateTimeAxisField( DateTimeFieldType.hourOfDay(), "dd.MM.YY\nHH:mm", new int[] { 1, 2, 4, 6, 8, 12 }, new int[] { 12 } );//$NON-NLS-1$
+      else if( dr < 10 * day )
+        return new DateTimeAxisField( DateTimeFieldType.hourOfDay(), "dd.MM\nHH:mm", new int[] { 1, 3, 6, 12 }, new int[] { 12 } );//$NON-NLS-1$
       else if( dr < 180 * day )
-        return new DateTimeAxisField( DateTimeFieldType.halfdayOfDay(), "dd.MM.YY\nHH:mm", new int[] {}, new int[] { 1 } );//$NON-NLS-1$
+        return new DateTimeAxisField( DateTimeFieldType.dayOfMonth(), "dd.MM", new int[] {}, new int[] {} );//$NON-NLS-1$
       else
         return new DateTimeAxisField( DateTimeFieldType.monthOfYear(), "dd.MM.YYYY", new int[] { 1, 2, 3, 4, 6 }, new int[] { 6 } );//$NON-NLS-1$
 
