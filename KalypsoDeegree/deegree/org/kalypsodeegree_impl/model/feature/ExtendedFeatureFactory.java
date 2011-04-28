58be66a1768fb443180481e00f3b26810a541b47
gernotbelger <gernotbelger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-04-28 13:23:33 +0000

    Weak-hashing of feature construction in order to improve performance when loading gml

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/feature/ExtendedFeatureFactory.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/feature/ExtendedFeatureFactory.java
@@ -40,17 +40,11 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypsodeegree_impl.model.feature;
 
-import java.lang.reflect.Constructor;
-
 import javax.xml.namespace.QName;
 
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
 import org.kalypso.gmlschema.feature.IFeatureType;
 import org.kalypso.gmlschema.property.relation.IRelationType;
-import org.kalypsodeegree.KalypsoDeegreeExtensions;
 import org.kalypsodeegree.model.feature.Feature;
-import org.osgi.framework.Bundle;
 
 /**
  * Extended Feature Factory. Difference to old Feature Factory: looks for IFeatureBinding handlers and returns special
@@ -80,35 +74,27 @@ public final class ExtendedFeatureFactory
     if( Feature.QNAME_FEATURE.equals( qName ) )
       return new Feature_Impl( parent, parentRelation, targetType, id, properties );
 
-    final IConfigurationElement element = KalypsoDeegreeExtensions.getFeatureBinding( qName );
-    if( element == null )
-    {
-      final IFeatureType substitutionFT = featureType.getSubstitutionGroupFT();
-      if( substitutionFT == null )
-      {
-        // Should never happen, all features must substitute gml:Feature
-        return new Feature_Impl( parent, parentRelation, targetType, id, properties );
-      }
+    return createBindingFeature( parent, parentRelation, featureType, id, properties, targetType, qName );
+  }
 
-      return createFeature( parent, parentRelation, substitutionFT, id, properties, targetType );
-    }
-    else
+  protected static Feature createBindingFeature( final Feature parent, final IRelationType parentRelation, final IFeatureType featureType, final String id, final Object[] properties, final IFeatureType targetType, final QName qName )
+  {
+    final FeatureBindingConstructor constructor = FeatureBindingConstructor.findBindingConstructor( qName );
+    if( constructor != null )
     {
-      try
-      {
-        final String pluginid = element.getContributor().getName();
-        final Bundle bundle = Platform.getBundle( pluginid );
-        final Class< ? > featureClass = bundle.loadClass( element.getAttribute( "class" ) );
-        final Constructor< ? > constructor = featureClass.getConstructor( Object.class, IRelationType.class, IFeatureType.class, String.class, Object[].class );
-        final Object newInstance = constructor.newInstance( parent, parentRelation, targetType, id, properties );
-        return (Feature) newInstance;
-      }
-      catch( final Throwable e )
-      {
-        e.printStackTrace();
-      }
+      final Feature instance = constructor.createInstance( parent, parentRelation, id, properties, targetType );
+      if( instance != null )
+        return instance;
+      // FALLTHROUGH: in case of exception build a normal feature
     }
 
-    return new Feature_Impl( parent, parentRelation, targetType, id, properties );
+    final IFeatureType substitutionFT = featureType.getSubstitutionGroupFT();
+    if( substitutionFT == null )
+    {
+      // Should never happen, all features must substitute gml:Feature
+      return new Feature_Impl( parent, parentRelation, targetType, id, properties );
+    }
+
+    return createFeature( parent, parentRelation, substitutionFT, id, properties, targetType );
   }
 }
