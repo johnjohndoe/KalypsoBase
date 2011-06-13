891bb4f26265a670779ea742dd288f0034b42fc7
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-06-13 14:43:05 +0000

    moved Featurecomparator to common place.

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/feature/FeatureComparator.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/model/feature/FeatureComparator.java
@@ -1,124 +1,159 @@
-/*--------------- Kalypso-Header ------------------------------------------
-
- This file is part of kalypso.
- Copyright (C) 2004, 2005 by:
-
- Technical University Hamburg-Harburg (TUHH)
- Institute of River and coastal engineering
- Denickestr. 22
- 21073 Hamburg, Germany
- http://www.tuhh.de/wb
-
- and
-
- Bjoernsen Consulting Engineers (BCE)
- Maria Trost 3
- 56070 Koblenz, Germany
- http://www.bjoernsen.de
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
- Contact:
-
- E-Mail:
- belger@bjoernsen.de
- schlienger@bjoernsen.de
- v.doemming@tuhh.de
-
- --------------------------------------------------------------------------*/
-
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
 package org.kalypsodeegree_impl.model.feature;
 
 import java.util.Comparator;
 import java.util.List;
 
+import javax.xml.namespace.QName;
+
+import org.kalypso.gmlschema.property.IPropertyType;
 import org.kalypsodeegree.model.feature.Feature;
+import org.kalypsodeegree.model.feature.GMLWorkspace;
 
 /**
- * A simple comparator for features which actually compares the value of the given property. A specific comparator can
- * be provided for comparing the values of the property.
+ * A comparator on features. The compared property values must implement {@link Comparable}.
  * 
- * @author schlienger
+ * @author Gernot Belger
  */
-public class FeatureComparator implements Comparator<Feature>
+public class FeatureComparator implements Comparator<Object>
 {
-  private final String m_propertyName;
+  private final IPropertyType m_pt;
 
-  private final Comparator m_propertyValueComparator;
+  private final GMLWorkspace m_workspace;
 
   /**
-   * Constructor without specific property comparator, values must be comparable.
-   * 
-   * @param propertyName
-   *          name of the property to be compared
+   * The qname of the property, which will be used for comparison.
    */
-  public FeatureComparator( final String propertyName )
+  private final QName m_qname;
+
+  public FeatureComparator( final Feature parentFeature, final IPropertyType pt )
   {
-    this( propertyName, null );
+    m_workspace = parentFeature.getWorkspace();
+    m_pt = pt;
+    m_qname = null;
   }
 
   /**
-   * Constructor with specific property comparator.
+   * The constructor.
    * 
-   * @param propertyName
-   *          name of the property to be compared
-   * @param propertyValueComparator
-   *          comparator to use for comparing the values of the given property. Optional, can be null.
+   * @param parentFeature
+   *            The parent of the list.
+   * @param qname
+   *            The qname of the property, which should be sorted.
    */
-  public FeatureComparator( final String propertyName, final Comparator< ? > propertyValueComparator )
+  public FeatureComparator( final Feature parentFeature, final QName qname )
   {
-    m_propertyName = propertyName;
-    m_propertyValueComparator = propertyValueComparator;
+    m_workspace = parentFeature.getWorkspace();
+    m_pt = null;
+    m_qname = qname;
   }
 
   /**
    * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
    */
   @Override
-  public int compare( final Feature f1, final Feature f2 )
+  @SuppressWarnings("unchecked") //$NON-NLS-1$
+  public int compare( final Object o1, final Object o2 )
   {
-    if( f1 == f2 )
-      return 0;
+    final Feature f1 = FeatureHelper.getFeature( m_workspace, o1 );
+    final Feature f2 = FeatureHelper.getFeature( m_workspace, o2 );
 
-    final Object value1 = f1.getProperty( m_propertyName );
-    final Object value2 = f2.getProperty( m_propertyName );
+    Object property1 = null;
+    Object property2 = null;
+    if( m_pt != null )
+    {
+      property1 = f1.getProperty( m_pt );
+      property2 = f2.getProperty( m_pt );
+    }
+    else if( m_qname != null )
+    {
+      property1 = f1.getProperty( m_qname );
+      property2 = f2.getProperty( m_qname );
+    }
 
-    return compareValues( value1, value2 );
+    if( m_pt != null && m_pt.isList() )
+      return compareLists( (List<Comparable<Object>>) property1, (List<Comparable<Object>>) property2 );
+
+    return compareComparable( (Comparable<Object>) property1, (Comparable<Object>) property2 );
   }
 
-  private int compareValues( final Object value1, final Object value2 )
+  private int compareComparable( final Comparable<Object> c1, final Comparable<Object> c2 )
   {
-    if( m_propertyValueComparator != null )
-      return m_propertyValueComparator.compare( value1, value2 );
+    if( c1 == null && c2 == null )
+      return 0;
 
-    if( value1 instanceof Comparable< ? > )
-      return ((Comparable) value1).compareTo( value2 );
+    if( c1 == null && c2 != null )
+      return -1;
 
-    /**
-     * Sepcial case: list-properties: just compare first value
-     */
-    if( value1 instanceof List< ? > && value2 instanceof List< ? > )
+    if( c2 == null )
+      return +1;
+
+    return c1.compareTo( c2 );
+  }
+
+  /**
+   * Compares two lists.
+   * <p>
+   * If the lists have not the same size, the size is compared.
+   * </p>
+   * <p>
+   * If the list do have the same size, they are compared entry-wise.
+   * </p>
+   */
+  private int compareLists( final List<Comparable<Object>> list1, final List<Comparable<Object>> list2 )
+  {
+    final int size1 = list1.size();
+    final int size2 = list2.size();
+
+    if( size1 != size2 )
+      return Double.compare( size1, size2 );
+
+    for( int i = 0; i < size1; i++ )
     {
-      final List< ? > list1 = (List< ? >) value1;
-      final List< ? > list2 = (List< ? >) value2;
-      if( list1.isEmpty() || list2.isEmpty() )
-        return 0;
-
-      final Object listElement1 = list1.get( 0 );
-      final Object listElement2 = list2.get( 0 );
-      return compareValues( listElement1, listElement2 );
+      final Comparable<Object> c1 = list1.get( i );
+      final Comparable<Object> c2 = list2.get( i );
+      final int dif = compareComparable( c1, c2 );
+      if( dif != 0 )
+        return dif;
     }
 
     return 0;
--- KalypsoUI/src/org/kalypso/ui/editor/actions/SortFeaturesActionDelegate.java
+++ KalypsoUI/src/org/kalypso/ui/editor/actions/SortFeaturesActionDelegate.java
@@ -73,6 +73,7 @@ import org.kalypsodeegree.model.feature.Feature;
 import org.kalypsodeegree.model.feature.FeatureList;
 import org.kalypsodeegree.model.feature.GMLWorkspace;
 import org.kalypsodeegree.model.feature.event.FeatureStructureChangeModellEvent;
+import org.kalypsodeegree_impl.model.feature.FeatureComparator;
 
 /**
  * @author Gernot Belger
