9b069d65595f9a817b6b9ad920c8f0cf100b971c
albert <albert@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-09-01 09:21:16 +0000

    Initial commit

---
+++ KalypsoOGC-Core/src/org/kalypso/ogc/core/utils/internal/ExtensionUtilities.java
@@ -0,0 +1,245 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.ogc.core.utils.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
+import org.kalypso.ogc.core.exceptions.ExceptionCode;
+import org.kalypso.ogc.core.exceptions.OWSException;
+import org.kalypso.ogc.core.operations.IOGCOperation;
+import org.kalypso.ogc.core.service.IOGCService;
+import org.kalypso.ogc.core.utils.OWSUtilities;
+
+/**
+ * This utilities class provides functions for reading the extension point <code>org.kalypso.ogc.core.service</code>.
+ * 
+ * @author Toni DiNardo
+ */
+public class ExtensionUtilities
+{
+  private static final String SERVICE_EXTENSION_POINT = "org.kalypso.ogc.core.service";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT = "service";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_NAME_ATTRIBUTE = "name";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_VERSION_ATTRIBUTE = "version";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_CLASS_ATTRIBUTE = "class";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT = "operation";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT_NAME_ATTRIBUTE = "name";
+
+  private static final String SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT_CLASS_ATTRIBUTE = "class";
+
+  /**
+   * The constructor.
+   */
+  private ExtensionUtilities( )
+  {
+  }
+
+  /**
+   * This function returns all in the extension registry registered OGC services.
+   * 
+   * @return All in the extension registry registered OGC services.
+   */
+  public static IOGCService[] getServices( ) throws OWSException
+  {
+    try
+    {
+      /* Memory for the OGC services. */
+      List<IOGCService> services = new ArrayList<IOGCService>();
+
+      /* Get the extension registry. */
+      IExtensionRegistry extensionRegistry = Platform.getExtensionRegistry();
+
+      /* Get the extension point. */
+      IExtensionPoint extensionPoint = extensionRegistry.getExtensionPoint( SERVICE_EXTENSION_POINT );
+
+      /* Get all configuration elements. */
+      IConfigurationElement[] configurationElements = extensionPoint.getConfigurationElements();
+      for( IConfigurationElement configurationElement : configurationElements )
+      {
+        /* If the configuration element is not the right one, continue. */
+        if( !SERVICE_EXTENSION_POINT_SERVICE_ELEMENT.equals( configurationElement.getName() ) )
+          continue;
+
+        /* Add the OGC service. */
+        services.add( (IOGCService) configurationElement.createExecutableExtension( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_CLASS_ATTRIBUTE ) );
+      }
+
+      return services.toArray( new IOGCService[] {} );
+    }
+    catch( Exception ex )
+    {
+      throw new OWSException( String.format( "Encountered an error while preparing the execution of the request. Cause: %s", ex.getMessage() ), ex, OWSUtilities.OWS_VERSION, "en", ExceptionCode.NO_APPLICABLE_CODE, null );
+    }
+  }
+
+  /**
+   * This function searches the extension registry for a OGC service matching the given name and version.
+   * 
+   * @param serviceName
+   *          The name of the requested OGC service.
+   * @param serviceVersion
+   *          The version of the requested OGC service.
+   * @return The OGC service matching the given name and version.
+   */
+  public static IOGCService getService( String serviceName, String serviceVersion ) throws OWSException
+  {
+    /* Get the service element. */
+    IConfigurationElement serviceElement = getServiceElement( serviceName, serviceVersion );
+    if( serviceElement == null )
+      throw new OWSException( String.format( "This server does not provide a service with the name '%s' and version '%s'.", serviceName, serviceVersion ), OWSUtilities.OWS_VERSION, "en", ExceptionCode.INVALID_PARAMETER_VALUE, null );
+
+    try
+    {
+      return (IOGCService) serviceElement.createExecutableExtension( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_CLASS_ATTRIBUTE );
+    }
+    catch( Exception ex )
+    {
+      throw new OWSException( String.format( "Encountered an error while preparing the execution of the request. Cause: %s", ex.getMessage() ), ex, OWSUtilities.OWS_VERSION, "en", ExceptionCode.NO_APPLICABLE_CODE, null );
+    }
+  }
+
+  /**
+   * This function searches the extension registry for an OGC operation matching the given name.
+   * 
+   * @param serviceName
+   *          The name of the requested OGC service.
+   * @param serviceVersion
+   *          The version of the requested OGC service.
+   * @param operationName
+   *          The name of the requested OGC operation.
+   * @return The OGC operation matching the given name.
+   */
+  public static IOGCOperation getOperation( String serviceName, String serviceVersion, String operationName ) throws OWSException
+  {
+    /* Get the service element. */
+    IConfigurationElement serviceElement = getServiceElement( serviceName, serviceVersion );
+    if( serviceElement == null )
+      throw new OWSException( String.format( "This server does not provide a service with the name '%s' and version '%s'.", serviceName, serviceVersion ), OWSUtilities.OWS_VERSION, "en", ExceptionCode.INVALID_PARAMETER_VALUE, null );
+
+    /* Get the operation element. */
+    IConfigurationElement operationElement = getOperationElement( serviceElement, operationName );
+    if( operationElement == null )
+      throw new OWSException( String.format( "The service with the name '%s' and version '%s' does not provide an operation with the name '%s'.", serviceName, serviceVersion, operationName ), OWSUtilities.OWS_VERSION, "en", ExceptionCode.OPERATION_NOT_SUPPORTED, null );
+
+    try
+    {
+      return (IOGCOperation) operationElement.createExecutableExtension( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT_CLASS_ATTRIBUTE );
+    }
+    catch( Exception ex )
+    {
+      throw new OWSException( String.format( "Encountered an error while preparing the execution of the request. Cause: %s", ex.getMessage() ), ex, OWSUtilities.OWS_VERSION, "en", ExceptionCode.NO_APPLICABLE_CODE, null );
+    }
+  }
+
+  /**
+   * This function searches the extension registry for a service element matching the given name and version.
+   * 
+   * @param serviceName
+   *          The name of the requested service element.
+   * @param serviceVersion
+   *          The version of the requested service element.
+   * @return The service element matching the given name and version.
+   */
+  private static IConfigurationElement getServiceElement( String serviceName, String serviceVersion )
+  {
+    /* Get the extension registry. */
+    IExtensionRegistry extensionRegistry = Platform.getExtensionRegistry();
+
+    /* Get the extension point. */
+    IExtensionPoint extensionPoint = extensionRegistry.getExtensionPoint( SERVICE_EXTENSION_POINT );
+
+    /* Get all configuration elements. */
+    IConfigurationElement[] configurationElements = extensionPoint.getConfigurationElements();
+    for( IConfigurationElement configurationElement : configurationElements )
+    {
+      /* If the configuration element is not the right one, continue. */
+      if( !SERVICE_EXTENSION_POINT_SERVICE_ELEMENT.equals( configurationElement.getName() ) )
+        continue;
+
+      /* Get the attributes. */
+      String nameAttribute = configurationElement.getAttribute( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_NAME_ATTRIBUTE );
+      String versionAttribute = configurationElement.getAttribute( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_VERSION_ATTRIBUTE );
+      if( nameAttribute.equals( serviceName ) && versionAttribute.equals( serviceVersion ) )
+        return configurationElement;
+    }
+
+    return null;
+  }
+
+  /**
+   * This function searches the extension registry for an operation element matching the given name.
+   * 
+   * @param serviceElement
+   *          The service element.
+   * @param operationName
+   *          The name of the requested operation element.
+   * @return The operation element matching the given name.
+   */
+  private static IConfigurationElement getOperationElement( IConfigurationElement serviceElement, String operationName )
+  {
+    /* Get all configuration elements. */
+    IConfigurationElement[] configurationElements = serviceElement.getChildren( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT );
+    for( IConfigurationElement configurationElement : configurationElements )
+    {
+      /* If the configuration element is not the right one, continue. */
+      if( !SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT.equals( configurationElement.getName() ) )
+        continue;
+
+      /* Get the attributes. */
+      String nameAttribute = configurationElement.getAttribute( SERVICE_EXTENSION_POINT_SERVICE_ELEMENT_OPERATION_ELEMENT_NAME_ATTRIBUTE );
+      if( nameAttribute.equals( operationName ) )
+        return configurationElement;
+    }
+
+    return null;
+  }
+}
---
+++ KalypsoWPS-Adapter/src/org/kalypso/wps/adapter/Activator.java
@@ -0,0 +1,61 @@
+package org.kalypso.wps.adapter;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle.
+ */
+public class Activator extends Plugin
+{
+  /**
+   * The plug-in ID.
+   */
+  public static final String PLUGIN_ID = "org.kalypso.wps.adapter";
+
+  /**
+   * The shared instance.
+   */
+  private static Activator plugin;
+
+  /**
+   * The constructor.
+   */
+  public Activator( )
+  {
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void start( BundleContext context ) throws Exception
+  {
+    super.start( context );
+
+    plugin = this;
+  }
+
+  /*
+   * (non-Javadoc)
+   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  @Override
+  public void stop( BundleContext context ) throws Exception
+  {
+    plugin = null;
+
+    super.stop( context );
+  }
+
+  /**
+   * Returns the shared instance.
+   * 
+   * @return The shared instance.
+   */
+  public static Activator getDefault( )
+  {
+    return plugin;
+  }
+}
