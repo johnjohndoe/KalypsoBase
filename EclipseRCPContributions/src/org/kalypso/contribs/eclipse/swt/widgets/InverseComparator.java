f1aab2af57ea6f039718d698809544bb8c4734eb
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-09-23 13:48:04 +0000

    Backport of column sorter helper . Now using swt built-in sort support. Fixes the problem with the strange icons.

---
+++ EclipseRCPContributions/src/org/kalypso/contribs/eclipse/swt/widgets/InverseComparator.java
@@ -0,0 +1,65 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraße 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.contribs.eclipse.swt.widgets;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+
+/**
+ * Inverts a given other {@link ViewerComparator}.
+ * 
+ * @author Gernot Belger
+ */
+public class InverseComparator extends ViewerComparator
+{
+  private final ViewerComparator m_delegate;
+
+  public InverseComparator( final ViewerComparator delegate )
+  {
+    m_delegate = delegate;
+  }
+
+  @Override
+  public int compare( final Viewer viewer, final Object e1, final Object e2 )
+  {
+    return m_delegate.compare( viewer, e1, e2 ) * -1;
+  }
+}
