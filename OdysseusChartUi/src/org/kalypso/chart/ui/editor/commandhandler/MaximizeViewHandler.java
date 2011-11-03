b4d194de12428021bdf0dd60ea0d1ffea6f79bfe
kuch <kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-11-03 10:47:41 +0000

    Prognose Sachsen Branch

---
+++ OdysseusChartUi/src/org/kalypso/chart/ui/editor/commandhandler/MaximizeViewHandler.java
@@ -0,0 +1,37 @@
+package org.kalypso.chart.ui.editor.commandhandler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.expressions.IEvaluationContext;
+import org.eclipse.core.runtime.Status;
+
+import de.openali.odysseus.chart.framework.model.IChartModel;
+import de.openali.odysseus.chart.framework.view.IChartComposite;
+
+/**
+ * This handler sets all axis ranges in a way that all relations which were set up by a user - e.g. by panning or
+ * zooming single layers / axes - are kept; for example, if there are two layers
+ * 
+ * @author burtscher1
+ */
+public class MaximizeViewHandler extends AbstractHandler
+{
+  /**
+   * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+   */
+  @Override
+  @SuppressWarnings("unchecked")
+  public Object execute( final ExecutionEvent event )
+  {
+    final IEvaluationContext context = (IEvaluationContext) event.getApplicationContext();
+
+    final IChartComposite chart = ChartHandlerUtilities.getChart( context );
+    if( chart == null )
+      return Status.CANCEL_STATUS;
+
+    final IChartModel model = chart.getChartModel();
+    model.autoscale();
+
+    return Status.OK_STATUS;
+  }
+}
