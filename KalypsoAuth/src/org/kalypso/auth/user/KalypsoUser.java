abecc9f16cc21db29669094c5fd09d914b4ab5f9
kuch <kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-12-01 13:28:39 +0000

    - removed kalypso modeler perspective shortcut - fixed start page kalypso modules composite

--- KalypsoAuth/src/org/kalypso/auth/user/KalypsoUser.java
+++ KalypsoAuth/src/org/kalypso/auth/user/KalypsoUser.java
@@ -1,5 +1,8 @@
 package org.kalypso.auth.user;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 /**
  * A Kalypso User, default implementation of IKalypsoUser.
  * 
@@ -45,5 +48,33 @@ public class KalypsoUser implements IKalypsoUser
   public String[] getRights( )
   {
     return m_rights;
+  }
+
+  @Override
+  public boolean equals( final Object obj )
+  {
+    if( obj instanceof IKalypsoUser )
+    {
+      final IKalypsoUser other = (IKalypsoUser) obj;
+
+      final EqualsBuilder builder = new EqualsBuilder();
+      builder.append( getUserName(), other.getUserName() );
+      builder.append( getRights(), other.getRights() );
+
+      return builder.isEquals();
+    }
+
+    return super.equals( obj );
+  }
+
+  @Override
+  public int hashCode( )
+  {
+    final HashCodeBuilder builder = new HashCodeBuilder();
+    builder.append( getUserName() );
+    builder.append( getRights() );
+
+    return builder.toHashCode();
+
   }
 }
