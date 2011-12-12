999d639f9190dfefe215fc3d64ad5ce58306a987
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-12-12 15:07:31 +0000

    CSV timeseries import support whitespace after last digit

--- KalypsoCore/src/org/kalypso/ogc/sensor/adapter/NativeObservationCSVAdapter.java
+++ KalypsoCore/src/org/kalypso/ogc/sensor/adapter/NativeObservationCSVAdapter.java
@@ -69,7 +69,7 @@ public class NativeObservationCSVAdapter extends AbstractObservationImporter
 {
   private final DateFormat m_grapDateFormat = new SimpleDateFormat( "dd MM yyyy HH mm ss" ); //$NON-NLS-1$
 
-  public static Pattern CSV_PATTERN = Pattern.compile( "([0-9]{1,2}.+?[0-9]{1,2}.+?[0-9]{2,4}.+?[0-9]{1,2}.+?[0-9]{1,2}).+?([0-9]+\\,+?[0-9]+)" ); //$NON-NLS-1$
+  public static Pattern CSV_PATTERN = Pattern.compile( "([0-9]{1,2}.+?[0-9]{1,2}.+?[0-9]{2,4}.+?[0-9]{1,2}.+?[0-9]{1,2}).+?([0-9]+\\,+?[0-9]+).+?" ); //$NON-NLS-1$
 
   private static final int MAX_NO_OF_ERRORS = 30;
 
