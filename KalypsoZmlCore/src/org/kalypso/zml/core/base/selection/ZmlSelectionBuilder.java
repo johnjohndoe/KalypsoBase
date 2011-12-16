0e5f5fac55a4eefa77d4feb9130edac3633cc84c
kuch <kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-12-16 15:14:07 +0000

    RRM TreeNode IObsProvider / IObservation selection handling

--- KalypsoZmlCore/src/org/kalypso/zml/core/base/selection/ZmlSelectionBuilder.java
+++ KalypsoZmlCore/src/org/kalypso/zml/core/base/selection/ZmlSelectionBuilder.java
@@ -40,16 +40,28 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.zml.core.base.selection;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.kalypso.commons.java.lang.Objects;
+import org.kalypso.ogc.sensor.DateRange;
+import org.kalypso.ogc.sensor.IAxis;
 import org.kalypso.ogc.sensor.IObservation;
+import org.kalypso.ogc.sensor.provider.IObsProvider;
+import org.kalypso.ogc.sensor.provider.PlainObsProvider;
+import org.kalypso.ogc.sensor.request.ObservationRequest;
+import org.kalypso.ogc.sensor.timeseries.AxisUtils;
+import org.kalypso.ogc.sensor.view.ObservationViewHelper;
 import org.kalypso.repository.IRepositoryItem;
 import org.kalypso.zml.core.base.IMultipleZmlSourceElement;
-import org.kalypso.zml.core.base.obsprovider.RepositoryItemZmlSelectionBuilder;
+import org.kalypso.zml.core.base.obsprovider.MultipleObsProvider;
+import org.kalypso.zml.core.base.obsprovider.ZmlPlainObsProvider;
 
 /**
  * @author Dirk Kuch
@@ -67,11 +79,12 @@ public final class ZmlSelectionBuilder
 
   }
 
-  private static IMultipleZmlSourceElement[] getSelection( final IStructuredSelection selection )
+  public static IMultipleZmlSourceElement[] getSelection( final IStructuredSelection selection )
   {
     final Iterator< ? > iterator = selection.iterator();
 
     final Set<IRepositoryItem> items = new LinkedHashSet<>();
+    final Set<IObsProvider> providers = new LinkedHashSet<>();
 
     while( iterator.hasNext() )
     {
@@ -84,10 +97,84 @@ public final class ZmlSelectionBuilder
           items.add( item );
         }
       }
+      else if( obj instanceof IAdaptable )
+      {
+        final IAdaptable adapter = (IAdaptable) obj;
+
+        final IObsProvider provider = (IObsProvider) adapter.getAdapter( IObsProvider.class );
+        if( Objects.isNotNull( provider ) )
+        {
+          providers.add( provider );
+          continue;
+        }
+
+        final IObservation observation = (IObservation) adapter.getAdapter( IObservation.class );
+        if( Objects.isNotNull( observation ) )
+        {
+          providers.add( new PlainObsProvider( observation, null ) );
+          continue;
+        }
+      }
     }
 
-    final RepositoryItemZmlSelectionBuilder selectionBuilder = new RepositoryItemZmlSelectionBuilder();
-    return selectionBuilder.toSelection( items.toArray( new IRepositoryItem[] {} ) );
+    final Map<String, IMultipleZmlSourceElement> sources = new HashMap<String, IMultipleZmlSourceElement>();
+    fill( sources, items.toArray( new IRepositoryItem[] {} ) );
+    fill( sources, providers.toArray( new IObsProvider[] {} ) );
+
+    return sources.values().toArray( new IMultipleZmlSourceElement[] {} );
   }
 
+  private static void fill( final Map<String, IMultipleZmlSourceElement> sources, final IObsProvider[] providers )
+  {
+    for( final IObsProvider provider : providers )
+    {
+      // FIXME lazy loading
+      final IObservation observation = provider.getObservation();
+      if( Objects.isNull( observation ) )
+        continue;
+
+      final IAxis[] valueAxes = AxisUtils.findValueAxes( observation.getAxes(), false );
+      for( final IAxis axis : valueAxes )
+      {
+        final String type = axis.getType();
+        IMultipleZmlSourceElement multiple = sources.get( type );
+        if( Objects.isNull( multiple ) )
+        {
+          multiple = new MultipleObsProvider( type );
+          sources.put( type, multiple );
+        }
+
+        multiple.add( new ZmlPlainObsProvider( multiple.getIdentifier(), observation, new ObservationRequest() ) );
+      }
+    }
+
+  }
+
+  private static void fill( final Map<String, IMultipleZmlSourceElement> sources, final IRepositoryItem[] items )
+  {
+    for( final IRepositoryItem item : items )
+    {
+      final IObservation observation = (IObservation) item.getAdapter( IObservation.class );
+      if( Objects.isNull( observation ) )
+        continue;
+
+      // TODO refactor ObservationViewHelper in KalypsoUI - to get rid of KalypsoUI dependency in KalypsoZmlCore
+      final DateRange dateRange = ObservationViewHelper.makeDateRange( item );
+
+      final IAxis[] valueAxes = AxisUtils.findValueAxes( observation.getAxes(), false );
+      for( final IAxis axis : valueAxes )
+      {
+        final String type = axis.getType();
+        IMultipleZmlSourceElement multiple = sources.get( type );
+        if( Objects.isNull( multiple ) )
+        {
+          multiple = new MultipleObsProvider( type );
+          sources.put( type, multiple );
+        }
+
+        multiple.add( new ZmlPlainObsProvider( multiple.getIdentifier(), observation, new ObservationRequest( dateRange ) ) );
+      }
+    }
+
+  }
 }
