1dc026befa59330219881ef97b7fc6ef0c175df6
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2011-12-21 10:33:03 +0000

    DefaulStatus for DataSourceProxyOsbervation

--- KalypsoCore/src/org/kalypso/ogc/sensor/timeseries/datasource/DataSourceProxyObservation.java
+++ KalypsoCore/src/org/kalypso/ogc/sensor/timeseries/datasource/DataSourceProxyObservation.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ogc.sensor.timeseries.datasource;
 
@@ -53,6 +53,7 @@ import org.kalypso.ogc.sensor.SensorException;
 import org.kalypso.ogc.sensor.metadata.MetadataHelper;
 import org.kalypso.ogc.sensor.metadata.MetadataList;
 import org.kalypso.ogc.sensor.request.IRequest;
+import org.kalypso.ogc.sensor.status.KalypsoStati;
 import org.kalypso.ogc.sensor.timeseries.AxisUtils;
 import org.kalypso.ogc.sensor.util.Observations;
 import org.kalypso.ogc.sensor.visitor.IObservationVisitor;
@@ -60,7 +61,7 @@ import org.kalypso.ogc.sensor.visitor.IObservationVisitor;
 /**
  * Data source proxy observation class. To add a data source axis to an model we have to create a new model. This is a
  * very slow operation, so we introduced this proxy class. the new model will be created by the first request.
- * 
+ *
  * @author Dirk Kuch
  */
 public class DataSourceProxyObservation implements IObservation
@@ -73,11 +74,19 @@ public class DataSourceProxyObservation implements IObservation
 
   private MetadataList m_metadata;
 
+  private final int m_defaultStatus;
+
   public DataSourceProxyObservation( final IObservation observation, final String itemIdentifier, final String repositoryId )
+  {
+    this( observation, itemIdentifier, repositoryId, KalypsoStati.BIT_OK );
+  }
+
+  public DataSourceProxyObservation( final IObservation observation, final String itemIdentifier, final String repositoryId, final int defaultStatus )
   {
     m_observation = observation;
     m_itemIdentifier = itemIdentifier;
     m_repositoryId = repositoryId;
+    m_defaultStatus = defaultStatus;
   }
 
   @Override
