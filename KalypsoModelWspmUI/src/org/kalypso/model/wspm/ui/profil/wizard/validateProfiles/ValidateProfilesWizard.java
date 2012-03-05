d020cb5f9610f553d9b1992eb52b9cf95f1d4ec7
kuch <kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-03-05 13:52:29 +0000

    fix - KalypsoWSPM Profile Validation

--- KalypsoModelWspmUI/src/org/kalypso/model/wspm/ui/profil/wizard/validateProfiles/ValidateProfilesWizard.java
+++ KalypsoModelWspmUI/src/org/kalypso/model/wspm/ui/profil/wizard/validateProfiles/ValidateProfilesWizard.java
@@ -40,49 +40,27 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.model.wspm.ui.profil.wizard.validateProfiles;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.apache.commons.lang.StringUtils;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWizard;
-import org.kalypso.contribs.eclipse.core.resources.ResourceUtilities;
 import org.kalypso.contribs.eclipse.jface.dialog.DialogSettingsUtils;
-import org.kalypso.contribs.eclipse.jface.operation.ICoreRunnableWithProgress;
-import org.kalypso.contribs.eclipse.jface.operation.RunnableContextHelper;
 import org.kalypso.contribs.eclipse.jface.wizard.ArrayChooserPage;
 import org.kalypso.model.wspm.core.KalypsoModelWspmCoreExtensions;
 import org.kalypso.model.wspm.core.KalypsoModelWspmCorePlugin;
 import org.kalypso.model.wspm.core.gml.IProfileFeature;
 import org.kalypso.model.wspm.core.gml.ProfileFeatureFactory;
-import org.kalypso.model.wspm.core.profil.IProfil;
 import org.kalypso.model.wspm.core.profil.reparator.IProfilMarkerResolution;
-import org.kalypso.model.wspm.core.profil.validator.IValidatorMarkerCollector;
 import org.kalypso.model.wspm.core.profil.validator.IValidatorRule;
 import org.kalypso.model.wspm.core.profil.validator.ValidatorRuleSet;
 import org.kalypso.model.wspm.ui.KalypsoModelWspmUIPlugin;
 import org.kalypso.model.wspm.ui.action.ProfileSelection;
 import org.kalypso.model.wspm.ui.i18n.Messages;
-import org.kalypso.model.wspm.ui.profil.validation.ResourceValidatorMarkerCollector;
 import org.kalypso.model.wspm.ui.profil.wizard.ProfileHandlerUtils;
 import org.kalypso.model.wspm.ui.profil.wizard.ProfilesChooserPage;
-import org.kalypso.ogc.gml.command.ChangeFeaturesCommand;
-import org.kalypso.ogc.gml.command.FeatureChange;
 import org.kalypso.ogc.gml.mapmodel.CommandableWorkspace;
-import org.kalypsodeegree.model.feature.Feature;
 
 /**
  * @author kimwerner
@@ -141,9 +119,6 @@ public class ValidateProfilesWizard extends Wizard implements IWorkbenchWizard
     m_quickFixChoosePage.setLabelProvider( new LabelProvider()
     {
 
-      /**
-       * @see org.eclipse.jface.viewers.LabelProvider#getText(java.lang.Object)
-       */
       @Override
       public String getText( final Object element )
       {
@@ -157,157 +132,22 @@ public class ValidateProfilesWizard extends Wizard implements IWorkbenchWizard
 
   }
 
-  /**
-   * @see org.eclipse.jface.wizard.Wizard#canFinish()
-   */
   @Override
   public boolean canFinish( )
   {
     return m_validatorChooserPage.isPageComplete();
   }
 
-  /**
-   * @see org.eclipse.jface.wizard.Wizard#performFinish()
-   */
   @Override
   public boolean performFinish( )
   {
-    final Object[] profilFeatures = m_profileChooserPage.getChoosen();
-    final Object[] choosenRules = m_validatorChooserPage.getChoosen();
+    final IProfileFeature[] profileFeatures = ValidateProfileHelper.toProfiles( m_profileChooserPage.getChoosen() );
+    final IValidatorRule[] rules = ValidateProfileHelper.toRules( m_validatorChooserPage.getChoosen() );
     final Object[] quickFixes = m_quickFixChoosePage.getChoosen();
-    final IProfil[] profiles = new IProfil[profilFeatures.length];
-    final String[] featureIDs = new String[profilFeatures.length];
-    final ArrayList<FeatureChange> featureChanges = new ArrayList<FeatureChange>();
 
-    final URL workspaceContext = m_workspace.getContext();
-    final IFile resource = workspaceContext == null ? null : ResourceUtilities.findFileFromURL( workspaceContext );
+    final ValidateProfilesJob job = new ValidateProfilesJob( profileFeatures, rules, quickFixes, m_workspace );
+    job.schedule();
 
-    final Shell shell = getShell();
-
-    final ICoreRunnableWithProgress m_validateJob = new ICoreRunnableWithProgress()
-    {
-      @Override
-      public IStatus execute( final IProgressMonitor monitor )
-      {
-        monitor.beginTask( Messages.getString( "org.kalypso.model.wspm.ui.profil.wizard.validateProfiles.ValidateProfilesWizard.7" ), 2 * profilFeatures.length ); //$NON-NLS-1$
-        for( int i = 0; i < profilFeatures.length; i++ )
-        {
-          if( profilFeatures[i] instanceof Feature )
-          {
-            final IProfileFeature wspmProfil = (IProfileFeature) profilFeatures[i];
-            profiles[i] = wspmProfil == null ? null : wspmProfil.getProfil();
-            featureIDs[i] = ((Feature) profilFeatures[i]).getId();
-          }
-
-          if( profiles[i] != null )
-          {
-            try
-            {
-              final IMarker[] markers = resource.findMarkers( KalypsoModelWspmUIPlugin.MARKER_ID, true, IResource.DEPTH_ZERO );
-              for( final IMarker marker : markers )
-              {
-                if( marker.getAttribute( IValidatorMarkerCollector.MARKER_ATTRIBUTE_PROFILE_ID ).equals( featureIDs[i] ) )
-                {
-                  marker.delete();
-                }
-              }
-            }
-            catch( final CoreException e )
-            {
-              monitor.done();
-              return new Status( IStatus.ERROR, KalypsoModelWspmCorePlugin.getID(), e.getLocalizedMessage() );
-            }
-          }
-          monitor.worked( i );
-        }
-        final HashMap<String, String> uiResults = new HashMap<String, String>();
-        for( int i = 0; i < profilFeatures.length; i++ )
-        {
-          if( profiles[i] != null )
-          {
-            final IValidatorMarkerCollector collector = new ResourceValidatorMarkerCollector( resource, null, "" + profiles[i].getStation(), featureIDs[i] ); //$NON-NLS-1$
-            try
-            {
-              for( final Object rule : choosenRules )
-              {
-                ((IValidatorRule) rule).validate( profiles[i], collector );
-              }
-              final IMarker[] markers = collector.getMarkers();
-              for( final IMarker marker : markers )
-              {
-                final String quickFixRes = marker.getAttribute( IValidatorMarkerCollector.MARKER_ATTRIBUTE_QUICK_FIX_RESOLUTIONS, null );
-
-                if( quickFixRes != null && quickFixes.length > 0 )
-                {
-                  final String[] resolutions = StringUtils.split( quickFixRes, '\u0000' );
-                  final IProfilMarkerResolution[] markerRes = new IProfilMarkerResolution[resolutions == null ? 0 : resolutions.length];
-                  for( int j = 0; j < markerRes.length; j++ )
-                  {
-                    final IProfilMarkerResolution mr = KalypsoModelWspmCoreExtensions.getReparatorRule( resolutions[j] );
-
-                    boolean resolved = false;
-                    for( final Object quickFix : quickFixes )
-                    {
-                      if( mr != null && mr.getClass().getName().equals( quickFix.getClass().getName() ) )
-                      {
-                        if( mr.hasUI() )
-                        {
-                          final String uiResult = uiResults.get( quickFix.getClass().getName() );
-                          final IProfil prof = profiles[i];
-                          if( uiResult == null )
-                          {
-                            shell.getDisplay().syncExec( new Runnable()
-                            {
-                              @Override
-                              public void run( )
-                              {
-                                uiResults.put( quickFix.getClass().getName(), mr.getUIresult( shell, prof ) );
-                              }
-                            } );
-
-                          }
-                          mr.setUIresult( uiResults.get( quickFix.getClass().getName() ) );
-                        }
-                        resolved = mr.resolve( profiles[i] );
-                      }
-                    }
-                    if( resolved )
-                    {
-                      marker.delete();
-                    }
-                  }
-                }
-              }
-            }
-            catch( final CoreException e )
-            {
-              monitor.done();
-              return new Status( IStatus.ERROR, KalypsoModelWspmCorePlugin.getID(), e.getLocalizedMessage() );
-            }
-          }
-          monitor.worked( profilFeatures.length + i );
-        }
-        monitor.done();
-        return Status.OK_STATUS;
-      }
-    };
-    shell.getDisplay().asyncExec( new Runnable()
-    {
-      @Override
-      public void run( )
-      {
-        RunnableContextHelper.execute( new ProgressMonitorDialog( shell ), true, true, m_validateJob );
-
-        try
-        {
-          m_workspace.postCommand( new ChangeFeaturesCommand( m_workspace, featureChanges.toArray( new FeatureChange[] {} ) ) );
-        }
-        catch( final Exception e )
-        {
-          e.printStackTrace();
-        }
-      }
-    } );
     return true;
   }
 }
