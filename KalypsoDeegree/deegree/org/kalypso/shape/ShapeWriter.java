5e5e392f835a0a7e4fc1b8373cab4a24e8f4ec15
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-03-07 18:29:52 +0000

    Removed obsolete exception

--- KalypsoDeegree/deegree/org/kalypso/shape/ShapeWriter.java
+++ KalypsoDeegree/deegree/org/kalypso/shape/ShapeWriter.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.shape;
 
@@ -57,7 +57,7 @@ import org.kalypso.transformation.PrjHelper;
 
 /**
  * Writes a {@link ShapeFile} based on data from a {@link IShapeDataProvider}.
- * 
+ *
  * @author Gernot Belger
  */
 public class ShapeWriter
@@ -73,7 +73,7 @@ public class ShapeWriter
    * @param shapeFileBase
    *          The absolute file path to the shape file that will be written.
    */
-  public void write( final String shapeFileBase, final IProgressMonitor monitor ) throws IOException, DBaseException, SHPException, ShapeDataException, CoreException
+  public void write( final String shapeFileBase, final IProgressMonitor monitor ) throws IOException, DBaseException, SHPException, ShapeDataException
   {
     final String taskMsg = String.format( "Writing shape %s", shapeFileBase );
 
@@ -122,7 +122,7 @@ public class ShapeWriter
     PrjHelper.fetchPrjFile( coordinateSystem, prjFile, monitor );
   }
 
-  private void writeData( final IDBFValue[] fields, final ShapeFile shapeFile, final IProgressMonitor monitor ) throws IOException, DBaseException, SHPException, ShapeDataException, CoreException
+  private void writeData( final IDBFValue[] fields, final ShapeFile shapeFile, final IProgressMonitor monitor ) throws IOException, DBaseException, SHPException, ShapeDataException
   {
     int count = 0;
     final String size = m_data.size() == -1 ? "Unknown" : String.format( "%d", m_data.size() );
