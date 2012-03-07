8ec6f663b4049c73aed60c80ecc976a32266696c
kuch <kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-03-07 08:12:54 +0000

    new plug-in net.sourceforge.nattable

---
+++ net.sourceforge.nattable/src/net/sourceforge/nattable/NatTableActivator.java
@@ -0,0 +1,28 @@
+package net.sourceforge.nattable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class NatTableActivator implements BundleActivator
+{
+
+  private static BundleContext CONTEXT;
+
+  static BundleContext getContext( )
+  {
+    return CONTEXT;
+  }
+
+  @Override
+  public void start( final BundleContext bundleContext ) throws Exception
+  {
+    NatTableActivator.CONTEXT = bundleContext;
+  }
+
+  @Override
+  public void stop( final BundleContext bundleContext ) throws Exception
+  {
+    NatTableActivator.CONTEXT = null;
+  }
+
+}
