55bde246509e9301ed73a772915acc24eb0e6624
kuch <kuch@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-04-23 14:14:52 +0000

    data sources of intervall filter

--- KalypsoCore/src/org/kalypso/ogc/sensor/filter/filters/interval/IntervalAxesValues.java
+++ KalypsoCore/src/org/kalypso/ogc/sensor/filter/filters/interval/IntervalAxesValues.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ogc.sensor.filter.filters.interval;
 
@@ -55,7 +55,6 @@ import org.kalypso.ogc.sensor.TupleModelDataSet;
 import org.kalypso.ogc.sensor.status.KalypsoStati;
 import org.kalypso.ogc.sensor.timeseries.AxisUtils;
 import org.kalypso.ogc.sensor.timeseries.datasource.DataSourceHandler;
-import org.kalypso.repository.IDataSourceItem;
 
 /**
  * @author Dirk Kuch
@@ -171,7 +170,7 @@ public class IntervalAxesValues
     final Set<TupleModelDataSet> dataSets = new LinkedHashSet<TupleModelDataSet>();
     for( final IAxis axis : m_valueAxes )
     {
-      dataSets.add( new TupleModelDataSet( axis, m_defaultValue, m_defaultStatus, IDataSourceItem.SOURCE_UNKNOWN ) );
+      dataSets.add( new TupleModelDataSet( axis, m_defaultValue, m_defaultStatus, IntervalSourceHandler.SOURCE_INTERVAL_FITLER ) );
     }
 
     return new IntervalData( interval, dataSets.toArray( new TupleModelDataSet[] {} ) );
@@ -183,7 +182,7 @@ public class IntervalAxesValues
     final Set<TupleModelDataSet> dataSets = new LinkedHashSet<TupleModelDataSet>();
     for( final IAxis axis : m_valueAxes )
     {
-      dataSets.add( new TupleModelDataSet( axis, 0d, KalypsoStati.BIT_OK, IDataSourceItem.SOURCE_UNKNOWN ) );
+      dataSets.add( new TupleModelDataSet( axis, 0d, KalypsoStati.BIT_OK, IntervalSourceHandler.SOURCE_INTERVAL_FITLER ) );
     }
 
     return new IntervalData( interval, dataSets.toArray( new TupleModelDataSet[] {} ) );
