4986e9381205baffc12043ac7e07a0a478096255
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-06-08 09:24:41 +0000

    Fixed merge errors

--- KalypsoDeegree/deegree/org/kalypsodeegree_impl/io/shpapi/ShapeFile.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree_impl/io/shpapi/ShapeFile.java
@@ -18,15 +18,9 @@
  *
  * Files in this package are originally taken from deegree and modified here
  * to fit in kalypso. As goals of kalypso differ from that one in deegree
-<<<<<<< .working
  * interface-compatibility to deegree is wanted but not retained always.
  * 
  * If you intend to use this software in other ways than in kalypso
-=======
- * interface-compatibility to deegree is wanted but not retained always.
- *
- * If you intend to use this software in other ways than in kalypso
->>>>>>> .merge-right.r3720
  * (e.g. OGC-web services), you should consider the latest version of deegree,
  * see http://www.deegree.org .
  *
