034e2e5c032812beae98bea734770d7081cb1b97
albert <albert@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-06-15 08:04:53 +0000

    branched for installation on 23.05.2012

---
+++ KalypsoGmlUI/src/org/kalypso/gml/ui/commands/exportshape/FieldNameEditingSupport.java
@@ -0,0 +1,125 @@
+/*----------------    FILE HEADER KALYPSO ------------------------------------------
+ *
+ *  This file is part of kalypso.
+ *  Copyright (C) 2004 by:
+ * 
+ *  Technical University Hamburg-Harburg (TUHH)
+ *  Institute of River and coastal engineering
+ *  Denickestraﬂe 22
+ *  21073 Hamburg, Germany
+ *  http://www.tuhh.de/wb
+ * 
+ *  and
+ *  
+ *  Bjoernsen Consulting Engineers (BCE)
+ *  Maria Trost 3
+ *  56070 Koblenz, Germany
+ *  http://www.bjoernsen.de
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ *  Contact:
+ * 
+ *  E-Mail:
+ *  belger@bjoernsen.de
+ *  schlienger@bjoernsen.de
+ *  v.doemming@tuhh.de
+ *   
+ *  ---------------------------------------------------------------------------*/
+package org.kalypso.gml.ui.commands.exportshape;
+
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ColumnViewer;
+import org.eclipse.jface.viewers.EditingSupport;
+import org.eclipse.jface.viewers.TextCellEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.widgets.Composite;
+import org.kalypso.shape.ShapeDataException;
+import org.kalypso.shape.dbf.IDBFValue;
+
+/**
+ * @author Gernot Belger
+ */
+public class FieldNameEditingSupport extends EditingSupport
+{
+  private final TextCellEditor m_cellEditor;
+
+  public FieldNameEditingSupport( final ColumnViewer viewer )
+  {
+    super( viewer );
+
+    final TextCellEditor textCellEditor = new TextCellEditor( (Composite) viewer.getControl(), SWT.NONE );
+    m_cellEditor = textCellEditor;
+
+    viewer.getControl().addDisposeListener( new DisposeListener()
+    {
+      @Override
+      public void widgetDisposed( final DisposeEvent e )
+      {
+        textCellEditor.dispose();
+      }
+    } );
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.EditingSupport#canEdit(java.lang.Object)
+   */
+  @Override
+  protected boolean canEdit( final Object element )
+  {
+    return true;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.EditingSupport#getCellEditor(java.lang.Object)
+   */
+  @Override
+  protected CellEditor getCellEditor( final Object element )
+  {
+    return m_cellEditor;
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.EditingSupport#getValue(java.lang.Object)
+   */
+  @Override
+  protected Object getValue( final Object element )
+  {
+    // TODO: connect to column label provider
+    try
+    {
+      final IDBFValue value = (IDBFValue) element;
+      return value.getField().getName();
+    }
+    catch( final ShapeDataException e )
+    {
+      e.printStackTrace();
+      return e.getLocalizedMessage();
+    }
+  }
+
+  /**
+   * @see org.eclipse.jface.viewers.EditingSupport#setValue(java.lang.Object, java.lang.Object)
+   */
+  @Override
+  protected void setValue( final Object element, final Object value )
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
