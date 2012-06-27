f26ffa9c7e794ff0e4153239bf5d882b84e45ef1
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-06-27 14:17:23 +0000

    Tag kalypso_12.7.0.20120627

---
+++ OdysseusChartUi/src/org/kalypso/chart/ui/editor/mousehandler/AbstractChartDragHandler.java
@@ -0,0 +1,189 @@
+package org.kalypso.chart.ui.editor.mousehandler;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Point;
+import org.kalypso.chart.ui.editor.commandhandler.ChartHandlerUtilities;
+
+import de.openali.odysseus.chart.framework.model.IChartModel;
+import de.openali.odysseus.chart.framework.model.impl.visitors.FindLayerTooltipVisitor;
+import de.openali.odysseus.chart.framework.model.layer.EditInfo;
+import de.openali.odysseus.chart.framework.model.layer.IEditableChartLayer;
+import de.openali.odysseus.chart.framework.model.layer.ILayerManager;
+import de.openali.odysseus.chart.framework.model.layer.manager.visitors.EditableChartLayerVisitor;
+import de.openali.odysseus.chart.framework.view.IChartComposite;
+
+/**
+ * FIXME: using the eidt info here in order to save the click-state is dubious. Separate editing and dragging (i.e.
+ * separate this into different helper classes or remove the edit stuff from this class).
+ * 
+ * @author kimwerner
+ */
+public abstract class AbstractChartDragHandler extends AbstractChartHandler
+{
+  private EditInfo m_editInfo = null;
+
+  private final int m_trashold;
+
+  private EditInfo m_clickInfo = null;
+
+  private int m_deltaSnapX = 0;
+
+  private int m_deltaSnapY = 0;
+
+  private int m_startX = 0;
+
+  private int m_startY = 0;
+
+  private final int m_observedButtonMask;
+
+  public AbstractChartDragHandler( final IChartComposite chart )
+  {
+    this( chart, 5 );
+  }
+
+  public AbstractChartDragHandler( final IChartComposite chart, final int trashHold )
+  {
+    this( chart, trashHold, SWT.BUTTON1 | SWT.BUTTON2 | SWT.BUTTON3 );
+  }
+
+  public AbstractChartDragHandler( final IChartComposite chart, final int trashHold, final int observedButtonMask )
+  {
+    super( chart );
+
+    m_trashold = trashHold;
+    m_observedButtonMask = observedButtonMask;
+  }
+
+  protected final int button2Mask( final int button )
+  {
+    return 1 << 18 + button;
+  }
+
+  protected abstract void doMouseMoveAction( final Point end, final EditInfo editInfo );
+
+  protected abstract void doMouseUpAction( final Point end, final EditInfo editInfo );
+
+  protected final EditInfo getHover( final Point screen )
+  {
+    final IChartModel model = getChart().getChartModel();
+    if( model == null )
+      return null;
+
+    final Point plotPoint = ChartHandlerUtilities.screen2plotPoint( screen, getChart().getPlotRect() );
+
+    final ILayerManager layerManager = model.getLayerManager();
+    final EditableChartLayerVisitor visitor = new EditableChartLayerVisitor();
+    layerManager.accept( visitor );
+
+    final IEditableChartLayer[] layers = visitor.getLayers();
+    ArrayUtils.reverse( layers );
+
+    for( final IEditableChartLayer layer : layers )
+    {
+      final EditInfo info = layer.getHover( plotPoint );
+      if( info != null )
+        return info;
+    }
+
+    return null;
+  }
+
+  protected void mouseDown( final Point down )
+  {
+    m_clickInfo = getHover( down );
+    if( m_clickInfo == null )
+      m_clickInfo = new EditInfo( null, null, null, null, null, ChartHandlerUtilities.screen2plotPoint( down, getChart().getPlotRect() ) );
+
+    // offset from cursor relative to the given InfoObject Center
+    final Point pos = ChartHandlerUtilities.plotPoint2screen( m_clickInfo.getPosition(), getChart().getPlotRect() );
+    m_deltaSnapX = down.x - pos.x;
+    m_deltaSnapY = down.y - pos.y;
+    m_startX = down.x;
+    m_startY = down.y;
+  }
+
+  @Override
+  public void mouseDown( final MouseEvent e )
+  {
+    if( (button2Mask( e.button ) & m_observedButtonMask) == 0 )
+      return;
+
+    mouseDown( new Point( e.x, e.y ) );
+  }
+
+  @Override
+  public void mouseMove( final MouseEvent e )
+  {
+    super.mouseMove( e );
+    setToolInfo( null );
+
+    final Point p = new Point( e.x, e.y );
+    if( m_clickInfo == null )
+      doSetTooltip( p );
+    else
+      mouseMove( p );
+  }
+
+  private void doSetTooltip( final Point point )
+  {
+    final IChartComposite chart = getChart();
+    final IChartModel model = chart.getChartModel();
+
+    final FindLayerTooltipVisitor visitor = new FindLayerTooltipVisitor( chart, ChartHandlerUtilities.screen2plotPoint( point, chart.getPlotRect() ) );
+    model.getLayerManager().accept( visitor );
+
+    setToolInfo( visitor.getEditInfo() );
+  }
+
+  protected void mouseMove( final Point move )
+  {
+    if( m_editInfo == null && (Math.abs( move.x - m_startX ) > m_trashold || Math.abs( move.y - m_startY ) > m_trashold) )
+      m_editInfo = m_clickInfo.clone();
+
+    final Point plotPoint = ChartHandlerUtilities.screen2plotPoint( new Point( move.x - m_deltaSnapX, move.y - m_deltaSnapY ), getChart().getPlotRect() );
+    doMouseMoveAction( plotPoint, m_editInfo == null ? m_clickInfo : m_editInfo );
+  }
+
+  @Override
+  public void mouseUp( final MouseEvent e )
+  {
+    final Point pos = new Point( e.x, e.y );
+    mouseUp( pos );
+  }
+
+  protected void mouseUp( final Point up )
+  {
+    try
+    {
+      if( m_editInfo != null )
+      {
+        Point position = new Point( up.x - m_deltaSnapX, up.y - m_deltaSnapY );
+        final Point plotPoint = ChartHandlerUtilities.screen2plotPoint( position, getChart().getPlotRect() );
+        doMouseUpAction( plotPoint, m_editInfo );
+      }
+      else if( m_clickInfo != null )
+        doMouseUpAction( null, m_clickInfo );
+    }
+    catch( final Error err )
+    {
+      err.printStackTrace();
+    }
+    finally
+    {
+      m_clickInfo = null;
+      m_editInfo = null;
+      final IChartComposite chart = getChart();
+      chart.setEditInfo( null );
+      chart.setDragArea( null );
+    }
+  }
+
+  protected void updateSelection( final EditInfo editInfo )
+  {
+    if( editInfo != null && editInfo.getLayer() != null )
+      ((IEditableChartLayer) editInfo.getLayer()).commitDrag( editInfo.getPosition(), editInfo );
+  }
+
+}
