3ee2779f1cf9030c6ee2c5ef3b78caa33fe99ece
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-07-17 16:39:04 +0000

    Protect databinding against null validators

--- KalypsoCommons/src/org/kalypso/commons/databinding/DataBinder.java
+++ KalypsoCommons/src/org/kalypso/commons/databinding/DataBinder.java
@@ -2,41 +2,41 @@
  *
  *  This file is part of kalypso.
  *  Copyright (C) 2004 by:
- * 
+ *
  *  Technical University Hamburg-Harburg (TUHH)
  *  Institute of River and coastal engineering
  *  Denickestraﬂe 22
  *  21073 Hamburg, Germany
  *  http://www.tuhh.de/wb
- * 
+ *
  *  and
- *  
+ *
  *  Bjoernsen Consulting Engineers (BCE)
  *  Maria Trost 3
  *  56070 Koblenz, Germany
  *  http://www.bjoernsen.de
- * 
+ *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
  *  License as published by the Free Software Foundation; either
  *  version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  Lesser General Public License for more details.
- * 
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- * 
+ *
  *  Contact:
- * 
+ *
  *  E-Mail:
  *  belger@bjoernsen.de
  *  schlienger@bjoernsen.de
  *  v.doemming@tuhh.de
- *   
+ *
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.commons.databinding;
 
@@ -57,7 +57,7 @@ import org.eclipse.core.databinding.validation.IValidator;
  * <li>converters</li>
  * <li>validators</li>
  * </ul>
- * 
+ *
  * @author Gernot Belger
  */
 public class DataBinder extends AbstractDataBinder
@@ -91,7 +91,8 @@ public class DataBinder extends AbstractDataBinder
   {
     checkApplied();
 
-    m_targetAfterGetValidator.add( validator );
+    if( validator != null )
+      m_targetAfterGetValidator.add( validator );
   }
 
   /**
@@ -101,7 +102,8 @@ public class DataBinder extends AbstractDataBinder
   {
     checkApplied();
 
-    m_targetAfterConvertValidator.add( validator );
+    if( validator != null )
+      m_targetAfterConvertValidator.add( validator );
   }
 
   /**
@@ -111,7 +113,8 @@ public class DataBinder extends AbstractDataBinder
   {
     checkApplied();
 
-    m_targetBeforeSetValidator.add( validator );
+    if( validator != null )
+      m_targetBeforeSetValidator.add( validator );
   }
 
   /**
@@ -121,7 +124,8 @@ public class DataBinder extends AbstractDataBinder
   {
     checkApplied();
 
-    m_modelAfterGetValidator.add( validator );
+    if( validator != null )
+      m_modelAfterGetValidator.add( validator );
   }
 
   /**
@@ -131,7 +135,8 @@ public class DataBinder extends AbstractDataBinder
   {
     checkApplied();
 
-    m_modelAfterConvertValidator.add( validator );
+    if( validator != null )
+      m_modelAfterConvertValidator.add( validator );
   }
 
   /**
@@ -141,7 +146,8 @@ public class DataBinder extends AbstractDataBinder
   {
     checkApplied();
 
-    m_modelBeforeSetValidator.add( validator );
+    if( validator != null )
+      m_modelBeforeSetValidator.add( validator );
   }
 
   @Override
