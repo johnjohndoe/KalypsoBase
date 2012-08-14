ec37990b1c31bb820bbad232d95ad980479f23c3
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-08-14 08:38:01 +0000

    AddTheme command now gets commandParameter about which wizardSelection to use.

--- KalypsoAddLayer/src/org/kalypso/ui/view/action/AddThemeHandler.java
+++ KalypsoAddLayer/src/org/kalypso/ui/view/action/AddThemeHandler.java
@@ -40,6 +40,7 @@
  *  ---------------------------------------------------------------------------*/
 package org.kalypso.ui.view.action;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.expressions.IEvaluationContext;
@@ -57,12 +58,18 @@ import org.kalypso.ui.editor.mapeditor.GisMapOutlinePage;
  */
 public class AddThemeHandler extends AbstractHandler
 {
+  /* id of an extension ot 'org.kalypso.ui.addLayerWizard#wizardSelection', if not set, a default id is used. */
+  private static final String PARAMETER_WIZARD_SELECTION = "wizardSelectioinId"; //$NON-NLS-1$
+
   private static final String WIZARD_SELECTION_DEFAULT = "org.kalypso.ui.addlayer.wizardselection.default"; //$NON-NLS-1$
 
   @Override
   public Object execute( final ExecutionEvent event )
   {
     final IEvaluationContext context = (IEvaluationContext) event.getApplicationContext();
+
+    final String wizardSelectionId = getWizardSelectionId( event );
+
     final IWorkbenchWindow activeWorkbenchWindow = (IWorkbenchWindow) context.getVariable( ISources.ACTIVE_WORKBENCH_WINDOW_NAME );
     final Shell shell = (Shell) context.getVariable( ISources.ACTIVE_SHELL_NAME );
     final IStructuredSelection selection = (IStructuredSelection) context.getVariable( ISources.ACTIVE_CURRENT_SELECTION_NAME );
@@ -71,11 +78,20 @@ public class AddThemeHandler extends AbstractHandler
 
     final IWorkbench workbench = activeWorkbenchWindow.getWorkbench();
 
-    final KalypsoAddLayerWizard wizard = new KalypsoAddLayerWizard( viewer, selection, workbench, WIZARD_SELECTION_DEFAULT );
+    final KalypsoAddLayerWizard wizard = new KalypsoAddLayerWizard( viewer, selection, workbench, wizardSelectionId );
     wizard.setForcePreviousAndNextButtons( true );
     final WizardDialog dialog = new WizardDialog( shell, wizard );
     dialog.open();
 
     return null;
+  }
+
+  private String getWizardSelectionId( final ExecutionEvent event )
+  {
+    final String wizardSelectionId = event.getParameter( PARAMETER_WIZARD_SELECTION );
+    if( StringUtils.isBlank( wizardSelectionId ) )
+      return WIZARD_SELECTION_DEFAULT;
+
+    return wizardSelectionId;
   }
 }
