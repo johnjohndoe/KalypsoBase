011049ca78a76ba46371124b4ed951a396b40508
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-08-27 08:39:20 +0000

    triangulated surface implements elevation model. Also keep last found triangle. We assume that the next search is near the last one, greatly improving performance in some situations.

--- KalypsoDeegree/deegree/org/kalypsodeegree/model/geometry/GM_TriangulatedSurface.java
+++ KalypsoDeegree/deegree/org/kalypsodeegree/model/geometry/GM_TriangulatedSurface.java
@@ -15,16 +15,16 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * history:
- * 
+ *
  * Files in this package are originally taken from deegree and modified here
  * to fit in kalypso. As goals of kalypso differ from that one in deegree
- * interface-compatibility to deegree is wanted but not retained always. 
- * 
- * If you intend to use this software in other ways than in kalypso 
+ * interface-compatibility to deegree is wanted but not retained always.
+ *
+ * If you intend to use this software in other ways than in kalypso
  * (e.g. OGC-web services), you should consider the latest version of deegree,
  * see http://www.deegree.org .
  *
- * all modifications are licensed as deegree, 
+ * all modifications are licensed as deegree,
  * original copyright:
  *
  * Copyright (C) 2001 by:
@@ -35,12 +35,14 @@
  */
 package org.kalypsodeegree.model.geometry;
 
+import org.kalypsodeegree.model.elevation.IElevationModel;
+
 /**
  * A triangulated surface.
- * 
+ *
  * @author Gernot Belger
  */
-public interface GM_TriangulatedSurface extends GM_PolyhedralSurface<GM_Triangle>
+public interface GM_TriangulatedSurface extends GM_PolyhedralSurface<GM_Triangle>, IElevationModel
 {
   /**
    * Returns the value of the triangulated at the given position.
@@ -48,7 +50,7 @@ public interface GM_TriangulatedSurface extends GM_PolyhedralSurface<GM_Triangle
    * The value is obtained by finding the first triangle which covers the given position and calculating the z-value of
    * the position of the linear-equation defined by the (corner-coordiantes of the) triangle.
    * </p>
-   * 
+   *
    * @param positionMust
    *          be in the same coordinate system as the tirangulated surface.
    * @return The linearly interpolated z-value of the first triangle covering the position. <code>Double.NaN</code> if
@@ -62,7 +64,7 @@ public interface GM_TriangulatedSurface extends GM_PolyhedralSurface<GM_Triangle
    * The value is obtained by finding the first triangle which covers the given position and calculating the z-value of
    * the position of the linear-equation defined by the (corner-coordiantes of the) triangle.
    * </p>
-   * 
+   *
    * @param positionMust
    *          be in the same coordinate system as the tirangulated surface.
    * @return The linearly interpolated z-value of the first triangle covering the position. <code>Double.NaN</code> if
