e91649a8d2ad1012f866f1cbbec292812ccbbe4b
albert <albert@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-09-03 13:57:26 +0000

    replaced headers of java files with new header

--- EclipsePlatformContributions/src/org/kalypso/contribs/eclipse/jface/viewers/FocusCellOwnerDrawHighlighter.java
+++ EclipsePlatformContributions/src/org/kalypso/contribs/eclipse/jface/viewers/FocusCellOwnerDrawHighlighter.java
@@ -1,15 +1,21 @@
-/*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/** This file is part of Kalypso
  *
- * Contributors:
- * 	   Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
- * 												 - fix for bug 183850, 182652
- *     IBM Corporation - initial API and implementation
- ******************************************************************************/
+ *  Copyright (c) 2012 by
+ *
+ *  Björnsen Beratende Ingenieure GmbH, Koblenz, Germany (Bjoernsen Consulting Engineers), http://www.bjoernsen.de
+ *  Technische Universität Hamburg-Harburg, Institut für Wasserbau, Hamburg, Germany
+ *  (Technical University Hamburg-Harburg, Institute of River and Coastal Engineering), http://www.tu-harburg.de/wb/
+ *
+ *  Kalypso is free software: you can redistribute it and/or modify it under the terms  
+ *  of the GNU Lesser General Public License (LGPL) as published by the Free Software 
+ *  Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  Kalypso is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with Kalypso.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package org.kalypso.contribs.eclipse.jface.viewers;
 
