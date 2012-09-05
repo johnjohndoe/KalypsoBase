c3207a2d1a7d503b97f12d85f5e001ee6b404807
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-09-05 20:16:12 +0000

    Comment

--- KalypsoCommons/src/org/kalypso/commons/java/lang/Objects.java
+++ KalypsoCommons/src/org/kalypso/commons/java/lang/Objects.java
@@ -57,6 +57,9 @@ public final class Objects
   {
   }
 
+  /**
+   * Returns <code>true</code> if any of the given objects is <code>null</code>.
+   */
   public static boolean isNull( final Object... objects )
   {
     if( objects == null )
