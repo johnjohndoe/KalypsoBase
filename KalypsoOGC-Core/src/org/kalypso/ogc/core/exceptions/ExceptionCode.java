85312f00eb81c398aa5c483c24432f1e6687a120
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-11-30 10:54:45 +0000

    Tag for release of kalypso_12.11.0.RC6.20121130

---
+++ KalypsoOGC-Core/src/org/kalypso/ogc/core/exceptions/ExceptionCode.java
@@ -0,0 +1,17 @@
+package org.kalypso.ogc.core.exceptions;
+
+/**
+ * This enumaration specifies all possible exception codes.
+ * 
+ * @author Toni DiNardo
+ */
+public enum ExceptionCode
+{
+  OPERATION_NOT_SUPPORTED,
+  MISSING_PARAMETER_VALUE,
+  INVALID_PARAMETER_VALUE,
+  VERSION_NEGOTIATON_FAILED,
+  INVALID_UPDATE_SEQUENCE,
+  OPTION_NOT_SUPPORTED,
+  NO_APPLICABLE_CODE
+}
