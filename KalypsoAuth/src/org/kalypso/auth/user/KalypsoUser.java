20a2e0ec93c313176a8b2eb6e070696e8a1b1212
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-11-30 10:54:48 +0000

    Tag for release of kalypso_12.11.0.RC6.20121130

---
+++ KalypsoAuth/src/org/kalypso/auth/user/KalypsoUser.java
@@ -0,0 +1,80 @@
+package org.kalypso.auth.user;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * A Kalypso User, default implementation of IKalypsoUser.
+ * 
+ * @author schlienger
+ */
+public class KalypsoUser implements IKalypsoUser
+{
+  private final String[] m_rights;
+
+  private final String m_userName;
+
+  public KalypsoUser( final String userName, final String[] rights )
+  {
+    m_userName = userName;
+    m_rights = rights;
+  }
+
+  @Override
+  public String getUserName( )
+  {
+    return m_userName;
+  }
+
+  @Override
+  public String toString( )
+  {
+    return m_userName;
+  }
+
+  @Override
+  public boolean hasRight( final String right )
+  {
+    for( final String mRight : m_rights )
+    {
+      if( right.equals( mRight ) )
+        return true;
+    }
+
+    return false;
+  }
+
+  @Override
+  public String[] getRights( )
+  {
+    return m_rights;
+  }
+
+  @Override
+  public boolean equals( final Object obj )
+  {
+    if( obj instanceof IKalypsoUser )
+    {
+      final IKalypsoUser other = (IKalypsoUser) obj;
+
+      final EqualsBuilder builder = new EqualsBuilder();
+      builder.append( getUserName(), other.getUserName() );
+      builder.append( getRights(), other.getRights() );
+
+      return builder.isEquals();
+    }
+
+    return super.equals( obj );
+  }
+
+  @Override
+  public int hashCode( )
+  {
+    final HashCodeBuilder builder = new HashCodeBuilder();
+    builder.append( getUserName() );
+    builder.append( getRights() );
+
+    return builder.toHashCode();
+
+  }
+}
