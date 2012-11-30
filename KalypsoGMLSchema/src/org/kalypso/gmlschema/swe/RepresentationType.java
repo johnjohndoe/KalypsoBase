83228ffa010447874761a46e5b76152fc4073b17
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-11-30 10:54:47 +0000

    Tag for release of kalypso_12.11.0.RC6.20121130

---
+++ KalypsoGMLSchema/src/org/kalypso/gmlschema/swe/RepresentationType.java
@@ -0,0 +1,81 @@
+package org.kalypso.gmlschema.swe;
+
+import javax.xml.namespace.QName;
+
+import org.kalypso.gmlschema.property.restriction.IRestriction;
+import org.kalypso.gmlschema.types.IMarshallingTypeHandler;
+import org.kalypso.gmlschema.types.MarshallingTypeRegistrySingleton;
+
+/**
+ * TODO: this is a bad representation of the property! We should instead have an own class for each KIND and return
+ * these one for the property-value
+ * 
+ * @author belger
+ */
+public class RepresentationType
+{
+  private final KIND m_kind;
+
+  private final QName m_valueTypeName;
+
+  private final String m_unit;
+
+  private final String m_frame;
+
+  private final String m_classification;
+
+  private final IRestriction[] m_restrictions;
+
+  public RepresentationType( final KIND kind, final QName valueTypeName, final String unit, final String frame, final IRestriction[] restrictions, final String classification )
+  {
+    m_kind = kind;
+    m_valueTypeName = valueTypeName;
+    m_unit = unit;
+    m_frame = frame;
+    m_restrictions = restrictions;
+    m_classification = classification;
+  }
+
+  public QName getValueTypeName( )
+  {
+    return m_valueTypeName;
+  }
+
+  public String getUnit( )
+  {
+    return m_unit;
+  }
+
+  public String getFrame( )
+  {
+    return m_frame;
+  }
+
+  public IRestriction[] getRestrictions( )
+  {
+    return m_restrictions;
+  }
+
+  public String getClassification( )
+  {
+    return m_classification;
+  }
+
+  public IMarshallingTypeHandler getTypeHandler( )
+  {
+    return MarshallingTypeRegistrySingleton.getTypeRegistry().getTypeHandlerForTypeName( m_valueTypeName );
+  }
+
+  public KIND getKind( )
+  {
+    return m_kind;
+  }
+
+  public static enum KIND
+  {
+    SimpleType,
+    Number,
+    Word,
+    Boolean;
+  }
+}
