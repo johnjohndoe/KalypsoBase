2063587429b4f62b286de774f7f2754583d603aa
belger <belger@9531889c-ada0-4d75-b468-cc276f5ed7d8>
2012-11-30 10:54:59 +0000

    Tag for release of kalypso_12.11.0.RC6.20121130

---
+++ KalypsoFeatureView/src/org/kalypso/featureview/KalypsoFeatureViewPlugin.java
@@ -0,0 +1,88 @@
+package org.kalypso.featureview;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The main plugin class to be used in the desktop.
+ */
+public class KalypsoFeatureViewPlugin extends AbstractUIPlugin
+{
+  public static final String PLUGIN_ID = "org.kalypso.featureview"; //$NON-NLS-1$
+
+  // The shared instance.
+  private static KalypsoFeatureViewPlugin plugin;
+
+  // Resource bundle.
+  private ResourceBundle resourceBundle;
+
+  /**
+   * The constructor.
+   */
+  public KalypsoFeatureViewPlugin( )
+  {
+    super();
+    plugin = this;
+    try
+    {
+      resourceBundle = ResourceBundle.getBundle( "org.kalypso.featureview.KalypsoFeatureViewPluginResources" ); //$NON-NLS-1$
+    }
+    catch( final MissingResourceException x )
+    {
+      resourceBundle = null;
+    }
+  }
+
+  /**
+   * This method is called upon plug-in activation
+   */
+  @Override
+  public void start( final BundleContext context ) throws Exception
+  {
+    super.start( context );
+  }
+
+  /**
+   * This method is called when the plug-in is stopped
+   */
+  @Override
+  public void stop( final BundleContext context ) throws Exception
+  {
+    super.stop( context );
+  }
+
+  /**
+   * Returns the shared instance.
+   */
+  public static KalypsoFeatureViewPlugin getDefault( )
+  {
+    return plugin;
+  }
+
+  /**
+   * Returns the string from the plugin's resource bundle, or 'key' if not found.
+   */
+  public static String getResourceString( final String key )
+  {
+    final ResourceBundle bundle = KalypsoFeatureViewPlugin.getDefault().getResourceBundle();
+    try
+    {
+      return bundle != null ? bundle.getString( key ) : key;
+    }
+    catch( final MissingResourceException e )
+    {
+      return key;
+    }
+  }
+
+  /**
+   * Returns the plugin's resource bundle,
+   */
+  public ResourceBundle getResourceBundle( )
+  {
+    return resourceBundle;
+  }
+}
